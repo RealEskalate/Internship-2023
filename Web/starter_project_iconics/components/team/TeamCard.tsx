53868fe86260762a01d1a4917af41f311703a793
Zerihun Moges <zerihunmoges78@gmail.com>
2023-04-22 05:50:33 -0700

    modify width of team card

--- Web/starter_project_iconics/components/team/TeamCard.tsx
+++ Web/starter_project_iconics/components/team/TeamCard.tsx
@@ -1,12 +1,22 @@
 import React from 'react'
 
-function TeamCard() {
+export interface TeamCardProps{
+    name: string
+    job: string
+    description: string
+    instagram?: string
+     linkedin?: string
+     facebook?: string
+     avatar?: string
+}
+
+function TeamCard({name, job, description, avatar = 'https://www.dmarge.com/wp-content/uploads/2021/01/dwayne-the-rock-.jpg', instagram, linkedin, facebook}: TeamCardProps) {
     return (
-        <div className='flex flex-col bg-white rounded-lg max-w-xs p-4 m-2 items-center justify-center shadow-lg border-none'>
-            <img className='rounded-full w-28 h-28 mt-2 object-cover ' src='https://www.dmarge.com/wp-content/uploads/2021/01/dwayne-the-rock-.jpg'></img>
-            <h1 className="font-bold uppercase text-[1.5rem] mt-2">nathaniel awel</h1>
-            <h2 className="uppercase text-[24]">software engineer</h2>
-            <p className="text-center my-4 text-[#7D7D7D]">He is a motivated software engineer looking to pursue a successful career in software development where he can help deliver software solutions to social problems. His skills include Web Development, Data Structures, Machine Learning, and DevOps. Natnael’s hobbies include coding, playing soccer, and watching movies.</p>
+        <div className='flex flex-col bg-white rounded-lg p-4 m-2 items-center justify-center shadow-lg'>
+            <img className='rounded-full w-28 h-28 mt-2 object-cover ' src={avatar}></img>
+            <h1 className="font-bold uppercase text-[1.5rem] mt-2">{name}</h1>
+            <h2 className="uppercase text-[24]">{job}</h2>
+            <p className="text-center my-4 text-[#7D7D7D]">{description}</p>
             <hr className='my-2 w-[100%] '></hr>
 
             <div className="flex justify-around w-[100%]">
