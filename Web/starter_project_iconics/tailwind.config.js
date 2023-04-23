5f9c689300930b98111c977feb80400e2caee9b8
aklile <aklileyilma@gmail.com>
2023-04-24 00:48:39 +0300

    add configuration for font-styles

--- Web/starter_project_iconics/tailwind.config.js
+++ Web/starter_project_iconics/tailwind.config.js
@@ -20,6 +20,10 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+      },
     },
   },
   plugins: [],
