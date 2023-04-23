c9e51aae1ed9cde158c419979d21106dfd772342
Yared Tsegaye <yaredtsegaye120@gmail.com>
2023-04-23 10:14:54 +0300

    [Mobile-dartknignt]: Create splash screen and add assets

---
+++ Mobile/starter_project_dark_knights/lib/features/example/presentation/screen/onboarding_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class OnboardingScreen extends StatefulWidget {
+  const OnboardingScreen({super.key});
+
+  @override
+  State<OnboardingScreen> createState() => _OnboardingScreenState();
+}
+
+class _OnboardingScreenState extends State<OnboardingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
