67ebf70f1ddeb4d98686269ec560fbdcd2055e73
unknown <yaredt29@gmail.com>
2023-04-23 22:40:12 +0300

    loop the hero images

--- Web/starter_project_vanguard/components/team/HeroSection.tsx
+++ Web/starter_project_vanguard/components/team/HeroSection.tsx
@@ -1,6 +1,23 @@
+import clsx from "clsx"
 import Image from "next/image"
 
 const HeroSection = () => {
+
+  const heroImages = [
+    {
+      className: "right-24 -top-6",
+      name: "teams-hero-image-1.png"
+    },
+    {
+      className: "-top-8 left-14",
+      name: "teams-hero-image-2.png"
+    },
+    {
+      className: "left-24 -bottom-20",
+      name: "teams-hero-image-3.png"
+    }
+
+]
   return (
     <div className="flex mx-10">
         <div className="basis-2/5 ">
@@ -8,12 +25,16 @@ const HeroSection = () => {
             <p className="text-secondary-text mt-8 text-xl">Meet our development team is a small but highly skilled and experienced group of professionals. We have a talented mix of web developers, software engineers, project managers and quality assurance specialists who are passionate about developing exceptional products and services.</p>
         </div>
         <div className=" relative basis-3/5 p-40 flex flex-col gap-4 bg-center bg-no-repeat bg-contain text-primary-text" style={{backgroundImage: 'url("/images/teams-hero-section/teams-hero-background.png")'}}>
-          <Image className=" md:absolute right-24 -top-6" width={230} height={250} src={'/images/teams-hero-section/teams-hero-image-1.png'} alt={"hero image"}/>
-          <Image className="absolute -top-8 left-14 " width={230} height={250} src={'/images/teams-hero-section/teams-hero-image-2.png'} alt={"hero-image-2"}/>
+          {
+            heroImages.map((img, idx:number) => {
+              return(
+                <Image key={idx} className={clsx("absolute", img.className)} width={230} height={250} src={`/images/teams-hero-section/${img.name}`} alt={img.name}/>
+              )
+            })
+          }
           <h1 className="uppercase font-bold text-5xl ml-24"><span className=" text-primary">Team</span>  work</h1>
           <h1 className="uppercase font-bold text-5xl ml-24">collaboration</h1>
           <h1 className="uppercase font-bold text-5xl mx-24 "><span className=" text-primary">Hard</span> work</h1>
-          <Image className="absolute left-24 -bottom-20" width={230} height={250} src={'/images/teams-hero-section/teams-hero-image-3.png'} alt={"hero-image-3"}/>
         </div>
         
 
