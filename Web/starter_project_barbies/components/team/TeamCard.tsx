4a78ed5649485f6c0417cdc9d4663de8f5552004
hello_world <haileformal@gmail.com>
2023-04-23 13:59:03 +0300

    capitalize name and job title

--- Web/starter_project_barbies/components/team/TeamCard.tsx
+++ Web/starter_project_barbies/components/team/TeamCard.tsx
@@ -22,16 +22,16 @@ const TeamCard = ({
   instagram,
 }: TeamCardProps) => {
   return (
-    <div className="team-card box-content font-sans w-72 m-4 text-center shadow-md shadow-gray-200 rounded-md py-2">
+    <div className="team-card box-content font-sans w-72 mt-4 text-center shadow-md shadow-gray-200 rounded-md py-2 m-auto">
       <div className="team-card__image container overflow-hidden my-4">
         <Image className="rounded-full mx-auto w-32" width={200} height={200} src={image} alt={name} />
       </div>
 
       <div className="team-card__content m-4">
-        <h3 className="team-card__name text-2xl m-3 font-extrabold tracking-widest">
+        <h3 className="team-card__name text-2xl m-3 font-extrabold tracking-widest uppercase">
           {name}
         </h3>
-        <p className="team-card__job-title m-3 tracking-widest text-base">
+        <p className="team-card__job-title m-3 tracking-widest text-base uppercase">
           {jobTitle}
         </p>
         <p className="team-card__description text-sm mt-5 mb-3 text-gray-400 font-light px-4 tracking-wide">
@@ -39,7 +39,7 @@ const TeamCard = ({
         </p>
       </div>
 
-      <hr className="h-0.5 border-t-0 bg-neutral-100 opacity-100 dark:opacity-50 inline-block ms-auto w-5/6" />
+      <hr className="h-0.5 border-t-0 bg-neutral-100 opacity-100 dark:opacity-50 block m-auto w-5/6" />
 
       <div className="team-card__social flex-row mt-3">
         <div className="team-card__social__icon inline-block w-1/3">
