61c94ec1fd972727146e0bdc093619dd2af6730c
unknown <yaredt29@gmail.com>
2023-04-23 21:04:20 +0300

    fix(web): fix conflicts when rebasing

--- Web/starter_project_vanguard/next.config.js
+++ Web/starter_project_vanguard/next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    //used to allow to use network images with "next Image"
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'media.licdn.com',
+        port: '',
+        pathname: '/dms/image/**',
+      },
+    ],
+  },
+
 }
 
 module.exports = nextConfig
