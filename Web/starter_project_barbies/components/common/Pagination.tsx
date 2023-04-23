f84b8851a29b25a25b93a194346ecbc919007d9b
Nazrawi Demeke Workalemahu <nazrawi.demeke@gmail.com>
2023-04-23 16:33:21 +0300

    fix tabs

--- Web/starter_project_barbies/components/common/Pagination.tsx
+++ Web/starter_project_barbies/components/common/Pagination.tsx
@@ -1,40 +1,40 @@
 import React, { useState } from "react"
 
 interface PaginationProps {
-	numberOfPages: number
+  numberOfPages: number
 }
 
 interface PaginationItemProps {
-	pageNumber: number
-	isCurrentPage: boolean
-	onPageClick: React.Dispatch<React.SetStateAction<number>>
+  pageNumber: number
+  isCurrentPage: boolean
+  onPageClick: React.Dispatch<React.SetStateAction<number>>
 }
 
 export const Pagination: React.FC<PaginationProps> = ({ numberOfPages }) => {
-	const [currentPage, setCurrentPage] = useState(1)
+  const [currentPage, setCurrentPage] = useState(1)
 
-	let paginationItems = []
-	for (let i = 1; i <= numberOfPages; i++) {
-		let paginationItem = <PaginationItem pageNumber={i} isCurrentPage={i == currentPage} onPageClick={setCurrentPage} />
-		paginationItems.push(paginationItem)
-	}
+  let paginationItems = []
+  for (let i = 1; i <= numberOfPages; i++) {
+    let paginationItem = <PaginationItem pageNumber={i} isCurrentPage={i == currentPage} onPageClick={setCurrentPage} />
+    paginationItems.push(paginationItem)
+  }
 
-	return (
-		<div className="flex bg-white px-4 py-12 flex-1 justify-center">
-			<nav className="flex gap-3 text-xs font-medium">
-				{paginationItems}
-			</nav>
-		</div>
-	)
+  return (
+    <div className="flex bg-white px-4 py-12 flex-1 justify-center">
+      <nav className="flex gap-3 text-xs font-medium">
+        {paginationItems}
+      </nav>
+    </div>
+  )
 }
 
 const PaginationItem: React.FC<PaginationItemProps> = ({ pageNumber, isCurrentPage, onPageClick }) => {
-	return (
-		<button
-			className={`${"rounded h-8 w-8 " + (isCurrentPage ? "bg-blue-600 text-white" : "bg-gray-200 text-black")}`}
-			onClick={() => { onPageClick(pageNumber) }}
-		>
-			{pageNumber}
-		</button>
-	)
+  return (
+    <button
+      className={`${"rounded h-8 w-8 " + (isCurrentPage ? "bg-blue-600 text-white" : "bg-gray-200 text-black")}`}
+      onClick={() => { onPageClick(pageNumber) }}
+    >
+      {pageNumber}
+    </button>
+  )
 }
