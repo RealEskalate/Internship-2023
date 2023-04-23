b3f7562c019c03ac6ea72d4be192860a3c99765c
Henok-Matheas <henokmatheaskebede@gmail.com>
2023-04-23 20:28:27 +0300

    fix(web-iconics): change naming of image to kebab-case, add responsiveness

--- Web/starter_project_iconics/components/Homepage/Landing.tsx
+++ Web/starter_project_iconics/components/Homepage/Landing.tsx
@@ -1,36 +1,35 @@
 import Image from 'next/image'
 import { BsArrowRightShort } from 'react-icons/bs'
 
-const Landing = () => {
+const Landing: React.FC = () => {
   return (
-    <div className="flex flex-row justify-between p-16">
-      <div className="flex flex-col items-center justify-between basis-5/12">
-        <div className="flex flex-col items-center justify-between">
-          <h1 className="text-7xl font-extrabold">
-            Africa to <span className="text-[#264FAD]">Silicon Valley</span>
-          </h1>
-          <p className="text-secondary-text text-3xl py-5">
-            A2SV up-skills high-potential university students, connects them
-            with opportunities at top tech companies
-          </p>
-          <div className="flex flex-row items-center justify-between">
-            <button className="rounded-lg mx-5 px-6 py-2 border-2 border-blue-500 text-blue-500 hover:bg-blue-600 hover:text-blue-100 duration-300">
-              Get started
-            </button>
-            <button className="flex flex-row items-center rounded-lg px-6 py-2 bg-primary text-blue-100 hover:bg-blue-600 duration-300">
-              Primary <BsArrowRightShort size={30} />
-            </button>
-          </div>
+    <div className="flex flex-col gap-20 lg:gap-0 lg:flex-row justify-around p-16">
+      <div className="flex flex-col items-center basis-5/12 gap-10">
+        <h1 className="text-7xl font-extrabold">
+          Africa to <span className="text-[#264FAD]">Silicon Valley</span>
+        </h1>
+        <p className="text-secondary-text text-3xl">
+          A2SV up-skills high-potential university students, connects them with
+          opportunities at top tech companies
+        </p>
+        <div className="flex flex-row items-center gap-4 w-full">
+          <button className="rounded-lg mx-5 py-2 capitalize w-1/3 border-2 border-primary text-primary hover:bg-blue-600 hover:text-blue-100 duration-300">
+            Get started
+          </button>
+          <button className="flex flex-row justify-center gap-1 rounded-lg w-1/3 py-2 bg-primary text-blue-100 hover:bg-blue-600 duration-300 uppercase">
+            <p>SUPPORT US</p>
+            <BsArrowRightShort className="hidden md:flex" size={25} />
+          </button>
         </div>
       </div>
-      <Image
-        src="/LandingPage.png"
-        alt="Landing page picture"
-        className="basis-6/12"
-        width={500}
-        height={300}
-        priority
-      />
+      <div className="w-full lg:w-6/12">
+        <Image
+          src="/img/landing-page.png"
+          alt="landing page image"
+          width={1000}
+          height={1000}
+        />
+      </div>
     </div>
   )
 }
