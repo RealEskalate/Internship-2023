3fe458a966d0b22cf8dd67014098936bfa435f70
Tseganesh Yifru <t5691yifru@gmail.com>
2023-04-25 01:56:43 +0300

    update blog-card page

--- Web/starter_project_vanguard/components/blog/tag.tsx
+++ Web/starter_project_vanguard/components/blog/tag.tsx
@@ -6,8 +6,8 @@ type ChipProps = {
 
 const Chip: FC<ChipProps> = ({ label }) => {
   return (
-    <div className="flex items-center px-2 py-1 rounded-full bg-gray-100 text-gray-700 mr-2 mb-2 mx-4 my-3">
-      <div className="text-sm font-medium ">{label}</div>
+    <div className="flex items-center px-2 py-1 rounded-full bg-gray-100 text-gray-700 mr-2 mb-2 mx-4 my-3 ">
+      <div className="text-sm font-light">{label}</div>
     </div>
   );
 };
