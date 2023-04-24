a091b3688436e47607ef8ab7aaa19bb2cd4899f3
Nazrawi Demeke Workalemahu <nazrawi.demeke@gmail.com>
2023-04-24 14:42:52 +0300

    refactor code

--- Web/starter_project_barbies/pages/blog/BlogDetail.tsx
+++ Web/starter_project_barbies/pages/blog/BlogDetail.tsx
@@ -1,27 +1,9 @@
-import BlogCard from '@/components/blog/BlogCard'
-import fonts from '@/styles/blog/blog-detail.module.css'
+import fonts from '@/styles/blog/blog-detail.module.scss'
 import { Blog } from '@/types/blog'
 import Image from 'next/image'
+import { RelatedBlogs } from '../../components/blog/RelatedBlogs'
 
 interface BlogDetailProps {
-  blog: Blog
-}
-
-interface BlogAuthorDetailProps {
-  author: {
-    name: string
-    email: string
-    image: string
-    profession: string
-    userName: string
-  }
-}
-
-interface BlogContentProps {
-  content: string
-}
-
-interface RelatedBlogsProps {
   blog: Blog
 }
 
@@ -36,7 +18,7 @@ export const BlogDetail: React.FC<BlogDetailProps> = ({ blog }) => {
 
       {/* Tags & read time */}
       <div className='flex items-center justify-center text-xs font-extralight mt-2'>
-        <div className={"flex items-center gap-3 " + fonts.monteserrat}>
+        <div className={`flex items-center gap-3 ${fonts.monteserrat}`}>
           <div>{blog.tags.join(', ')}</div>
           <div>|</div>
           <div>{blog.readTime} MIN READ</div>
@@ -67,29 +49,37 @@ export const BlogDetail: React.FC<BlogDetailProps> = ({ blog }) => {
   )
 }
 
-const BlogAuthorDetail: React.FC<BlogAuthorDetailProps> = ({ author }) => {
+interface BlogAuthorDetailProps {
+  author: Blog['author']
+}
+
+const BlogAuthorDetail: React.FC<BlogAuthorDetailProps> = ({ author: {name, image, profession, userName} }) => {
   return (
-    <div className={"text-xs font-extralight " + fonts.monteserrat}>
+    <div className={`text-xs font-extralight ${fonts.monteserrat}`}>
 
       {/* Author image */}
       <div className="flex items-center justify-center">
-        <Image src={author.image} alt={author.name} width={38} height={38} className="rounded-full" />
+        <Image src={image} alt={name} width={38} height={38} className="rounded-full" />
       </div>
 
       {/* Author name & profession */}
       <div className='grid grid-cols-3 mt-2' style={{ gridTemplateColumns: `400px 20px 400px` }}>
-        <div className='text-end'>{author.name.toUpperCase()}</div>
+        <div className='text-end'>{name.toUpperCase()}</div>
         <div className='text-center'>|</div>
-        <div>{author.profession.toUpperCase()}</div>
+        <div>{profession.toUpperCase()}</div>
       </div>
 
       {/* Author username */}
       <div className='flex items-center justify-center text-blue-500 font-medium mt-1'>
-        <div className='text-blue'>{author.userName.toUpperCase()}</div>
+        <div className='text-blue'>{userName.toUpperCase()}</div>
       </div>
 
     </div>
   )
+}
+
+interface BlogContentProps {
+  content: Blog['content']
 }
 
 const BlogContent: React.FC<BlogContentProps> = ({ content }) => {
@@ -97,7 +87,7 @@ const BlogContent: React.FC<BlogContentProps> = ({ content }) => {
   let paragraphs = []
   let key = 0
   for (let line of content.split('\n')) {
-    let paragraph = <p key={key++} className={`${'mt-6 ' + ((key == 1) ? ('text-xl ' + fonts.cannon) : ('text-sm font-thin ' + fonts.monteserrat))}`}>
+    let paragraph = <p key={key++} className={`${'mt-6 ' + ((key == 1) ? (`text-xl ${fonts.cannon}`) : (`text-sm font-thin ${fonts.monteserrat}`))}`}>
       {line}
     </p>
     paragraphs.push(paragraph)
@@ -106,32 +96,6 @@ const BlogContent: React.FC<BlogContentProps> = ({ content }) => {
   return (
     <div className='mx-96'>
       {paragraphs}
-    </div>
-  )
-}
-
-const RelatedBlogs: React.FC<RelatedBlogsProps> = ({ blog }) => {
-  // Fetch related blogs
-  let relatedBlogs: Blog[] = new Array(3).fill(blog);
-
-  let blogs = []
-  for (let blog of relatedBlogs) {
-    blogs.push(BlogCard({ blog }))
-  }
-
-  return (
-    <div className={fonts.monteserrat}>
-
-      {/* Title */}
-      <div className='text-lg pt-20 flex items-start font-black'>
-        <h1>Related Blogs</h1>
-      </div>
-
-      {/* Related blogs */}
-      <div className='flex items-center gap-8 justify-center mt-6 text-xs'>
-        {blogs}
-      </div>
-
     </div>
   )
 }
