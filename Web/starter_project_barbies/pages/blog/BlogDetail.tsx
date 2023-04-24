684ab73629c13076cee2fde41af92bc066d82b5b
Nazrawi Demeke Workalemahu <nazrawi.demeke@gmail.com>
2023-04-24 10:57:24 +0300

    setup fonts

--- Web/starter_project_barbies/pages/blog/BlogDetail.tsx
+++ Web/starter_project_barbies/pages/blog/BlogDetail.tsx
@@ -1,4 +1,5 @@
 import BlogCard from '@/components/blog/BlogCard'
+import fonts from '@/styles/blog/blog-detail.module.css'
 import { Blog } from '@/types/blog'
 import Image from 'next/image'
 
@@ -29,16 +30,16 @@ export const BlogDetail: React.FC<BlogDetailProps> = ({ blog }) => {
     <div className='bg-white text-black'>
 
       {/* Blog title */}
-      <div className='text-2xl pt-20 flex items-center justify-center'>
-        <h1>{blog.title}</h1>
+      <div className='text-3xl pt-20 flex items-center justify-center'>
+        <h1 className={fonts.cannon}>{blog.title}</h1>
       </div>
 
       {/* Tags & read time */}
       <div className='flex items-center justify-center text-xs font-extralight mt-2'>
-        <div className="flex items-center gap-3">
-          <div className='text-black'>{blog.tags.join(', ')}</div>
-          <div className='text-black'>|</div>
-          <div className='text-black'>{blog.readTime} MIN READ</div>
+        <div className={"flex items-center gap-3 " + fonts.monteserrat}>
+          <div>{blog.tags.join(', ')}</div>
+          <div>|</div>
+          <div>{blog.readTime} MIN READ</div>
         </div>
       </div>
 
@@ -68,7 +69,7 @@ export const BlogDetail: React.FC<BlogDetailProps> = ({ blog }) => {
 
 const BlogAuthorDetail: React.FC<BlogAuthorDetailProps> = ({ author }) => {
   return (
-    <div className='text-xs font-extralight'>
+    <div className={"text-xs font-extralight " + fonts.monteserrat}>
 
       {/* Author image */}
       <div className="flex items-center justify-center">
@@ -84,7 +85,7 @@ const BlogAuthorDetail: React.FC<BlogAuthorDetailProps> = ({ author }) => {
 
       {/* Author username */}
       <div className='flex items-center justify-center text-blue-500 font-medium mt-1'>
-        <p className='text-blue'>{author.userName.toUpperCase()}</p>
+        <div className='text-blue'>{author.userName.toUpperCase()}</div>
       </div>
 
     </div>
@@ -92,10 +93,11 @@ const BlogAuthorDetail: React.FC<BlogAuthorDetailProps> = ({ author }) => {
 }
 
 const BlogContent: React.FC<BlogContentProps> = ({ content }) => {
+  // Split content into paragraphs
   let paragraphs = []
   let key = 0
   for (let line of content.split('\n')) {
-    let paragraph = <p key={key++} className={`${'mt-6 ' + ((key == 1) ? 'text-xl' : 'text-base font-thin')}`}>
+    let paragraph = <p key={key++} className={`${'mt-6 ' + ((key == 1) ? ('text-xl ' + fonts.cannon) : ('text-sm font-thin ' + fonts.monteserrat))}`}>
       {line}
     </p>
     paragraphs.push(paragraph)
@@ -118,15 +120,15 @@ const RelatedBlogs: React.FC<RelatedBlogsProps> = ({ blog }) => {
   }
 
   return (
-    <div className='bg-white text-black'>
+    <div className={fonts.monteserrat}>
 
       {/* Title */}
-      <div className='text-lg pt-20 flex items-start'>
+      <div className='text-lg pt-20 flex items-start font-black'>
         <h1>Related Blogs</h1>
       </div>
 
-      {/* Blogs */}
-      <div className='flex items-center gap-8 justify-center mt-6 text-sm'>
+      {/* Related blogs */}
+      <div className='flex items-center gap-8 justify-center mt-6 text-xs'>
         {blogs}
       </div>
 
