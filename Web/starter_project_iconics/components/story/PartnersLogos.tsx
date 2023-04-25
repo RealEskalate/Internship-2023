239e6aec39c661a346afcf10277666c658fe24e6
FevenBelay23 <fevenbelay942@gmail.com>
2023-04-25 11:01:40 +0300

    fix success stories page

--- Web/starter_project_iconics/components/story/PartnersLogos.tsx
+++ Web/starter_project_iconics/components/story/PartnersLogos.tsx
@@ -6,24 +6,24 @@ const PartnerLogos: React.FC = () => {
         <h2 className="text-3xl font-semibold text-center mb-6 mt-8 font-DMSans">Current Interview Partners</h2>
         <div className="flex justify-center items-center flex-wrap gap-8">
           <div className="w-52 h-52 flex justify-center items-center bg-white rounded-full">
-            <img src="/success-stories-images/google.png" alt="Google logo" />
+            <img src="/success-stories-images/google-logo.png" alt="Google logo" />
           </div>
           <div className="w-52 h-52 flex justify-center items-center bg-white rounded-full">
-            <img src="/success-stories-images/palantir.png" alt="Palantir logo" />
+            <img src="/success-stories-images/palantir-logo.png" alt="Palantir logo" />
           </div>
           <div className="w-52 h-52 flex justify-center items-center bg-white rounded-full">
-            <img src="/success-stories-images/inst-deep.png" alt="Instdeep logo" />
+            <img src="/success-stories-images/inst-deep-logo.png" alt="Instdeep logo" />
           </div>
           <div className="w-52 h-52 flex justify-center items-center bg-white rounded-full">
-            <img src="/success-stories-images/meta.png" alt="meta logo" />
+            <img src="/success-stories-images/meta-logo.png" alt="meta logo" />
           </div>
         </div>
         <div className="flex justify-center items-center flex-wrap gap-4 mt-0 space-x-64">
           <div className="w-52 h-52 flex justify-center items-center bg-white rounded-full">
-            <img src="/success-stories-images/databricks.png" alt="databricks logo" />
+            <img src="/success-stories-images/databricks-logo.png" alt="databricks logo" />
           </div>
           <div className="w-52 h-52 flex justify-center items-center bg-white rounded-full">
-            <img src="/success-stories-images/linkedin.png" alt="linkedin logo" />
+            <img src="/success-stories-images/linkedin-logo.png" alt="linkedin logo" />
           </div>
         </div>
       </div>
