a7fbdb56a2dd7b0aa5d10f206b38820195dafee6
Tseganesh Yifru <t5691yifru@gmail.com>
2023-04-25 15:17:08 +0300

    update search component

--- Web/starter_project_vanguard/components/blog/searchbar.tsx
+++ Web/starter_project_vanguard/components/blog/searchbar.tsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import { FormEvent } from 'react';
 
-function SearchBar() {
+function SearchBar(): JSX.Element {
+  const handleFormSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const searchTerm = event.currentTarget.search.value;
+    // do something with the search term, such as sending it to a search API
+    console.log(searchTerm);
+  };
+
   return (
-    <div>
-        
-    </div>
-  )
+    <form onSubmit={handleFormSubmit}>
+      <div className="flex items-center justify-center mt-4">
+        <div className="relative text-gray-600">
+          <input
+            type="search"
+            name="search"
+            placeholder="Search..."
+            className="bg-white h-16 w-256 pl-10 rounded-full text-sm focus:outline-none border border-gray-400"
+          />
+          <button
+            type="submit"
+            className="absolute right-0 top-0 mt-3 mr-4"
+          >
+          </button>
+        </div>
+      </div>
+    </form>
+  );
 }
 
-export default SearchBar
+
+export default SearchBar;
--- Web/starter_project_vanguard/pages/blog/blogs.tsx
+++ Web/starter_project_vanguard/pages/blog/blogs.tsx
@@ -1,8 +1,13 @@
+import SearchBar from '@/components/blog/searchbar'
+import Text from "@/components/blog/text"
 import React from 'react'
 
 function Blogs() {
   return (
-    <div>Blogs</div>
+    <div className='bg-white'>
+        <Text >
+        <SearchBar />
+    </div>
   )
 }
 
