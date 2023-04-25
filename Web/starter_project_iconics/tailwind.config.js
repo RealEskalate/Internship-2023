54916558078ffd315716339c443a9d50bd02f14a
aklile <aklileyilma@gmail.com>
2023-04-25 15:06:19 +0300

    remove default config

--- Web/starter_project_iconics/tailwind.config.js
+++ Web/starter_project_iconics/tailwind.config.js
@@ -16,11 +16,6 @@ module.exports = {
         'primary-text': '#353535',
         'secondary-text': '#717171',
       },
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
       fontFamily: {
         montserrat: ['Montserrat', 'sans-serif'],
         poppins: ['Poppins', 'sans-serif'],
