b5f28694e39d340389bc62cfc4749ba9f13a9860
Natnael16 <natnaeldenbi@gmail.com>
2023-04-25 16:13:50 +0300

    Added constant image and colors file to track colors and images

---
+++ Mobile/starter_project_dartsmiths/lib/core/utils/images.dart
@@ -0,0 +1,4 @@
+const String onBoardingPhoto1 = 'assets/images/onboarding_photo_1.png';
+const String onBoardingPhoto2 = 'assets/images/onboarding_photo_2.png';
+const String onBoardingPhoto3 = 'assets/images/onboarding_photo_3.png';
+const String onBoardingPhoto4 = 'assets/images/onboarding_photo_4.png';
