a097e44f052d4f6177405f58c393b7f1d9a4ca7f
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2023-04-25 15:20:56 +0300

    feat(web-iconics): add configuration for font-styles (#42)
    * add configuration for font-styles

--- Web/starter_project_iconics/tailwind.config.js
+++ Web/starter_project_iconics/tailwind.config.js
@@ -10,15 +10,17 @@ module.exports = {
     extend: {
       colors: {
         primary: '#264FAD',
+        secondary: '#E1E7EC',
         accent: '#FF9F43',
         danger: '#b91c1c',
         'primary-text': '#353535',
         'secondary-text': '#717171',
       },
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+        imfell: ['IM Fell French Canon', 'serif'],
+        DMSans: ['Dm Sans'],
       },
     },
   },
