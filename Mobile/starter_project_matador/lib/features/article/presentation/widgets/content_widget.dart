5279d290661d24df4884322963be7aaa6e85a1c1
MuleHakim <mulukenhakim97@gmail.com>
2023-04-25 23:20:50 +0300

    Update ContentFormField

--- Mobile/starter_project_matador/lib/features/article/presentation/widgets/content_widget.dart
+++ Mobile/starter_project_matador/lib/features/article/presentation/widgets/content_widget.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class ContentWidget extends StatelessWidget {
-  const ContentWidget({Key? key}) : super(key: key);
+class ContentFormField extends StatelessWidget {
+  const ContentFormField({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
