0745126ef1c3de84ab22fb6c3915f8c9dc45d9d9
Yared <90713646+Yared04@users.noreply.github.com>
2023-04-25 14:49:54 +0300

    feat(web-vanguard): implemented the hero section on teams page (#26)
    * feat(web): implement hero section of the teams page
    
    * fix(web-vanguard): fix issues according to the comment received from previous pr
    
    * fix(web-vanguard): loop the hero images
    
    *  fix(web-vanguard):removed package.lock.json
    
    *  fix(web-vanguard):change images to img
    
    *  fix(web-vanguard):fix folder structure
    
    *  fix(web-vanguard):add package.lock.json
    
    *  fix(web-vanguard):restore the images folder

--- Web/starter_project_vanguard/next.config.js
+++ Web/starter_project_vanguard/next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  //Configured to use network images, from websites listed below, with "next Image"
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'media.licdn.com',
+        port: '',
+        pathname: '/dms/image/**',
+      },
+    ],
+  },
+
 }
 
 module.exports = nextConfig
