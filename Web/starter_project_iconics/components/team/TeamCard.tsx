151dcf02839fd13a6612def1a5d422d9395d41c4
Zerihun Moges <zerihunmoges78@gmail.com>
2023-04-25 05:08:02 -0700

    modifiy text size of contents

--- Web/starter_project_iconics/components/team/TeamCard.tsx
+++ Web/starter_project_iconics/components/team/TeamCard.tsx
@@ -11,22 +11,22 @@ export interface TeamCardProps{
 
 function TeamCard({name, job, description, avatar = 'https://www.dmarge.com/wp-content/uploads/2021/01/dwayne-the-rock-.jpg', instagram, linkedin, facebook}: TeamCardProps) {
     return (
-        <div className='flex flex-col bg-white rounded-lg p-8 m-2 items-center justify-center shadow-lg'>
+        <div className='flex flex-col bg-white rounded-lg p-6 m-2 items-center justify-center shadow-lg'>
             <img className='rounded-full w-28 h-28 mt-2 object-cover ' src={avatar}></img>
             <h1 className="font-bold uppercase text-2xl m-1">{name}</h1>
             <h2 className="uppercase text-xl">{job}</h2>
             <p className="text-center my-4 text-xl text-[#7D7D7D]">{description}</p>
-            <hr className='my-2 w-[100%] '></hr>
+            <hr className='my-6 w-[100%] '></hr>
 
             <div className="flex justify-around w-[100%]">
                 
-                    <a href=""><img className='rounded-md object-contain w-6' src='/facebook.png'></img></a>
+                    <a href=""><img className='rounded-md object-contain w-xl' src='/facebook.png'></img></a>
               
                 <a href=''>
-                    <img className='rounded-md object-contain w-6' src='/instagram.png'></img>
+                    <img className='rounded-md object-contain w-xl' src='/instagram.png'></img>
                 </a>
                 <a href=''>
-                    <img className='rounded-md object-contain w-6' src='/linkedin.png'></img>
+                    <img className='rounded-md object-contain w-xl' src='/linkedin.png'></img>
                 </a>
 
             </div>
