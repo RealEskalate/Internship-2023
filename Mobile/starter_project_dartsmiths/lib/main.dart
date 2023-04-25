773b6328f12bb19df78ecf5a09289e66d41c05b1
jealsab <jealsabb@gmail.com>
2023-04-25 10:10:06 +0300

    add home page

--- Mobile/starter_project_dartsmiths/lib/main.dart
+++ Mobile/starter_project_dartsmiths/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:dartsmiths/features/example/presentation/screen/home_page.dart';
+import 'package:dartsmiths/features/home/presentation/screen/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
