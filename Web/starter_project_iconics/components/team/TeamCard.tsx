7d4507dd25a4ecbbc70e1bf070badddfb1116c8e
Zerihun Moges <zerihunmoges78@gmail.com>
2023-04-25 05:45:46 -0700

    remove avatar default value

--- Web/starter_project_iconics/components/team/TeamCard.tsx
+++ Web/starter_project_iconics/components/team/TeamCard.tsx
@@ -6,10 +6,10 @@ export interface TeamCardProps{
     instagram?: string
      linkedin?: string
      facebook?: string
-     avatar?: string
+     avatar: string
 }
 
-function TeamCard({name, job, description, avatar = 'https://www.dmarge.com/wp-content/uploads/2021/01/dwayne-the-rock-.jpg', instagram, linkedin, facebook}: TeamCardProps) {
+function TeamCard({name, job, description, avatar, instagram, linkedin, facebook}: TeamCardProps) {
     return (
         <div className='flex flex-col bg-white rounded-lg p-6 m-2 items-center justify-center shadow-xl'>
             <img className='rounded-full w-32 h-32 mt-2 object-cover ' src={avatar}></img>
