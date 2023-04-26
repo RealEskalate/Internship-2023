585a54c0e9096cac8330b564419787144b2b3941
Segni Desta <segnidesta60@gmail.com>
2023-04-26 08:26:57 +0300

    mobile responsive navigation bar

--- Web/starter_project_vanguard/components/layout/navbar.tsx
+++ Web/starter_project_vanguard/components/layout/navbar.tsx
@@ -2,9 +2,11 @@ import clsx from 'clsx'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import { AiOutlineMenu } from 'react-icons/ai'
 
-import logo from '../../public/images/a2sv-logo.png'
 import { NavItem } from '@/types/nav'
+import { useState } from 'react'
+import logo from '../../public/img/a2sv-logo.png'
 
 const navItems: NavItem[] = [
   { name: 'Home', href: '/' },
@@ -13,17 +15,17 @@ const navItems: NavItem[] = [
   { name: 'About Us', href: '/about' },
   { name: 'Get Involved', href: '/get-involved' },
 ]
-
 const Navbar = () => {
   const router = useRouter()
   const currSegment = router.pathname.split('/')[1] || ''
+  const [toggle, setToggle] = useState(false)
 
   return (
-    <div className="sticky top-0 bg-white bg-opacity-100 flex justify-between font-montserrat p-10">
+    <div className="sticky top-0 z-50 bg-white bg-opacity-100 flex justify-between font-montserrat px-10 pt-5">
       <Link href="/">
-        <Image src={logo} alt="A2SV Logo" />
+        <Image src={logo} alt="A2SV Logo" className="h-12 w-25  " />
       </Link>
-      <div className="flex gap-4">
+      <div className="hidden md:flex gap-4">
         {navItems.map((navItem, index) => (
           <Link
             href={navItem.href}
@@ -39,10 +41,44 @@ const Navbar = () => {
           </Link>
         ))}
       </div>
-      <div className="flex gap-4">
-        <button className="bg-primary text-white rounded-md w-[78px] h-[36px]">
+
+      <div className="hidden md:flex float-right gap-4">
+        <button className="bg-primary text-white rounded-md w-[78px] h-[36px] md:float-right">
           Donate
         </button>
+      </div>
+      <div
+        className="md:hidden flex flex-col "
+        onClick={() => setToggle(!toggle)}
+      >
+        <AiOutlineMenu className="w-7 h-7 cursor-pointer" />
+        <div
+          className={`${
+            !toggle ? 'hidden' : 'flex'
+          } flex-col gap-4 p-6 black-gradient absolute top-20 right-2 mx-4 my-2 min-w-[140px] rounded-xl
+          z-50 bg-gray-100 bg-opacity-100
+          `}
+        >
+          {' '}
+          {navItems.map((navItem, index) => (
+            <Link
+              href={navItem.href}
+              key={index}
+              className={clsx(
+                'font-semibold',
+                currSegment === navItem.href.split('/')[1]
+                  ? 'border-b-4 border-primary text-primary'
+                  : 'text-tertiary-text'
+              )}
+              onClick={() => setToggle(!toggle)}
+            >
+              {navItem.name}
+            </Link>
+          ))}
+          <button className="bg-primary text-white rounded-md w-[78px] h-[36px] md:float-right">
+            Donate
+          </button>
+        </div>
       </div>
     </div>
   )
