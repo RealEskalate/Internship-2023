420ae56b0372dd5129fa7765051337c1e5b2f571
hello_world <haileformal@gmail.com>
2023-04-26 12:38:54 +0300

    address nit PR comments

--- Web/starter_project_barbies/pages/team/index.tsx
+++ Web/starter_project_barbies/pages/team/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import TeamsImage from '../../public/image/team/teams_page_img.svg'
+import heroImage from '../../public/image/team/hero-image.svg'
 import Image from 'next/image'
-import TeamMemberCard from "@/components/team/TeamMemberCard";
-import TeamMembers from "@/data/team/data.json";
+import TeamMemberCard from '@/components/team/TeamMemberCard'
+import teamMembers from '@/data/team-members.json'
 
-const Team = () => {
+const Index = () => {
   return (
     <div className="px-12 my-8 md:px-20 lg:px-28">
       <div className="flex flex-col md:flex-row">
@@ -21,14 +21,14 @@ const Team = () => {
           </p>
         </div>
         <div className="w-0 md:w-3/5 my-8">
-          <Image src={TeamsImage} alt="placeholder" />
+          <Image src={heroImage} alt="placeholder" />
         </div>
       </div>
 
       <hr className="h-0.5 border-t-0 bg-neutral-100 opacity-100 dark:opacity-100 block w-5/6 my-8 m-auto" />
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        {TeamMembers.map((data, index) => (
+        {teamMembers.map((data, index) => (
           <TeamMemberCard key={index} teamMember={data} />
         ))}
       </div>
@@ -36,4 +36,4 @@ const Team = () => {
   )
 }
 
-export default Team
+export default Index
