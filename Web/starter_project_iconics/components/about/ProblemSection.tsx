8359670ee4b5da2850769810c381a016d538524e
Brook Alemu <brookwworku3@gmail.com>
2023-04-26 04:41:47 -0900

    First commit

---
+++ Web/starter_project_iconics/components/about/ProblemSection.tsx
@@ -0,0 +1,22 @@
+
+
+const ProblemSection = () => {
+    return (
+        <div className='problem-section'>
+            <div className = "col">
+            <h1>The Problem We &nbsp;<div className='blue-word'>Are Solving</div></h1>
+            <img  src = "https://www.shutterstock.com/image-vector/picture-icon-260nw-413647060.jpg" height = {"33px"} alt = "africa-map" />
+            <p>Africa’s future depends on innovation. Transformative technologies can drive rapid economic growth and lift millions of people out of poverty. However, university computer science education is misaligned with global market needs and fails to incorporate practice-based learning, leaving students unable to apply their skills in real-world contexts.</p>
+            <img src = "https://www.shutterstock.com/image-vector/picture-icon-260nw-413647060.jpg" height={"33px"} alt = "africa-map" />
+
+            <p>With few global tech companies on the continent, aspiring engineers don’t have access to experienced mentors, or the opportunity to work on products that operate at scale. Smart and ambitious students who could create life-changing technologies aren’t able to reach their potential.</p>
+
+            </div>
+
+            <div className='col img-col'>
+                <img src='/public/images/Drawing 1.jpg' height={"350px"} alt='africa-map' />
+            </div>
+        </div>
+    )
+}
+export default ProblemSection
