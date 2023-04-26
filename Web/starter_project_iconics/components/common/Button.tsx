386f93bf7f6b5f11dc4320b86e500a065db814e7
Zerihun Moges <zerihunmoges78@gmail.com>
2023-04-26 04:49:21 -0700

    change arbitrary color with theme

--- Web/starter_project_iconics/components/common/Button.tsx
+++ Web/starter_project_iconics/components/common/Button.tsx
@@ -30,7 +30,7 @@ function Button({ label,
   ...props }: ButtonProps){
   return (
 
-    <button {...props} className={clsx( outline? 'outline outline-2 outline-primary text-primary bg-white hover:bg-primary hover:text-white' : 'text-white bg-primary hover:bg-blue-600', 'flex justify-center items-center gap-x-3 px-8 py-3 m-2 text-md  rounded-md cursor-pointer', className)}>{startIcon && startIcon} {label} {endIcon && endIcon} </button>
+    <button {...props} className={clsx( outline? 'outline outline-2 outline-primary text-primary bg-secondary hover:bg-primary hover:text-secondary' : 'text-secondary bg-primary', 'flex justify-center items-center gap-x-3 px-8 py-3 m-2 text-md  rounded-md cursor-pointer', className)}>{startIcon && startIcon} {label} {endIcon && endIcon} </button>
 
   )
 }
