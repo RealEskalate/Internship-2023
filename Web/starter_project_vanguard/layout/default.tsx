d7b8dd00420cd2062d903f48d3192f67fb267a0e
Jebessa Dejene <89038777+jebessaD@users.noreply.github.com>
2023-04-26 22:22:09 +0300

    Implement footer component

--- Web/starter_project_vanguard/layout/default.tsx
+++ Web/starter_project_vanguard/layout/default.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { ReactNode, Suspense } from 'react'
 
 import Navbar from '@/components/layout/navbar'
+import Footer from '@/components/layout/Footer'
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
     <div>
       <Navbar/>
       <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
+      <Footer/>
     </div>
   )
 }
