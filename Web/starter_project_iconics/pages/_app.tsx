b3d432d55431136542ed01b0d335e46037ff4f54
aklile <aklileyilma@gmail.com>
2023-04-26 11:24:21 +0300

    add navbar layout

--- Web/starter_project_iconics/pages/_app.tsx
+++ Web/starter_project_iconics/pages/_app.tsx
@@ -1,10 +1,12 @@
 import Footer from '@/components/layout/Footer'
+import Navbar from '@/components/layout/Navbar'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Navbar />
       <Component {...pageProps} />
       <Footer />
     </>
