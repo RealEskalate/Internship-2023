9327a1a7ac16db86a621dca80fc9cfabcc047ddf
jealsab <jealsabb@gmail.com>
2023-04-26 08:19:38 +0300

    add search bar

--- Mobile/starter_project_dartsmiths/lib/features/home/presentation/screen/home_page.dart
+++ Mobile/starter_project_dartsmiths/lib/features/home/presentation/screen/home_page.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
+
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-//  color
+    Color _color =
+        Color(int.parse("#669AFF".substring(1, 7), radix: 16) + 0xFF000000);
+    Color _color3 =
+        Color(int.parse("#5E5F6F".substring(1, 7), radix: 16) + 0xFF000000);
+    Color _color2 =
+        Color(int.parse("#376AED".substring(1, 7), radix: 16) + 0xFF000000);
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
@@ -34,6 +40,15 @@ class HomePage extends StatelessWidget {
                     fontSize: 25,
                     fontWeight: FontWeight.w800))),
         actions: [
+          // Container(
+          //   child: Image.asset(
+          //     'assets/backgrd.jpg',
+          //     fit: BoxFit.cover,
+          //   ),
+          //   decoration: BoxDecoration(
+          //     borderRadius: BorderRadius.circular(300),
+          //   ),
+          // ),
           Padding(
               padding: EdgeInsets.only(right: 10, top: 5),
               child: Stack(
@@ -52,6 +67,7 @@ class HomePage extends StatelessWidget {
                                     Border.all(color: Colors.white, width: 2)),
                             width: 35,
                             height: 35,
+                            // /color: Color.fromARGB(255, 31, 30, 28),
                           )
                         ]),
                   ),
@@ -59,6 +75,77 @@ class HomePage extends StatelessWidget {
               )),
         ],
       ),
+      body: Container(
+        height: double.infinity,
+        child: Column(
+          children: [
+            Padding(
+              padding:
+                  EdgeInsets.only(left: 15, right: 15, top: 20, bottom: 10),
+              child: Container(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.only(left: 20),
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                            child: Text(
+                              "Search and article ..",
+                              style: TextStyle(
+                                  letterSpacing: 1,
+                                  fontFamily: "Poppins",
+                                  fontWeight: FontWeight.w100,
+                                  fontSize: 16,
+                                  color: Color(int.parse(
+                                          "#9A9494".substring(1, 7),
+                                          radix: 16) +
+                                      0xFF000000)),
+                            ),
+                            color: Colors.white,
+                          ),
+                          Container(
+                            decoration: BoxDecoration(
+                                color: _color,
+                                borderRadius: BorderRadius.circular(10)),
+                            width: 55,
+                            height: 60,
+                            child: IconButton(
+                                onPressed: () => {},
+                                icon: Icon(
+                                  size: 35,
+                                  Icons.search_sharp,
+                                  color: Colors.white,
+                                )),
+                          ),
+                        ]),
+                  ),
+                  width: double.infinity,
+                  height: 220,
+                ),
+                width: double.infinity,
+                height: 50,
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.3),
+                      spreadRadius: 5,
+                      blurRadius: 9,
+                      offset: Offset(0, 9),
+                    )
+                  ],
+                  borderRadius: BorderRadius.circular(10),
+                  // border: Border.all(color: Colors.black, width: 2),
+                ),
+              ),
+            ),
+        
+        ],
+      ),),
 
       // ],
       // ),
