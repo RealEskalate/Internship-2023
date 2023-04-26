6eafe9890ac66faa034609d366a911efc96ff575
jealsab <jealsabb@gmail.com>
2023-04-26 23:03:01 +0300

    add profile widget

---
+++ Mobile/starter_project_dartsmiths/lib/features/home/presentation/widgets/profile.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class MyProfile extends StatelessWidget {
+  const MyProfile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+              padding: EdgeInsets.only(right: 10, top: 5),
+              child: Stack(
+                children: [
+                  CircleAvatar(
+                      backgroundImage: AssetImage("assets/images/backgrd.jpg")),
+                  Padding(
+                    padding: EdgeInsets.only(left: 2.5, bottom: 12),
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(17),
+                                border:
+                                    Border.all(color: Colors.white, width: 2)),
+                            width: 35,
+                            height: 35,
+                          )
+                        ]),
+                  ),
+                ],
+              ));
+  }
+}
