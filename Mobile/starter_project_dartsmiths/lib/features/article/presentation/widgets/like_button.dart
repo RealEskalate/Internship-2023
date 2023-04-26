e207de9da5ff9f3a190466ff40182e78ccbe5b2e
Hiwot Derese <Hiwotderese16@gmail.com>
2023-04-26 16:20:05 +0300

    declare variables for ui input

--- Mobile/starter_project_dartsmiths/lib/features/article/presentation/widgets/like_button.dart
+++ Mobile/starter_project_dartsmiths/lib/features/article/presentation/widgets/like_button.dart
@@ -3,32 +3,36 @@
 import 'package:flutter/material.dart';
 import '../../../../core/utils/ui_converter.dart';
 
+String likes = '2.3k';
+
 class like_button extends StatelessWidget {
   const like_button({super.key});
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-        height: UIConverter.getComponentWidth(context, 22),
-        width: UIConverter.getComponentWidth(context, 50),
-        child: FloatingActionButton(
-            onPressed: () {},
-            shape: BeveledRectangleBorder(borderRadius: BorderRadius.circular(UIConverter.designWidth * 0.01)),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: const [
-                Icon(
-                  Icons.thumb_up_alt_outlined,
-                  color: Colors.white,
-                ),
-                Text(
-                  '2.3k',
-                  style: TextStyle(color: Colors.white),
-                ),
-              ],
-            ) //child widget inside this button
-        
-      ),
+      height: UIConverter.getComponentWidth(context, 22),
+      width: UIConverter.getComponentWidth(context, 50),
+      child: FloatingActionButton(
+          onPressed: () {},
+          shape: BeveledRectangleBorder(
+              borderRadius:
+                  BorderRadius.circular(UIConverter.designWidth * 0.01)),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              const Icon(
+                Icons.thumb_up_alt_outlined,
+                color: Colors.white,
+              ),
+              Text(
+                likes,
+                style: const TextStyle(color: Colors.white),
+              ),
+            ],
+          ) //child widget inside this button
+
+          ),
     );
   }
 }
