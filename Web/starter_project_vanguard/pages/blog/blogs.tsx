30bf0daf99926369b0aefe66f74ed4a6474ced68
Tseganesh Yifru <t5691yifru@gmail.com>
2023-04-26 19:45:46 +0300

    add pagination for blogs page

--- Web/starter_project_vanguard/pages/blog/blogs.tsx
+++ Web/starter_project_vanguard/pages/blog/blogs.tsx
@@ -2,20 +2,34 @@ import { map } from 'cypress/types/bluebird'
 import React from 'react'
 import Blog from '@/components/blog/blog'
 import Search from '@/components/blog/search'
-import Button  from '@/components/blog/addbutton'
 
-const blogs = [
-    <Blog/>,
-    <Blog />,
-    <Blog/>,
-    <Blog />,
-    <Blog/>,
-    <Blog />,
-]
+import { useState } from 'react';
+import Pagination from '@/components/blog/pagination';
+
+
+
+
+
+
+
 
 function Blogs() {
+
+  const blogs = [
+    <Blog/>,
+    
+  
+]
+const [currentPage, setCurrentPage] = useState(1);
+const [currentBlogs, setBlogs] = useState(blogs);
+
+const onPageChange = (page: number) => {
+  setCurrentPage(page);
+};
+
+
   return (
-    <div className='justify-center'>
+    <div className='justify-center min-h-screen'>
       
       <div className='h-80'>
       <Search />
@@ -24,9 +38,12 @@ function Blogs() {
       
      <div className='pt-4'>
      <ul>
-        {blogs.map((item) => (
-            <li>{item}</li>
-        ))}
+     {currentBlogs.map((blog) => (
+          <li className="text-2xl font-bold">{blog}</li>
+      ))}
+      <Pagination currentPage={currentPage} totalPages={Math.ceil(blogs.length / 2)} onPageChange={onPageChange} />
+
+        
       </ul>
      </div>
      
