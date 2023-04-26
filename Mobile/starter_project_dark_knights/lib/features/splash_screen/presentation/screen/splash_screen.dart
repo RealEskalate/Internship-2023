b4a55e4121b9b1f40af2e41f27b5caa8bd9fe418
Yared Tsegaye <yaredtsegaye120@gmail.com>
2023-04-26 10:43:27 +0300

    (Mobile-Dartknights): Add asset images and splash screen page

---
+++ Mobile/starter_project_dark_knights/lib/features/splash_screen/presentation/screen/splash_screen.dart
@@ -0,0 +1,24 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacementNamed('/main');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Image(image: AssetImage("assets/images/splash_screen.png"));
+  }
+}
