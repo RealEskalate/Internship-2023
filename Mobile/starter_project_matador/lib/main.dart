c9a7bbaf5d4e290f94b5538f90ae9657466fc646
Segni Dessalegn <segnides@gmail.com>
2023-04-26 13:41:11 +0300

    reorganize folders

--- Mobile/starter_project_matador/lib/main.dart
+++ Mobile/starter_project_matador/lib/main.dart
@@ -4,6 +4,7 @@ import 'features/feed/presentation/screen/home_page.dart';
 
 import 'features/login_page/Presentation/screen/login_page.dart';
 import 'features/example/presentation/screen/onboarding_1_screen.dart';
+import 'features/onboarding/presentation/screen/onboarding_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,9 +16,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Onboarding Screen 1',
+      title: 'Onboarding Screen',
       debugShowCheckedModeBanner: false,
-      home: OnBoardingScreen1(),
+      home: OnBoardingScreen(),
     );
   }
 }
