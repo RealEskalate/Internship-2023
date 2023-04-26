092632d3fbd7492a511c8b7fdf2f0994f78c176a
Zerihun Moges <91532718+zerihunMoges@users.noreply.github.com>
2023-04-26 05:52:46 -0700

    feat(web-iconics) : implement button component  (#23)
    * resolve conflict
    
    * add optional icon to button
    
    * increase default button size
    
    * remove changes in hero
    
    * add outline to button
    
    * change arbitrary color with theme
    
    * apply lint
    
    * remove package.lock
    
    * replace clsx with backtick
    
    ---------
    
    Co-authored-by: Zerihun Moges <zerihunmoges78@gmail.com>

--- Web/starter_project_iconics/components/common/Button.tsx
+++ Web/starter_project_iconics/components/common/Button.tsx
@@ -1,4 +1,3 @@
-import clsx from 'clsx'
 
 export interface ButtonProps extends React.ComponentProps<'button'> {
 
@@ -15,14 +14,23 @@ export interface ButtonProps extends React.ComponentProps<'button'> {
    */
   onClick?: () => void
 
+  // optional icon preceding text
+  endIcon?: JSX.Element
 
+  // optional trailing icon
+  startIcon?: JSX.Element
+
+  // add outline border
+  outline?: boolean
 }
 
-function Button({label,
-className,
-...props}: ButtonProps) {
+function Button({ label,
+  className, startIcon, endIcon, outline,
+  ...props }: ButtonProps){
   return (
-    <button {...props} className={clsx( 'px-4 py-2 m-2 text-white bg-[#264FAD] rounded-md cursor-pointer', className)}>{label}  </button>
+
+    <button {...props} className={`${outline? 'outline outline-2 outline-primary text-primary  bg-secondary hover:bg-primary hover:text-secondary' : 'text-secondary bg-primary'} flex justify-center items-center gap-x-3 px-8 py-3 m-2 text-md  rounded-md cursor-pointer ${className}`}>{startIcon && startIcon} {label} {endIcon && endIcon} </button>
+
   )
 }
 
