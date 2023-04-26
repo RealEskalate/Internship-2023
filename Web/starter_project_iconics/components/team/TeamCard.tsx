d37bf2e79ba35ad68992fbf52b1ffcaad95c6719
Zerihun Moges <zerihunmoges78@gmail.com>
2023-04-26 00:56:48 -0700

    use array for social media

--- Web/starter_project_iconics/components/team/TeamCard.tsx
+++ Web/starter_project_iconics/components/team/TeamCard.tsx
@@ -6,32 +6,27 @@ export interface TeamCardProps{
     instagram?: string
      linkedin?: string
      facebook?: string
-     avatar: string
+     avatar?: string
 }
 
-function TeamCard({name, job, description, avatar, instagram, linkedin, facebook}: TeamCardProps) {
+function TeamCard({name, job, description, avatar = 'https://www.dmarge.com/wp-content/uploads/2021/01/dwayne-the-rock-.jpg', instagram, linkedin, facebook}: TeamCardProps) {
+    const socials = [{link: facebook, image: '/img/teams-page/facebook.png' }, {link: instagram, image: '/img/teams-page/instagram.png' }, {link: linkedin, image: '/img/teams-page/linkedin.png' }]
     return (
         <div className='flex flex-col bg-white rounded-lg p-6 m-2 items-center justify-center shadow-xl max-w-sm'>
-            <img className='rounded-full w-32 h-32 mt-2 object-cover ' src={avatar}></img>
-            <h1 className="font-bold uppercase text-2xl m-3">{name}</h1>
-            <h2 className="uppercase text-xl">{job}</h2>
-            <p className="text-center my-4 text-xl text-[#7D7D7D]">{description}</p>
-            <hr className='my-6 w-[100%] '></hr>
+        <img className='rounded-full w-32 h-32 mt-2 object-cover ' src={avatar}></img>
+        <h1 className="font-bold uppercase text-black text-2xl m-3">{name}</h1>
+        <h2 className="uppercase text-black text-xl">{job}</h2>
+        <p className="text-center my-4 text-xl text-[#7D7D7D]">{description}</p>
+        <hr className='my-6 w-[100%] '></hr>
 
             <div className="flex justify-around w-[100%]">
                 
-                    <a href=""><img className='rounded-md object-contain w-8' src='/facebook.png'></img></a>
-              
-                <a href=''>
-                    <img className='rounded-md object-contain w-8' src='/instagram.png'></img>
-                </a>
-                <a href=''>
-                    <img className='rounded-md object-contain w-8' src='/linkedin.png'></img>
-                </a>
+                {socials.map((social) => <a href={social.link}><img className='rounded-md object-contain w-8' src={social.image}></img></a>)}
+                    
 
             </div>
         </div>
     )
 }
 
-export default TeamCard
+export default TeamCard 
