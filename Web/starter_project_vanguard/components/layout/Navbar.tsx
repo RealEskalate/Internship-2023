4233666456b879d01022f47f0b6f7de4f01ec5bf
Segni Desta <segnidesta60@gmail.com>
2023-04-27 22:21:07 +0300

    change font

--- Web/starter_project_vanguard/components/layout/Navbar.tsx
+++ Web/starter_project_vanguard/components/layout/Navbar.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link'
 import { useState } from 'react'
 import { AiOutlineMenu } from 'react-icons/ai'
 import logo from '../../public/img/a2sv-logo.png'
+import { navItems } from './data/nav-items'
 
 const Navbar: React.FC = () => {
   const [toggle, setToggle] = useState(false)
