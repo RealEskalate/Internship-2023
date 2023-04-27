2fb7bd67b5e4a44b6c370327bc0ca915c4c5d003
Yared <90713646+Yared04@users.noreply.github.com>
2023-04-27 16:08:21 +0300

    Yared.teams page responsiveness (#85)
    * feat(web-vanguard): implement the pagination component
    
    * made the items page responsive

--- Web/starter_project_vanguard/pages/teams/index.tsx
+++ Web/starter_project_vanguard/pages/teams/index.tsx
@@ -3,10 +3,13 @@ import HeroSection from '@/components/teams/HeroSection'
 import Members from '@/components/teams/Members'
 const index = () => {
   return (
-    <div className="mx-10">
+    <div className="lg:mx-10 mx-8">
       <HeroSection />
       <hr className="border mt-32 mb-16" />
       <Members />
+      <div className="my-14">
+      </div>
+      
     </div>
   )
 }
