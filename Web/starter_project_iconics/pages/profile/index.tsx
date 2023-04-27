09cf69a8f8c9dbae8d20d76a439d1f1e341ee2ca
Blen <62964622+blenassefa2@users.noreply.github.com>
2023-04-27 11:33:06 +0300

    feat(web-iconics): convert file formats

--- Web/starter_project_iconics/pages/profile/index.tsx
+++ Web/starter_project_iconics/pages/profile/index.tsx
@@ -1,12 +1,14 @@
 import ProfileLayout from '@/components/profile/profileLayout'
-import { cards } from '../../data/profile/blogs'
+import cards from '../../data/profile/blogs.json'
 import MyBlogs from '@/components/profile/myBlogs'
+import { Blog } from '@/types/profile/blog'
 
 const index = () => {
+  const result: Blog[] = JSON.parse(JSON.stringify(cards))
   return (
     <ProfileLayout
       buttonText="New Blog"
-      element={<MyBlogs cards={cards} />}
+      element={<MyBlogs cards={result} />}
       text="Manage Blogs"
       innerText="Edit, Delete and View the Status of your blogs"
     ></ProfileLayout>
