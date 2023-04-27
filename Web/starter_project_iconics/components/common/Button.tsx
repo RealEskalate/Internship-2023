3415a27872d330f23236256e75f0d1c264fdc2af
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2023-04-27 17:23:37 +0300

    deployment fixes (#97)
    deployment fixes

--- Web/starter_project_iconics/components/common/Button.tsx
+++ Web/starter_project_iconics/components/common/Button.tsx
@@ -1,6 +1,5 @@
-
+import { ReactNode } from 'react'
 export interface ButtonProps extends React.ComponentProps<'button'> {
-
   /**
    * Content of the button
    */
@@ -15,22 +14,34 @@ export interface ButtonProps extends React.ComponentProps<'button'> {
   onClick?: () => void
 
   // optional icon preceding text
-  endIcon?: JSX.Element
+  endIcon?: ReactNode
 
   // optional trailing icon
-  startIcon?: JSX.Element
+  startIcon?: ReactNode
 
   // add outline border
   outline?: boolean
 }
 
-function Button({ label,
-  className, startIcon, endIcon, outline,
-  ...props }: ButtonProps){
+function Button({
+  label,
+  className,
+  startIcon,
+  endIcon,
+  outline,
+  ...props
+}: ButtonProps) {
   return (
-
-    <button {...props} className={`${outline? 'outline outline-2 outline-primary text-primary  bg-secondary hover:bg-primary hover:text-secondary' : 'text-secondary bg-primary'} flex justify-center items-center gap-x-3 px-8 py-3 m-2 text-md  rounded-md cursor-pointer ${className}`}>{startIcon && startIcon} {label} {endIcon && endIcon} </button>
-
+    <button
+      {...props}
+      className={`${
+        outline
+          ? 'outline outline-2 outline-primary text-primary  bg-secondary hover:bg-primary hover:text-secondary'
+          : 'text-secondary bg-primary'
+      } flex justify-center items-center gap-x-3 px-8 py-3 m-2 text-md  rounded-md cursor-pointer ${className}`}
+    >
+      {startIcon && startIcon} {label} {endIcon && endIcon}{' '}
+    </button>
   )
 }
 
