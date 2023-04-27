931fd476c162e1f3520a49a0523ca57fcebee9fd
unknown <yaredt29@gmail.com>
2023-04-27 08:51:11 +0300

    make the pages functional

--- Web/starter_project_vanguard/components/common/pagination.tsx
+++ Web/starter_project_vanguard/components/common/pagination.tsx
@@ -13,18 +13,18 @@ const Pagination: React.FC<Props> = ({ currentPage, totalPages, onPageChange }) 
     Array.from({ length: end - start + 1 }, (_, i) => i + start);
 
   const pages = range(1, totalPages);
-  console.log(pages);
+
   return (
     <div className="flex justify-center mt-4">
       <nav className="block ">
-        <ul className="flex pl-0 rounded list-none flex-wrap mb-10">
+        <ul className="flex pl-0 rounded-md list-none flex-wrap mb-10">
           {pages.map((page) => (
             <li key={page}>
               <button
                 className={`${
                   currentPage === page
-                    ? 'bg-blue-800 text-white'
-                    : 'text-black hover:text-primary bg-gray-300'
+                    ? 'bg-primary text-white'
+                    : 'text-primary-text hover:text-primary bg-gray-300'
                 } font-bold py-2 px-4 border border-gray-300  rounded mx-2  mb-10`}
                 onClick={() => onPageChange(page)}
               >
