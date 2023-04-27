78db511cf0b86722c48d6d19e8e4c0117a8487dc
unknown <yaredt29@gmail.com>
2023-04-27 08:08:35 +0300

    made the items page responsive

--- Web/starter_project_vanguard/pages/teams/index.tsx
+++ Web/starter_project_vanguard/pages/teams/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import HeroSection from '@/components/teams/HeroSection'
 import Members from '@/components/teams/Members'
-import Pagination from '@/components/common/Pagination'
 const index = () => {
   return (
     <div className="lg:mx-10 mx-8">
@@ -9,7 +8,6 @@ const index = () => {
       <hr className="border mt-32 mb-16" />
       <Members />
       <div className="my-14">
-        <Pagination/>
       </div>
       
     </div>
