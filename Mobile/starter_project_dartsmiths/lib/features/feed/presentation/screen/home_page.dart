b4d8f90a81af4792b866d5917cac305411fe44f5
jealsab <jealsabb@gmail.com>
2023-04-29 09:30:43 +0300

    Completed home page

--- Mobile/starter_project_dartsmiths/lib/features/feed/presentation/screen/home_page.dart
+++ Mobile/starter_project_dartsmiths/lib/features/feed/presentation/screen/home_page.dart
@@ -15,54 +15,75 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-        appBar: AppBar(
-          elevation: 0,
-          backgroundColor: whiteColor,
-          leading: Padding(
-              padding: EdgeInsets.all(15),
-              child: Container(
-                  height: UIConverter.getComponentHeight(context, 35),
-                  width: UIConverter.getComponentWidth(context, 35),
-                  child: SvgPicture.asset("images/menu_bar.svg"))),
-          title: Center(
-              child: Text("Welcome  Back!",
-                  style: myTextStyle.copyWith(
-                      color: blackColor,
-                      fontSize: 25,
-                      fontWeight: FontWeight.w800))),
-          actions: [ProfilePic()],
-        ),
-        body: Container(
-            height: double.infinity,
-            child: Column(children: [
-              Searchbar(),
-              Padding(
-                  padding: EdgeInsets.only(
-                    top: UIConverter.getComponentHeight(context, 20),
-                    right: UIConverter.getComponentWidth(context, 25),
-                    left: UIConverter.getComponentWidth(context, 25),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: whiteColor,
+        leading: Padding(
+            padding: EdgeInsets.only(
+              bottom: UIConverter.getComponentHeight(context, 15),
+              top: UIConverter.getComponentHeight(context, 15),
+              right: UIConverter.getComponentWidth(context, 15),
+              left: UIConverter.getComponentWidth(context, 15),
+            ),
+            child: Container(
+                height: UIConverter.getComponentHeight(context, 35),
+                width: UIConverter.getComponentWidth(context, 35),
+                child: SvgPicture.asset("images/menu_bar.svg"))),
+        title: Center(
+            child: Text("Welcome  Back!",
+                style: myTextStyle.copyWith(
+                    color: blackColor,
+                    fontSize: 25,
+                    fontWeight: FontWeight.w800))),
+        actions: [ProfilePic()],
+      ),
+      body: Container(
+        height: double.infinity,
+        child: Column(
+          children: [
+            Searchbar(),
+            Padding(
+                padding: EdgeInsets.only(
+                  top: UIConverter.getComponentHeight(context, 20),
+                  right: UIConverter.getComponentWidth(context, 25),
+                  left: UIConverter.getComponentWidth(context, 25),
+                ),
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      FilterButton(
+                        text: "All",
+                        isActive: true,
+                      ),
+                      FilterButton(
+                        text: "Sports",
+                        isActive: false,
+                      ),
+                      FilterButton(
+                        text: "Tech",
+                        isActive: false,
+                      ),
+                      FilterButton(
+                        text: "Politics",
+                        isActive: false,
+                      )
+                    ])),
+            Expanded(
+              child: ListView(
+                children: [
+                  ArticleCard(),
+                  ArticleCard(),
+                  SizedBox(
+                    height: UIConverter.getComponentHeight(context, 70),
                   ),
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        FilterButton(
-                          text: "All",
-                          isActive: true,
-                        ),
-                        FilterButton(
-                          text: "Sports",
-                          isActive: false,
-                        ),
-                        FilterButton(
-                          text: "Tech",
-                          isActive: false,
-                        ),
-                        FilterButton(
-                          text: "Politics",
-                          isActive: false,
-                        )
-                      ])),
-            ])));
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: AddButton(),
+    );
   }
 }
