3daf0ddffa7f7dadb4de19a3a4d12cc729fbcfef
Daniel Tefera Deti <danieltefera2211@gmail.com>
2023-05-01 15:41:27 +0300

    feat(Backend.DaniT-Folder-Structure-Setup): Folder structure setup for team Maverick completed.

---
+++ Backend/starter_project_mavericks/BlogApp/BlogApp.Application/Profiles/MappingProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using BlogApp.Application.Features._Indices.DTOs;
+using BlogApp.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogApp.Application.Profiles
+{
+    public class MappingProfile: Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<_Index, _IndexDto>().ReverseMap();
+            CreateMap<_Index, Create_IndexDto>().ReverseMap();
+            #region _index Mappings
+
+            CreateMap<_Index, _IndexDto>().ReverseMap();
+            CreateMap<_Index, Create_IndexDto>().ReverseMap();
+
+            #endregion _index
+        }
+    }
+}
