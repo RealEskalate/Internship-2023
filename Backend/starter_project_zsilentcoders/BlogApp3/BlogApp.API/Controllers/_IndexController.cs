451466aff6ebc31021de81a6e826e9689c6a8a9b
Daniel Tefera Deti <danieltefera2211@gmail.com>
2023-05-01 23:23:23 +0300

    feat(Backend.DaniT-Folder-Structure-Setup): implement basic folder structure for zsilentcoders group

---
+++ Backend/starter_project_zsilentcoders/BlogApp3/BlogApp.API/Controllers/_IndexController.cs
@@ -0,0 +1,61 @@
+﻿using BlogApp.Application.Features._Indices.CQRS.Commands;
+using BlogApp.Application.Features._Indices.CQRS.Queries;
+using BlogApp.Application.Features._Indices.DTOs;
+using BlogApp.Application.Responses;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BlogApp.Api.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class _IndexController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public _IndexController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<_IndexDto>>> Get()
+        {
+            var indices = await _mediator.Send(new Get_IndexListQuery());
+            return Ok(indices);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<_IndexDto>> Get(int id)
+        {
+            var indices = await _mediator.Send(new Get_IndexDetailQuery { Id = id });
+            return Ok(indices);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<BaseCommandResponse>> Post([FromBody] Create_IndexDto create_IndexDto)
+        {
+            var command = new Create_IndexCommand { _IndexDto = create_IndexDto };
+            var repsonse = await _mediator.Send(command);
+            return Ok(repsonse);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Put([FromBody] _IndexDto indexDto)
+        {
+            var command = new Update_IndexCommand { _IndexDto = indexDto };
+            await _mediator.Send(command);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var command = new Delete_IndexCommand { Id = id };
+            await _mediator.Send(command);
+            return NoContent();
+        }
+    }
+}
---
+++ Backend/starter_project_zsilentcoders/BlogApp3/BlogApp.Application/Contracts/Persistence/I_IndexRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlogApp.Domain;
+
+namespace BlogApp.Application.Contracts.Persistence
+{
+    public interface I_IndexRepository : IGenericRepository<_Index>
+    {
+    }
+}
