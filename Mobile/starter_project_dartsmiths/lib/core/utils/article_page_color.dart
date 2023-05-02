d13ef1d921b251de1aefe05eac7bb5e6f8e39dd7
Tamirat Dereje <tamiratdereje167@gmail.com>
2023-05-02 11:00:57 +0300

    fix changes

---
+++ Mobile/starter_project_dartsmiths/lib/core/utils/article_page_color.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+class BlogColorConstant {
+  static  Color headerTitle = const  Color(0XFF0D253C);
+  static  Color backIconColor = const Color(0XFF878585);
+  static  Color backIconBgColor = const Color(0XFFEAEBF0);
+  static  Color paceHolderColor  = const Color(0XFF979EA5);
+  static  Color borderLineColor = const Color(0X7B8BB242);
+  static  Color buttonTextColor = const Color(0XFFFFFFFF);
+  static  Color buttonBgColor = const Color(0XFF376AED);
+  static  Color contentBgColor = const Color(0XFFD7DDEB);
+  static  Color addIcondColor = const Color(0XFF376AED);
+  static  Color chipsBgColor = const Color(0XFF376AED);
+
+}
+
