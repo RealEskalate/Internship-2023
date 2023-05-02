6cacc62348575ee03c91279d08cae5940f8385ac
Daniel Tefera Deti <danieltefera2211@gmail.com>
2023-05-02 11:21:10 +0300

    Folder structure final setup.

---
+++ Backend/starter_project_mavericks/BlogApp/BlogApp.Application/Features/_Indices/DTOs/Validators/I_IndexDtoValidator.cs
@@ -0,0 +1,21 @@
+﻿using BlogApp.Application.Features._Indices.DTOs;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogApp.Application.Features._Indices.DTOs.Validators
+{
+    public class I_IndexDtoValidator : AbstractValidator<I_IndexDto>
+    {
+        public I_IndexDtoValidator()
+        {
+            RuleFor(p => p.Name)
+                .NotEmpty().WithMessage("{PropertyName} is required.")
+                .NotNull()
+                .MaximumLength(50).WithMessage("{PropertyName} must not exceed {ComparisonValue} characters.");
+        }
+    }
+}
---
+++ Backend/starter_project_zsilentcoders/BlogApp3/BlogApp.Application/Features/_Indices/CQRS/Queries/Get_IndexListQuery.cs
@@ -0,0 +1,14 @@
+﻿using BlogApp.Application.Features._Indices.DTOs;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogApp.Application.Features._Indices.CQRS.Queries
+{
+    public class Get_IndexListQuery: IRequest<List<_IndexDto>>
+    {
+    }
+}
