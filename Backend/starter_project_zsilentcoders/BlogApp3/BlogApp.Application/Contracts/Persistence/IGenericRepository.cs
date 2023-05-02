e310123cbd2764470af7f558c36fc0012ced2d55
danielteferadeti <56646941+danielteferadeti@users.noreply.github.com>
2023-05-02 17:36:50 +0300

    feat(Backend.DaniT-Folder-Structure-Setup): Implement folder structure setup (#108)
    * feat(Backend.DaniT-Folder-Structure-Setup): Folder structure setup
    
    * feat(Backend.DaniT-Folder-Structure-Setup): setup gitignore
    
    * feat(Backend.DaniT-Folder-Structure-Setup): setup git ignore

---
+++ Backend/starter_project_zsilentcoders/BlogApp3/BlogApp.Application/Contracts/Persistence/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogApp.Application.Contracts.Persistence
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> Get(int id);
+        Task<IReadOnlyList<T>> GetAll();
+        Task<T> Add(T entity);
+        Task<bool> Exists(int id);
+        Task Update(T entity);
+        Task Delete(T entity);
+    }
+}
