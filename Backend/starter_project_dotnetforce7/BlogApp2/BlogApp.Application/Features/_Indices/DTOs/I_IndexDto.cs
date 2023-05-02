d8effe1911b4eab9ef534851c8e450c25b4b5769
Daniel Tefera Deti <danieltefera2211@gmail.com>
2023-05-02 14:17:53 +0300

    feat(Backend.DaniT-Folder-Structure-Setup): Folder structure setup

---
+++ Backend/starter_project_dotnetforce7/BlogApp2/BlogApp.Application/Features/_Indices/DTOs/I_IndexDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogApp.Application.Features._Indices.DTOs
+{
+    public interface I_IndexDto
+    {
+        public string Name { get; set; }
+    }
+}
