2accd7d8b13234f0ad67914722d2af82c8d5864b
Daniel Tefera Deti <danieltefera2211@gmail.com>
2023-05-02 11:21:10 +0300

    Folder structure final setup.

---
+++ Backend/starter_project_mavericks/BlogApp/BlogApp.Application/Features/_Indices/CQRS/Commands/Delete_IndexCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogApp.Application.Features._Indices.CQRS.Commands
+{
+    public class Delete_IndexCommand : IRequest
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Backend/starter_project_mavericks/BlogApp/BlogApp.Persistence/Repositories/GenericRepository.cs
@@ -0,0 +1,54 @@
+﻿using BlogApp.Application.Contracts.Persistence;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogApp.Persistence.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly BlogAppDbContext _dbContext;
+
+        public GenericRepository(BlogAppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<T> Add(T entity)
+        {
+            await _dbContext.AddAsync(entity);
+            return entity;
+        }
+
+        public async Task Delete(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+        }
+
+        public async Task<bool> Exists(int id)
+        {
+            var entity = await Get(id);
+            return entity != null;
+        }
+
+        public async Task<T> Get(int id)
+        {
+            return await _dbContext.Set<T>().FindAsync(id);
+        }
+
+        public async Task<IReadOnlyList<T>> GetAll()
+        {
+            return await _dbContext.Set<T>().AsNoTracking().ToListAsync();
+        }
+
+        public async Task Update(T entity)
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+        }
+
+
+    }
+}
