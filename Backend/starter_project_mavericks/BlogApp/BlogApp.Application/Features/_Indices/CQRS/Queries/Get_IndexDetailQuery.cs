67abb325927444a1f3245dd9e192de27902d360e
danielteferadeti <56646941+danielteferadeti@users.noreply.github.com>
2023-05-02 17:36:50 +0300

    feat(Backend.DaniT-Folder-Structure-Setup): Implement folder structure setup (#108)
    * feat(Backend.DaniT-Folder-Structure-Setup): Folder structure setup
    
    * feat(Backend.DaniT-Folder-Structure-Setup): setup gitignore
    
    * feat(Backend.DaniT-Folder-Structure-Setup): setup git ignore

---
+++ Backend/starter_project_mavericks/BlogApp/BlogApp.Application/Features/_Indices/CQRS/Queries/Get_IndexDetailQuery.cs
@@ -0,0 +1,15 @@
+﻿using BlogApp.Application.Features._Indices.DTOs;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogApp.Application.Features._Indices.CQRS.Queries
+{
+    public class Get_IndexDetailQuery : IRequest<_IndexDto>
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Backend/starter_project_zsilentcoders/BlogApp3/BlogApp.Application/Features/_Indices/CQRS/Handlers/Get_IndexListQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using BlogApp.Application.Contracts.Persistence;
+using BlogApp.Application.Features._Indices.CQRS.Queries;
+using BlogApp.Application.Features._Indices.DTOs;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogApp.Application.Features._Indices.CQRS.Handlers
+{
+    public class Get_IndexListQueryHandler : IRequestHandler<Get_IndexListQuery, List<_IndexDto>>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public Get_IndexListQueryHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<List<_IndexDto>> Handle(Get_IndexListQuery request, CancellationToken cancellationToken)
+        {
+            var _Indices = await _unitOfWork._IndexRepository.GetAll();
+            Console.Write(_Indices);
+            return _mapper.Map<List<_IndexDto>>(_Indices);
+        }
+    }
+}
