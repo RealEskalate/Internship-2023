537b3a65749f52189e3d38f92b0fbd23f7ba527f
Brook Alemu <brookwworku3@gmail.com>
2023-05-02 05:01:41 -0900

    Implement about_us

--- Web/starter_project_iconics/components/about/SocialProjectsSections.tsx
+++ Web/starter_project_iconics/components/about/SocialProjectsSections.tsx
@@ -1,30 +1,30 @@
 
-
+import Image from "next/image"
 const SocialProjectsSections = () => {
   return (
     <>
-<h1 className="social-section-title"> Social &nbsp;<div className="blue-word">Projects</div></h1>
-       
+<h1 className="social-section-title p-12 items-center justify-centerexit
+ lg"> <div className="font-poppins text-6xl"> Social <span className = "text-blue-800">Projects </span></div></h1>
 
-<div className="social-project-section">
+<div className="social-project-section grid grid-cols-2 p-12 lg">
    <div className="col img-col">
-   <img src = "/public/images/Rectangle 4(2).png" height={"400px"} width={"800px"}/>
+   <Image src = "/about/img/Rectangle 4(2).png" alt = "Rectangle 4" height={400} width={800} />
 </div>
 
 <div className="col right">
-   <h3> <div className="blue-word">Social Project</div></h3>
-   <h2> <div className="blue-word"> Hakim Hub</div></h2>
-   <p>HakimHub is a platform that provides information about healthcare facilities and healthcare professionals in Ethiopia. Hakimhub makes information about hospitals, medical laboratories, and doctors conveniently accessible to its users.</p>
+   <h3> <div className="font-poppins text-1xl text-blue-800 text-sm text-right pl-12 lg">Social Project</div></h3>
+   <h2> <div className="font-poppins text-3xl text-blue-800 text-right pl-12	"> Hakim Hub</div></h2>
+   <p className="font-poppins font-light text-2xl">HakimHub is a platform that provides information about healthcare facilities and healthcare professionals in Ethiopia. Hakimhub makes information about hospitals, medical laboratories, and doctors conveniently accessible to its users.</p>
 </div>
 <div className="col left">
 
-   <h3> <div className="blue-word">Social Project</div></h3>
-   <h2> <div className="blue-word">Track Sym</div></h2>
-   <p>TrackSym is a non-commercial app that uses crowd-sourcing to collect and visualize the density of the relevant Covid-19 symptoms. Symptom data, aggregated by places, can help people avoid visiting areas that are heavily populated by symptomatic people.</p>
+   <h3> <div className="font-poppins text-1xl text-blue-800 text-sm lg">Social Project</div></h3>
+   <h2> <div className="font-poppins text-3xl text-blue-800">Track Sym</div></h2>
+   <p className="font-poppins font-light text-2xl">TrackSym is a non-commercial app that uses crowd-sourcing to collect and visualize the density of the relevant Covid-19 symptoms. Symptom data, aggregated by places, can help people avoid visiting areas that are heavily populated by symptomatic people.</p>
 </div>
 
-   <div className="col img-col">
-   <img src = "/public/images/Rectangle 5(2).png" height={"400px"} width={"800px"}  />
+   <div className="col img-col lg">
+   <Image src = "/about/img/Rectangle 5(2).png" alt = "Rectangle 5" height={400} width={800}  />
 
 </div>
 </div>
