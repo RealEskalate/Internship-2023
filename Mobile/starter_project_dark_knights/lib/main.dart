513ab3277316605fda7868172bfbad12e61b8fe3
Yared <83911480+Yared-betsega@users.noreply.github.com>
2023-05-02 11:11:04 +0300

    (Mobile-Dartknights): Implement the onboarding screen (#96)
    * (Mobile-Dartknights): duplicate the example folder and add starter files
    
    * fix(Mobile-Dartknights): add onboarding screen page
    
    * add(Mobile-Dartknights): Add widgets for onboarding screen
    
    * (Mobile-Dartknights): Add separate widgets for the onboarding screen
    
    * (Mobile-Dartknights): Add images and fonts to assets
    
    * (Mobile-Dartknights): Add images and fonts to assets
    
    * fix(Mobile-Dartknights): update height and width to MediaQuery
    
    * fix(Mobile-Dartknights): Update round border size into MediaQuery
    
    * fix(Mobile-Dartknights): resolve PR reviews

--- Mobile/starter_project_dark_knights/lib/main.dart
+++ Mobile/starter_project_dark_knights/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:dark_knights/features/signup/presentation/screen/signup_page.dart';
+import 'package:dark_knights/features/onboarding/presentation/screen/onboarding_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -25,7 +26,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const OnboardingScreen(),
     );
   }
 }
