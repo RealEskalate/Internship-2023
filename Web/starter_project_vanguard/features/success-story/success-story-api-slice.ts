f2c68f41f9e30811acce8f77c83dd21e17336a0d
Endale Yohannes <endaleyohannes8@gmail.com>
2023-05-02 10:17:55 +0300

    custom 404 page

--- Web/starter_project_vanguard/features/success-story/success-story-api-slice.ts
+++ Web/starter_project_vanguard/features/success-story/success-story-api-slice.ts
@@ -1,10 +1,11 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { SuccessStory } from '../../types/success-story'
 
+const url = 'http://localhost:3004'
 export const apiSlice = createApi({
   reducerPath: 'success-story/api',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:3004',
+    baseUrl: url,
   }),
   endpoints(builder) {
     return {
