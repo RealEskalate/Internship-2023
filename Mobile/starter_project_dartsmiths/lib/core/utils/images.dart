ec0d48f6557d48ef9676bad9130718cc81e13793
lealemgetahun <56633085+lealemgetahun@users.noreply.github.com>
2023-05-02 14:19:53 +0300

    (feat-Dartsmiths): add login page (#73)
    * implement login_card
    
    * update column widget alignment
    
    * implement login card
    
    * update alignment of login card texts
    
    * update naming of login card class
    
    * update colors in login card and login page
    
    * update text called show
    
    * (fix) login page and login card, create custom login text widget, add show functionality
    
    * (fix) main.dart file changes reverted
    
    * update login page a2sv logo
    
    ---------
    
    Co-authored-by: bereket <bereketdemissie4382@gmail.com>

--- Mobile/starter_project_dartsmiths/lib/core/utils/images.dart
+++ Mobile/starter_project_dartsmiths/lib/core/utils/images.dart
@@ -4,3 +4,4 @@ const String onBoardingPhoto3 = 'assets/images/onboarding_photo_3.png';
 const String onBoardingPhoto4 = 'assets/images/onboarding_photo_4.png';
 const String articleReadingFasionImage1 = "images/fashion1.jpg";
 const String articleReadingFasionImage3 = "images/fashin3.jpg";
+const String A2SVLogo = 'assets/images/a2sv_logo.jpg';
