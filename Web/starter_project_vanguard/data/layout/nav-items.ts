957944644af39a03847a8754ad61880177cdf104
Segni Desta <segnidesta60@gmail.com>
2023-05-02 11:47:32 +0300

    fix a conflict

--- Web/starter_project_vanguard/data/layout/nav-items.ts
+++ Web/starter_project_vanguard/data/layout/nav-items.ts
@@ -3,7 +3,7 @@ import { NavItem } from '@/types/nav'
 export const navItems: NavItem[] = [
   { name: 'Home', href: '/' },
   { name: 'Teams', href: '/teams' },
-  { name: 'Success Stories', href: '/successs-story' },
+  { name: 'Success Stories', href: '/success-story' },
   { name: 'About Us', href: '/about' },
   { name: 'Get Involved', href: '/get-involved' },
 ]
