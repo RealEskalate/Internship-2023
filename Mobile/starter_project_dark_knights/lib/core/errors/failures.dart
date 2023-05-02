ba0f3ff4ce05102b76f138c5e4d8880b1e8a8d5d
hayattofik-01 <hayattofik22@gmail.com>
2023-05-02 15:10:55 +0300

    add blue card color

--- Mobile/starter_project_dark_knights/lib/core/errors/failures.dart
+++ Mobile/starter_project_dark_knights/lib/core/errors/failures.dart
@@ -1,9 +1,5 @@
 import 'package:equatable/equatable.dart';
 
-abstract class Failure extends Equatable {
-  Failure([List properties = const <dynamic>[]]) : super();
-
-  @override
-  // TODO: implement props
-  List<Object> get props => [];
+abstract class Failure extends Equatable{
+ Failure([List properties = const<dynamic>[]]) : super();
 }
