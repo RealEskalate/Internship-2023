a248bf4b9458424fa4a5a742e5e093a65c4f94ba
Daniel Tefera Deti <danieltefera2211@gmail.com>
2023-05-02 14:17:53 +0300

    feat(Backend.DaniT-Folder-Structure-Setup): Folder structure setup

---
+++ Backend/starter_project_mavericks/BlogApp/BlogApp.Persistence/BlogAppDbContextFactory.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace BlogApp.Persistence
+{
+    public class BlogAppDbContextFactory : IDesignTimeDbContextFactory<BlogAppDbContext>
+    {
+        public BlogAppDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                 .SetBasePath(Directory.GetCurrentDirectory())
+                 .AddJsonFile("appsettings.json")
+                 .Build();
+
+            var builder = new DbContextOptionsBuilder<BlogAppDbContext>();
+            var connectionString = configuration.GetConnectionString("BlogAppConnectionString");
+
+            builder.UseNpgsql(connectionString);
+
+            return new BlogAppDbContext(builder.Options);
+        }
+    }
+}
