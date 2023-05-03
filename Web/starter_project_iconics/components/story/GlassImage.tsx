40ab4c77adeee60c766bfc0fe9cd658b5bb46c92
FevenBelay23 <91492132+FevenBelay23@users.noreply.github.com>
2023-05-03 15:46:11 +0300

    use next/image (#119)

--- Web/starter_project_iconics/components/story/GlassImage.tsx
+++ Web/starter_project_iconics/components/story/GlassImage.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from 'next/image'
 
  interface GlassImageProps  {
   image: string,
@@ -10,7 +11,7 @@ import React from 'react'
 const GlassImage: React.FC<GlassImageProps> = ({ image, name, job, location }) => {
   return (
     <div className="relative inline-block sm:max-w-md md:max-w-md lg:w-auto">
-      <img src={`/img/success-stories-images/people/${image}`} alt="image"  />
+      <Image  width={442} height={662}  src={`/img/success-stories-images/people/${image}`} alt="image"/>
       <div className="font-poppins text-gray-50 absolute bottom-0 left-0 right-0 backdrop-filter backdrop-blur-md rounded-xl w-full z-10">
         <div className="lg:m-4 lg:py-2">
           <p className="lg:pt-4 px-4  text-2xl font-bold">{name}</p>
