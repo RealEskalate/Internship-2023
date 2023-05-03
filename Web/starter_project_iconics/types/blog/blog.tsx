d76db2f6b22540b9e43424a070f61e9840e1d2db
Aklile-Yilma <91611223+Aklile-Yilma@users.noreply.github.com>
2023-05-03 14:20:18 +0300

    feat(web-iconics): implement blog page (#51)
    * implement blog page

---
+++ Web/starter_project_iconics/types/blog/blog.tsx
@@ -0,0 +1,10 @@
+export interface BlogData {
+  name: string
+  profileImg: string
+  profession: string
+  datePosted: string
+  title: String
+  description: string
+  tags: string[]
+  blogImg: string
+}
