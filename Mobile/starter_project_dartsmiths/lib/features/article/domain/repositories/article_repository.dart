6917e6eab979ae05e64b5b023d3acbdce7c77892
Hiwot Derese <Hiwotderese16@gmail.com>
2023-05-03 13:59:28 +0300

    delete entities

--- Mobile/starter_project_dartsmiths/lib/features/article/domain/repositories/article_repository.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dartz/dartz.dart';
-import '../../../../core/error/failures.dart';
-import '../entities/article.dart';
-
-abstract class ArticleRepository {
-  Future<Either<Failure, Article>> getArticle(Article article);
-  // Future<Either<Failure, Article>> getArticle(Article article);
-}
