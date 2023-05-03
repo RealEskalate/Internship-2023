64ee7a46908475bdf19d61bd146148cf552fa6a3
Ekram Kedir <102755583+ekramkedir2020@users.noreply.github.com>
2023-05-03 14:39:15 +0300

    ekram.story-page (#76)
    * implement story page
    
    * implement story page
    
    * Implement story-page
    
    * Implement Partners-section
    
    * Implement successStory-section
    
    * Implement Title-section
    
    * Include dataSets
    
    * Clean-up files
    
    * update Success-Story Card
    
    * update Partners section
    
    * Add Partners-data
    
    * Remove un-necessary imports

---
+++ Web/starter_project_barbies/pages/story/index.tsx
@@ -0,0 +1,16 @@
+import Title from '../../components/story/Title'
+import  Profile  from '../../components/story/SuccessStory'
+import Partners from '../../components/story/Partners'
+
+
+
+export default function Home() {
+  return (
+    <div>
+      <Title />
+      <Profile />
+      <Partners />
+    </div>
+    
+  )
+}
