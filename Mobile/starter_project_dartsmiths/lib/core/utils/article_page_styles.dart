2369b830d6c2fbc18c12c13a5c2da315c8f4295c
Tamirat Dereje <91497780+tamiratdereje@users.noreply.github.com>
2023-05-03 11:49:58 +0300

    article update and create ui  (#111)
    * article update and create ui 
    
    * fix review changes for components
    
    * Create custom text field

---
+++ Mobile/starter_project_dartsmiths/lib/core/utils/article_page_styles.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+
+TextTheme postArticleTheme = const TextTheme(
+    // title fonts
+    displayLarge: TextStyle(
+      fontSize: 24,
+      // fontFamily: 'Poppins',
+      color: Color(0XFF0D253C)
+    ),
+
+    // place holder font
+    bodyLarge:  TextStyle(
+      fontSize: 19,
+      // fontFamily: 'Poppins',
+      color: Color(0XFF979EA5)
+    ),
+
+    // button font
+    bodyMedium: TextStyle(
+      fontSize: 17,
+      // fontFamily: 'Poppins',
+      color: Color(0XFFFFFFFF)
+    ),
+
+    // small watermarked words font
+    bodySmall: TextStyle(
+      fontSize: 12,
+      fontWeight: FontWeight.w500,
+      // fontFamily: 'Poppins',
+      color: Color(0XFF979EA5)
+    )
+
+    );
+ 
+  
