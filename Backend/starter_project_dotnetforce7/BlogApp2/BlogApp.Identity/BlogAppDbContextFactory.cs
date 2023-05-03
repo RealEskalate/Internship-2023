a77ff8fd1823130405d4ac4986c5177bc1b8ff72
Bruk Tedla <biruk.tedla93@gmail.com>
2023-05-03 15:05:06 +0300

    feat(backend): add migrations

--- Backend/starter_project_dotnetforce7/BlogApp2/BlogApp.Identity/BlogAppDbContextFactory.cs
+++ Backend/starter_project_dotnetforce7/BlogApp2/BlogApp.Identity/BlogAppDbContextFactory.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using System;
