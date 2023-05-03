41be0ed426bec5d6ebff68aec703b3ebb08e5821
Henok Matheas Kebede <82048614+Henok-Matheas@users.noreply.github.com>
2023-05-03 14:58:11 +0300

    feat(web-iconics): implement build better section for the homepage (#74)
    * feat(web-iconics): Implement build-better section of homepage, change hero section to landing section
    
    * fix(web-iconics) change the background to white

--- Web/starter_project_iconics/pages/index.tsx
+++ Web/starter_project_iconics/pages/index.tsx
@@ -1,10 +1,11 @@
-import Hero from '@/components/home-page/Hero'
+import BuildBetter from '@/components/home/BuildBetter'
+import Landing from '@/components/home/Landing'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col justify-between">
-      <Hero />
-
-       </main>
+    <main className="bg-white flex min-h-screen flex-col justify-between">
+      <Landing />
+      <BuildBetter />
+    </main>
   )
 }
