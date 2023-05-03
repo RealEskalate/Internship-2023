cdb0cdf55411ee2fa04a020a34e42a4bb821274c
Bruk Tedla <biruk.tedla93@gmail.com>
2023-05-03 15:05:06 +0300

    feat(backend): add migrations

---
+++ Backend/starter_project_dotnetforce7/BlogApp2/BlogApp.Identity/BlogAppDbContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace BlogApp.Identity
+{
+    public class BlogAppDbContextFactory : IDesignTimeDbContextFactory<BlogIdentityDbContext>
+    {
+
+        public BlogIdentityDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configurationRoot = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory() + "/../BlogApp.API")
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<BlogIdentityDbContext>();
+            var connectionString = configurationRoot.GetConnectionString("BlogAppConnectionString");
+
+            builder.UseNpgsql(connectionString);
+            return new BlogIdentityDbContext(builder.Options);
+        }
+    }
+}
+
