7c036cc51ab1cbbd43e882abd37ed1e90ae077e6
unknown <yaredt29@gmail.com>
2023-05-03 10:13:52 +0300

    make the some changes on the hero of teams page

--- Web/starter_project_vanguard/components/teams/HeroSection.tsx
+++ Web/starter_project_vanguard/components/teams/HeroSection.tsx
@@ -35,7 +35,7 @@ const HeroSection: React.FC = () => {
       <div className="relative hidden 2lg:flex basis-3/5 p-40 flex-col gap-4 text-primary-text">
         <div className="absolute top-4 -z-10">
           <Image
-            className=' w-[530px]'
+            className=' w-[490px] xl:w-[530px]'
             src="/img/teams/hero-section/hero-background.png"
             width={550}
             height={500}
