58c771777b688f24901a76d118b966729334d7e6
Joshua Tesfaye <87969938+EyasuTesfu@users.noreply.github.com>
2023-05-03 11:33:56 +0300

    feat(mobile.matador): Add Login Page (#80)
    * fix(mobile.matador.joshua): fix comments
    
    * fix(mobile:matador.joshua): fix UI comments
    
    * fix(matador.joshua): remove global variables

--- Mobile/starter_project_matador/lib/main.dart
+++ Mobile/starter_project_matador/lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'features/onboarding/presentation/screen/onboarding_screen.dart';
 import 'features/feed/presentation/screen/home_page.dart';
 
+import 'features/login_page/Presentation/screen/login_page.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -12,17 +14,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Login Page',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const HomePage(),
