85fa82040628953318d60c2d99b2daac0f66375d
bereket <bereketdemissie4382@gmail.com>
2023-05-03 14:38:13 +0300

    update naming

--- Mobile/starter_project_dartsmiths/lib/features/authentication/domain/entity/authentication_payload.dart
+++ Mobile/starter_project_dartsmiths/lib/features/authentication/domain/entity/authentication_payload.dart
@@ -1,10 +1,10 @@
 import 'package:equatable/equatable.dart';
 
-class User extends Equatable {
+class UserAuthCredential extends Equatable {
   final String username;
   final String password;
 
-  const User({required this.username, required this.password});
+  const UserAuthCredential({required this.username, required this.password});
   @override
   List<Object> get props => [username, password];
 }
