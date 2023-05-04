b6673f119c9fb4f1ac467739dd4a54a2216d6c35
Gadisa Amenu <86168126+gadisamenu@users.noreply.github.com>
2023-05-04 15:16:24 +0300

    feat(gadisa.rate-crud): implement rate crud (#125)
    * feat(gadisa.rate-crud): implement rate crud
    
    * fix(gadisa.rete-crud):fix handlers  and test

---
+++ Backend/starter_project_dotnetforce7/BlogApp2/BlogApp.API/Controllers/BaseApiController.cs
@@ -0,0 +1,32 @@
+﻿using BlogApp.Application.Responses;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseApiController : ControllerBase
+    {
+        private IMediator _mediatr;
+
+        protected IMediator Mediator => _mediatr ??= HttpContext.RequestServices.GetService<IMediator>();
+
+        protected ActionResult HandleResult<T>(Result<T> result)
+        {
+            if (result == null)
+                return NotFound(new Result<int>{ Success=false ,Message="Item not found"});
+
+            if (result.Success && result.Value != null)
+                return Ok(result);
+            if (result.Success && result.Value == null)
+                return NotFound(result);
+           
+
+            return BadRequest(result);
+        }
+
+    }
+
+
+}
---
+++ Backend/starter_project_dotnetforce7/BlogApp2/BlogApp.Application.UnitTest/Rate/Query/GetRateListQueryHandlerTest.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using BlogApp.Application.Contracts.Persistence;
+using BlogApp.Application.Features.Rates.CQRS.Handlers;
+using BlogApp.Application.Features.Rates.CQRS.Queries;
+using BlogApp.Application.Features.Rates.DTOs;
+using BlogApp.Application.Profiles;
+using BlogApp.Application.Responses;
+using BlogApp.Application.UnitTest.Mocks;
+using Moq;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogApp.Application.UnitTest.Rates.Query
+{
+    public class GetRateListQueryHandlerTest
+    {
+
+
+        private readonly IMapper _mapper;
+        private readonly Mock<IUnitOfWork> _mockRepo;
+        private readonly GetRateListQueryHandler _handler;
+        public GetRateListQueryHandlerTest()
+        {
+            _mockRepo = MockUnitOfWork.GetUnitOfWork();
+            var mapperConfig = new MapperConfiguration(c =>
+            {
+                c.AddProfile<MappingProfile>();
+            });
+            _mapper = mapperConfig.CreateMapper();
+
+            _handler = new GetRateListQueryHandler(_mockRepo.Object, _mapper);
+
+        }
+
+
+        [Fact]
+        public async Task GetRateList()
+        {
+            var result = await _handler.Handle(new GetRateListQuery(), CancellationToken.None);
+            result.ShouldBeOfType<Result<List<RateDto>>>();
+            result.Success.ShouldBeTrue();
+            result.Value.ShouldBeOfType<List<RateDto>>();
+            result.Value.Count.ShouldBe(2);
+        }
+    }
+}
+
+
+
