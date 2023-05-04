1b4c39af2e92a6ca24ae5fc4d42f067f79285d60
duressa <duressajemal3@gmail.com>
2023-05-04 10:46:48 +0300

    fix(mobile-matador): fix merge conflicts

--- Mobile/starter_project_matador/lib/main.dart
+++ Mobile/starter_project_matador/lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'features/feed/presentation/screen/home_page.dart';
 
-import 'features/login_page/Presentation/screen/login_page.dart';
-
 void main() {
   runApp(const MyApp());
 }
