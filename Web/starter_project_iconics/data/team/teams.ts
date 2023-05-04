1b1865a29488b397968f5c7470d5a770b9548a85
Zerihun Moges <zerihunmoges78@gmail.com>
2023-05-03 23:43:55 -0700

    remove empty teams-page file

--- Web/starter_project_iconics/data/team/teams.ts
+++ Web/starter_project_iconics/data/team/teams.ts
@@ -5,40 +5,40 @@ export const teamMembers: Team[] =  [{
     job: 'software engineer',
     description: 'He is a motivated software engineer looking to pursue a successful career in software development where he can help deliver software solutions to social problems. His skills include Web Development, Data Structures, Machine Learning, and DevOps. Natnael’s hobbies include coding, playing soccer, and watching movies.',
     avatar: '/img/teams/naty.png',
-    socialMedia: [{type: 'facebook', link: "facebook.com"},{type: 'linkedin', link: "facebook.com"},{type: 'instagram', link: "facebook.com"}]
+    socialMedia: [{type: 'facebook', link: "https://facebook.com"},{type: 'linkedin', link: "https://facebook.com"},{type: 'instagram', link: "https://facebook.com"}]
 },
 {
     name: 'Nathaniel Awel',
     job: 'software engineer',
     description: 'He is a motivated software engineer looking to pursue a successful career in software development where he can help deliver software solutions to social problems. His skills include Web Development, Data Structures, Machine Learning, and DevOps. Natnael’s hobbies include coding, playing soccer, and watching movies.',
     avatar: '/img/teams/naty.png',
-    socialMedia: [{type: 'facebook', link: "facebook.com"},{type: 'linkedin', link: "facebook.com"},{type: 'instagram', link: "facebook.com"}]
+    socialMedia: [{type: 'facebook', link: "https://facebook.com"},{type: 'linkedin', link: "https://facebook.com"},{type: 'instagram', link: "https://facebook.com"}]
 },
 {
     name: 'Nathaniel Awel',
     job: 'software engineer',
     description: 'He is a motivated software engineer looking to pursue a successful career in software development where he can help deliver software solutions to social problems. His skills include Web Development, Data Structures, Machine Learning, and DevOps. Natnael’s hobbies include coding, playing soccer, and watching movies.',
     avatar: '/img/teams/naty.png',
-    socialMedia: [{type: 'facebook', link: "facebook.com"},{type: 'linkedin', link: "facebook.com"},{type: 'instagram', link: "facebook.com"}]
+    socialMedia: [{type: 'facebook', link: "https://facebook.com"},{type: 'linkedin', link: "https://facebook.com"},{type: 'instagram', link: "https://facebook.com"}]
 },
 {
     name: 'Nathaniel Awel',
     job: 'software engineer',
     description: 'He is a motivated software engineer looking to pursue a successful career in software development where he can help deliver software solutions to social problems. His skills include Web Development, Data Structures, Machine Learning, and DevOps. Natnael’s hobbies include coding, playing soccer, and watching movies.',
     avatar: '/img/teams/naty.png',
-    socialMedia: [{type: 'facebook', link: "facebook.com"},{type: 'linkedin', link: "facebook.com"},{type: 'instagram', link: "facebook.com"}]
+    socialMedia: [{type: 'facebook', link: "https://facebook.com"},{type: 'linkedin', link: "https://facebook.com"},{type: 'instagram', link: "https://facebook.com"}]
 },
 {
     name: 'Nathaniel Awel',
     job: 'software engineer',
     description: 'He is a motivated software engineer looking to pursue a successful career in software development where he can help deliver software solutions to social problems. His skills include Web Development, Data Structures, Machine Learning, and DevOps. Natnael’s hobbies include coding, playing soccer, and watching movies.',
     avatar: '/img/teams/naty.png',
-    socialMedia: [{type: 'facebook', link: "facebook.com"},{type: 'linkedin', link: "facebook.com"},{type: 'instagram', link: "facebook.com"}]
+    socialMedia: [{type: 'facebook', link: "https://facebook.com"},{type: 'linkedin', link: "https://facebook.com"},{type: 'instagram', link: "https://facebook.com"}]
 },
 {
     name: 'Nathaniel Awel',
     job: 'software engineer',
     description: 'He is a motivated software engineer looking to pursue a successful career in software development where he can help deliver software solutions to social problems. His skills include Web Development, Data Structures, Machine Learning, and DevOps. Natnael’s hobbies include coding, playing soccer, and watching movies.',
     avatar: '/img/teams/naty.png',
-    socialMedia: [{type: 'facebook', link: "facebook.com"},{type: 'linkedin', link: "facebook.com"},{type: 'instagram', link: "facebook.com"}]
+    socialMedia: [{type: 'facebook', link: "https://facebook.com"},{type: 'linkedin', link: "https://facebook.com"},{type: 'instagram', link: "https://facebook.com"}]
 }]
