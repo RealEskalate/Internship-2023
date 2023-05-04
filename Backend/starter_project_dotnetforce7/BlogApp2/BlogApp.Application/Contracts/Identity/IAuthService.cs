c76ef3828c55421a6cff5f8b4ffbfb24c9ed99c8
Biruk Tassew <biruktassew21@gmail.com>
2023-05-04 10:59:33 +0300

    feat(admin-frontend): fix dependency issue and clean up

--- Backend/starter_project_dotnetforce7/BlogApp2/BlogApp.Application/Contracts/Identity/IAuthService.cs
+++ Backend/starter_project_dotnetforce7/BlogApp2/BlogApp.Application/Contracts/Identity/IAuthService.cs
@@ -3,6 +3,6 @@ using BlogApp.Application.Models.Identity;
 namespace BlogApp.Application.Contracts.Identity;
 public interface IAuthService
 {
-    Task<AuthRequest> Login(AuthRequest authRequest);
-    Task<RegistrationRequest> Register(RegistrationRequest registrationRequest);
+    Task<AuthResponse> Login(AuthRequest authRequest);
+    Task<RegistrationResponse> Register(RegistrationRequest registrationRequest);
 }
