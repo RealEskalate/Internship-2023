bfdb96953c44844a9894418ea220edfc7b311385
hayattofik-01 <hayattofik22@gmail.com>
2023-05-04 11:19:57 +0300

    add repository for the user

--- Mobile/starter_project_dark_knights/lib/features/user_profile/domain/repositories/user_repository.dart
+++ Mobile/starter_project_dark_knights/lib/features/user_profile/domain/repositories/user_repository.dart
@@ -3,26 +3,22 @@ import 'package:dark_knights/core/errors/failures.dart';
 import 'package:dark_knights/features/user_profile/domain/entities/user_entity.dart';
 import 'package:dartz/dartz.dart';
 
-abstract class UserRepository{
+abstract class UserRepository {
+  Future<List<UserEntity>> getAllUsers();
 
-  Future<List<UserEntity> > getAllUsers();
+  Future<Either<Failure, UserEntity>> createUser(UserEntity user);
 
-  Future<Either<Failure,UserEntity>>  createUser(UserEntity user);
+  Future<Either<Failure, UserEntity>> getFollowing(String userId);
 
-Future<Either<Failure,UserEntity>>  getFollowing(String userId);
+  Future<Either<Failure, UserEntity>> editUserProfile(UserEntity user);
 
+  Future<Either<Failure, UserEntity>> getNumberOfFollowers(String userId);
 
- Future<Either<Failure,UserEntity>>  editUserProfile(UserEntity user);
- 
- 
-Future<Either<Failure,UserEntity>>  getNumberOfFollowers(String userId);
-  
-Future<Either<Failure,UserEntity>>  deleteUser(String userId);
+  Future<Either<Failure, UserEntity>> deleteUser(String userId);
 
-Future<Either<Failure,UserEntity>>  getUser(String userId);
+  Future<Either<Failure, UserEntity>> getUser(String userId);
 
- Future<Either<Failure,UserEntity>>  getNumberOfFollowing(String userId);
- 
- Future<Either<Failure,UserEntity>>  getFollowers(String  userId);
+  Future<Either<Failure, UserEntity>> getNumberOfFollowing(String userId);
 
+  Future<Either<Failure, UserEntity>> getFollowers(String userId);
 }
