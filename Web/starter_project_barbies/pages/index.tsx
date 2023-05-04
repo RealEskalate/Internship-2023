3d13de18ada306b22380c9cc38e72e551ecf0c7a
Kidus Abebe <kidusabebe101@gmail.com>
2023-05-04 14:01:30 +0300

    solve index.tsx conflict for footer component

--- Web/starter_project_barbies/pages/index.tsx
+++ Web/starter_project_barbies/pages/index.tsx
@@ -1,11 +1,9 @@
-import Image from 'next/image'
-import Footer from '@/components/layout/Footer'
-import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <Footer/>
+    <div>
+      
+    </div>
   )
 }
