128e1f4aed10fc35d3cf4f98d2d8263091d5bd4a
variant-abdulaziz <73662920+variant-abdulaziz@users.noreply.github.com>
2023-05-04 14:42:30 +0300

    feat(Abdulaziz.imp-rating): implement and test rating feature (#123)
    * feat(Abdulaziz.imp-rating): implement rating for blogs
    
    * fix(Abdulaziz.imp-rating): fix rating operations responses as per specification
    
    * feat(Abdulaziz.imp-rating): test rating feature
    
    * fix(Abdulaziz.imp-rating): migrate to BaseResponse
    
    * fix(Abdulaziz.imp-rating): fix dependencies that are based on baseresponse
    
    * feat(Abdulaziz.imp-rating): add response wrapper

---
+++ Backend/starter_project_mavericks/BlogApp/BlogApp.API/Controllers/RatingsController.cs
@@ -0,0 +1,35 @@
+﻿using API.Controllers;
+using BlogApp.Application.Features.Ratings.CQRS.Commands;
+using BlogApp.Application.Features.Ratings.DTOs;
+using BlogApp.Application.Responses;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApp.API.Controllers;
+[Route("Blogs")]
+[ApiController]
+public class RatingsController : BaseApiController
+{
+    private readonly IMediator _mediator;
+    public RatingsController(IMediator mediator) : base()
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost("{blogId}/Rate")]
+    public async Task<ActionResult<BaseResponse<Nullable<int>>>> Post(int blogId, [FromBody] RatingDto ratingDto)
+    {
+        var command = new CreateRatingCommand { BlogId = blogId, RatingDto = ratingDto };
+        var response = await _mediator.Send(command);
+        return HandleResponse<Nullable<int>>(response);
+    }
+
+    [HttpPut("{blogId}/Rate")]
+    public async Task<ActionResult<BaseResponse<Nullable<int>>>> Put(int blogId, [FromBody] RatingDto ratingDto)
+    {
+        var command = new UpdateRatingCommand { BlogId = blogId, RatingDto = ratingDto };
+        var response = await _mediator.Send(command);
+        return HandleResponse<Nullable<int>>(response);
+    }
+}
---
+++ Backend/starter_project_mavericks/BlogApp/BlogApp.Application/Features/Ratings/DTOs/RatingDto.cs
@@ -0,0 +1,12 @@
+﻿using BlogApp.Application.Features.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogApp.Application.Features.Ratings.DTOs;
+public class RatingDto
+{
+    public int Rate { get; set; }
+}
