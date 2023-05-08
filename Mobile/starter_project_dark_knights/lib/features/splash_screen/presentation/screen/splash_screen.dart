0c53d5a0a5dffc751fe02a19fe7e02d5b9d69e66
Yared <83911480+Yared-betsega@users.noreply.github.com>
2023-05-08 11:15:07 +0300

    (Mobile-Dartknights): Implement the splash screen (#75)
    * (Mobile-Dartknights): Duplicate the example folder and add starter files
    
    * (Mobile-Dartknights): Add asset images and splash screen page
    
    * fix(Mobile-Dartknights): update the route to onboarding page

---
+++ Mobile/starter_project_dark_knights/lib/features/splash_screen/presentation/screen/splash_screen.dart
@@ -0,0 +1,26 @@
+import 'dart:async';
+import 'package:dark_knights/features/onboarding/presentation/screen/onboarding_page.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 3), () {
+      Navigator.of(context).push(
+          MaterialPageRoute(builder: (context) => const OnboardingScreen()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Image(image: AssetImage("assets/images/splash_screen.png"));
+  }
+}
