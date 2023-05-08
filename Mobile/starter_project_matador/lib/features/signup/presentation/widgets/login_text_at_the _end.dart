7c74f168bbd158097957147a5353a5179e7c5a94
SEID MUHAMMED <abuhaithemalthry@gmail.com>
2023-05-08 17:17:06 +0300

    untracked unwanted changes (#157)

---
+++ Mobile/starter_project_matador/lib/features/signup/presentation/widgets/login_text_at_the _end.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import '../../../../core/utils/constants/styles.dart';
+
+class Footer extends StatelessWidget {
+  const Footer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var Height = MediaQuery.of(context).size.height;
+    var Width = MediaQuery.of(context).size.width;
+    return Container(
+      margin: EdgeInsets.only(
+        top: (17 / 812) * Height,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Have an account?  ',
+            style: haveAnAccountTextStyle,
+          ),
+          GestureDetector(
+            onTap: () {},
+            child: Text(
+              'Login',
+              style: loginBottomTextStyle,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
