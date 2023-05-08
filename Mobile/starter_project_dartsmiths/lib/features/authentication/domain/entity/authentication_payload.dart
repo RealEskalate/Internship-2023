94415283cb554924d4513ddfff029cb05b75a143
bereket <bereketdemissie4382@gmail.com>
2023-05-08 10:34:14 +0300

    create login usecase and login repository, test login usecase, add failure.dart and refactore login usecase

--- Mobile/starter_project_dartsmiths/lib/features/authentication/domain/entity/authentication_payload.dart
+++ Mobile/starter_project_dartsmiths/lib/features/authentication/domain/entity/authentication_payload.dart
@@ -1,5 +1,14 @@
 import 'package:equatable/equatable.dart';
 
+// class UserAuthCredential extends Equatable {
+//   final String username;
+//   final String password;
+
+//   const UserAuthCredential({required this.username, required this.password});
+//   @override
+//   List<Object> get props => [username, password];
+// }
+
 class UserAuthCredential extends Equatable {
   final String username;
   final String password;
---
+++ Mobile/starter_project_dartsmiths/lib/features/authentication/domain/use_cases/login_usecase.dart
@@ -0,0 +1,24 @@
+import 'package:dartsmiths/core/usecase/usecase.dart';
+import 'package:dartsmiths/features/authentication/domain/entity/authentication_payload.dart';
+import 'package:dartsmiths/features/authentication/domain/repository/login_repository.dart';
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/errors/failures.dart';
+
+class LoginUsecase
+    implements Usecase<UserAuthCredential, UserAuthCredentialParams> {
+  final AuthenticationRepository repository;
+  @override
+  Future<Either<Failure, UserAuthCredential>> call(
+      UserAuthCredentialParams params) async {
+    return await repository.login(username: params.username, password: params.password);
+  }
+
+  LoginUsecase(this.repository);
+}
+
+class UserAuthCredentialParams {
+  String username;
+  String password;
+  UserAuthCredentialParams({required this.username, required this.password});
+}
