ed4f968e190b549669857acc5a8e1c666eb7677f
Henok Matheas Kebede <82048614+Henok-Matheas@users.noreply.github.com>
2023-05-08 11:44:40 +0300

    feat(web-iconics): implement success-rate section of the homepage (#120)
    * feat(web-iconics): implement success-rate section of the homepage
    
    * fix(web-iconics): change the color from Hex to the one defined in the themes

--- Web/starter_project_iconics/pages/index.tsx
+++ Web/starter_project_iconics/pages/index.tsx
@@ -1,11 +1,13 @@
 import BuildBetter from '@/components/home/BuildBetter'
 import Landing from '@/components/home/Landing'
+import SuccessRate from '@/components/home/SuccessRate'
 
 export default function Home() {
   return (
     <main className="bg-white flex min-h-screen flex-col justify-between">
       <Landing />
       <BuildBetter />
+      <SuccessRate />
     </main>
   )
 }
