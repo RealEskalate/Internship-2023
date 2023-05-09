38f0fc987d3d8bbf33dde897f80f49addd868807
Zerihun Moges <zerihunmoges78@gmail.com>
2023-05-09 06:34:38 -0700

    add ... in pagination

--- Web/starter_project_iconics/components/common/Pagination.tsx
+++ Web/starter_project_iconics/components/common/Pagination.tsx
@@ -4,30 +4,28 @@ interface PaginationProps{
     currentPage: number
     setCurrentPage: (page: number) => void
 }
-interface paginationPageProps{
-  page: string,
-  onClick?:  () => void,
-  active?: boolean
-}
-const PaginationPage : React.FC<paginationPageProps> = ({page, onClick, active}) => {
-  return (
-
-      <button onClick={onClick} className={`${active? 'bg-primary text-secondary': 'bg-secondary text-primary'} m-1 px-3 py-2 rounded-md cursor-pointer`}>{page}</button>
-
-  )
-}
-
 
 const Pagination : React.FC<PaginationProps> = ({pagesCount, currentPage, setCurrentPage}) => {
 
-  let pages: string[] = []
+  
+  let pages: JSX.Element[] = []
+  const visible = 2
 
-  for (let page = 1; page <= pagesCount; page++) {
-    pages.push(page.toString())
+  for (let page:number = 1; page <= pagesCount; page++) {
+    
+    if (page <= visible || page > pagesCount-visible || (currentPage-visible <= page && currentPage+visible >= page)) {
+      pages.push( <button onClick={() => setCurrentPage(page)} className={`${currentPage === page? 'bg-primary text-secondary': 'bg-secondary text-primary'} m-1 px-3 py-2 rounded-md cursor-pointer`}>{page}</button>)
+    }
+    else{
+      pages.push( <button className={`flex text-primary m-1 px-3 py-2 rounded-md items-center cursor-pointer`} disabled>{'...'}</button>)
+      page = page < currentPage ? currentPage-(visible+1) : pagesCount - visible
+    }
   }
-
+  
   return (
-    <div className='flex p-4 justify-center'> {pages.map((page, index) => <PaginationPage key={index} onClick={() => {setCurrentPage(index)}} page={page} active={pages[currentPage] === page ? true: false}></PaginationPage>)}</div>
+    <div className='flex flex-wrap p-4 justify-center'> 
+    {pages}
+    </div>
   )
 }
 
