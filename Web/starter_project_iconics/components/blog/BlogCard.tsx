b2c56906e43bdb3fcd04bad851771972c2e8bbaa
Blen <62964622+blenassefa2@users.noreply.github.com>
2023-05-09 16:16:41 +0300

    feat(web-iconic): merge data type created earlier

--- Web/starter_project_iconics/components/blog/BlogCard.tsx
+++ Web/starter_project_iconics/components/blog/BlogCard.tsx
@@ -1,43 +1,52 @@
 import React from 'react'
-import { Blog } from '../../types/profile/blog'
+import { BlogData } from '@/types/blog/blog'
 import BlogFooter from './BlogCardFooter'
 
 interface Props {
-  blog: Blog
+  blog: BlogData
   isMyBlogsPage: boolean
 }
 
 const BlogCard: React.FC<Props> = ({ blog, isMyBlogsPage }) => {
+  let {
+    name,
+    profileImg,
+    profession,
+    datePosted,
+    title,
+    description,
+    tags,
+    blogImg,
+    likes,
+    status,
+  } = blog
+  if (likes == undefined) likes = 0
+
+  if (status == undefined) status = 'pending'
   return (
     <div className="blog w-[400px] font-montserrat flex flex-col border border-gray-300 rounded-lg overflow-hidden shadow-md bg-white h-543 my-8 font-bold">
-      <img
-        className="w-full h-60 object-cover"
-        src={blog.photoUrl}
-        alt={`${blog.writter.firstName} ${blog.writter.lastName}`}
-      />
+      <img className="w-full h-60 object-cover" src={blogImg} alt={name} />
       <div className="p-4">
         <b>
           <h2 className="blog_title font-bold text-lg text-gray-700 break-words overflow-hidden line-clamp-3 leading-snug">
-            {blog.title}
+            {title}
           </h2>
         </b>
 
         <div className="blog_writter flex items-center mb-6 mt-4">
           <img
             className="blog_writter_image w-10 h-10 object-cover rounded-full mr-2"
-            src={blog.writter.photoUrl}
-            alt={`${blog.writter.firstName} ${blog.writter.lastName}`}
+            src={profileImg}
+            alt={name}
           />
 
           <div className="blog_writter_details flex flex-col items-start">
             <span className="blog_writter_name m-0   text-gray-600 leading-5">
               <b className="font-thin">&nbsp;{' by '}</b>&nbsp;
-              <b>
-                {blog.writter.firstName} {blog.writter.lastName}
-              </b>
+              <b>{name}</b>
               <b className="font-thin">
                 &nbsp;&nbsp;{' | '}&nbsp;&nbsp;
-                {new Date(blog.datePosted).toLocaleDateString('en-US', {
+                {new Date(datePosted).toLocaleDateString('en-US', {
                   month: 'short',
                   day: 'numeric',
                   year: 'numeric',
@@ -47,7 +56,7 @@ const BlogCard: React.FC<Props> = ({ blog, isMyBlogsPage }) => {
           </div>
         </div>
         <div className="blog_tags flex flex-row items-center mb-4">
-          {blog.tags.map((tag, index) => (
+          {tags.map((tag: string, index: number) => (
             <span
               key={index}
               className="blog_tag mr-2 px-2 py-1 bg-gray-100 text-gray-500 rounded-full text-xs"
@@ -62,8 +71,8 @@ const BlogCard: React.FC<Props> = ({ blog, isMyBlogsPage }) => {
         <br />
         <hr className="line border-gray-200" />
         <BlogFooter
-          numberOfLikes={blog.likes}
-          blogStatus={blog.status}
+          numberOfLikes={likes}
+          blogStatus={status}
           myBlog={isMyBlogsPage}
         />
       </div>
--- Web/starter_project_iconics/types/blog/blog.tsx
+++ Web/starter_project_iconics/types/blog/blog.tsx
@@ -14,4 +14,6 @@ export interface Blog {
   }
   tags: string[]
   paragraph: string
+  likes?: number
+  status?: 'pending' | 'approved' | 'declined'
 }
