17aa6a5e98c054168ee47a7141f5c97ea4ab9380
Yared <83911480+Yared-betsega@users.noreply.github.com>
2023-05-09 10:50:04 +0300

    (Mobile-Dartknights): Add common errors and usecases (#168)
    * (Mobile-Dartknights): Add common errors and usecases
    
    * (Mobile-Dartknights): Add common errors and usecases

---
+++ Mobile/starter_project_dark_knights/lib/core/usecases/usecase.dart
@@ -0,0 +1,12 @@
+import 'package:dark_knights/core/errors/failures.dart';
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+
+abstract class UseCase<Type, Params> {
+  Future<Either<Failure, Type>> call(Params params);
+}
+
+class NoParams extends Equatable {
+  @override
+  List<Object?> get props => throw UnimplementedError();
+}
