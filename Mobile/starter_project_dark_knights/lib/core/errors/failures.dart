1fdef94c0827fb9225f590261935bd716d6b827f
hayattofik-01 <hayattofik22@gmail.com>
2023-05-09 11:20:00 +0300

    fix failures error

--- Mobile/starter_project_dark_knights/lib/core/errors/failures.dart
+++ Mobile/starter_project_dark_knights/lib/core/errors/failures.dart
@@ -1,5 +1,9 @@
 import 'package:equatable/equatable.dart';
 
-abstract class Failure extends Equatable{
- Failure([List properties = const<dynamic>[]]) : super();
+abstract class Failure extends Equatable {
+  Failure([List properties = const <dynamic>[]]) : super();
+
+  @override
+  // TODO: implement props
+  List<Object> get props => [];
 }
