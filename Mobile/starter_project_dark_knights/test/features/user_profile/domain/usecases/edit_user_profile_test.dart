2e6390b21b831b54ce20af5d2677cab21f80c8ac
Yared Tsegaye <yaredtsegaye120@gmail.com>
2023-05-09 12:02:36 +0300

    (Mobile-Dartknights): Add edit profile use case test

---
+++ Mobile/starter_project_dark_knights/test/features/user_profile/domain/usecases/edit_user_profile_test.dart
@@ -0,0 +1,7 @@
+import 'package:mockito/annotations.dart';
+
+import 'package:dartz/dartz.dart';
+import 'package:mockito/mockito.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+@GenerateMocks([UserRepository])
