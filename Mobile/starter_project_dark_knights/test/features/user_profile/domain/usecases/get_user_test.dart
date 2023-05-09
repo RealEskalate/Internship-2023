5cb4c53e11c3ae841c321cba6d5c8dc7c68b65d3
hayattofik-01 <hayattofik22@gmail.com>
2023-05-09 11:48:16 +0300

    add  test for  getUser usecase

--- Mobile/starter_project_dark_knights/test/features/user_profile/domain/usecases/get_user_test.dart
+++ Mobile/starter_project_dark_knights/test/features/user_profile/domain/usecases/get_user_test.dart
@@ -1,19 +1,40 @@
+import 'dart:ffi';
+
+import 'package:dark_knights/core/usecases/usecase.dart';
+import 'package:dark_knights/features/user_profile/domain/entities/user_entity.dart';
 import 'package:dark_knights/features/user_profile/domain/repositories/user_repository.dart';
 import 'package:dark_knights/features/user_profile/domain/usecases/get_user.dart';
+import 'package:dartz/dartz.dart';
+import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-class MockUserEntityRepository  extends Mock 
-implements UserRepository{
+import 'get_user_test.mocks.dart';
 
-}
-
+@GenerateMocks([UserRepository])
 void main(){
-  GetUser usecase;
-  MockUserEntityRepository mockUserEntityRepository;
-  setUp(() {
-mockUserEntityRepository = MockUserEntityRepository();
-usecase = GetUser(mockUserEntityRepository);
+    
+    setUp(() {
+final mockUserRepository = MockUserRepository();
+final usecase = GetUser(mockUserRepository);
+    });
+    final user = UserEntity(id: "user_123", username: "@jhon", firstName: "jhon", lastName:"abe", occupation: "designer", selfDescription: "I love design", password: "123", image: "someUrl");
+    test(
+      'should get user details',
+      ()async{
+        final mockUserRepository = MockUserRepository();
+final usecase = GetUser(mockUserRepository);
+when(mockUserRepository.getUser("user_123")).thenAnswer((_) async => Right(user));
 
-  });
+final result = await usecase( Params(id: "user_123"));
+
+expect(result,Right(user));
+verify(mockUserRepository.getUser("user_123"));
+verifyNoMoreInteractions(mockUserRepository);
+      }
+    );
+ 
 }
+
+
+
