8a9283e1bb377f702eadedfab666aeeb44da5641
TigistW <88367517+TigistW@users.noreply.github.com>
2023-05-09 11:56:57 +0300

    feat(backend-zsilentcoders): implement blog CRUD (#166)
    * feat(backend-zsilentcoders): Implement blog CRUD
    
    * fix(backend-zsilentcoders): add data change handle
    
    * fix(backend-zsilentcoders): fix blog CRUD

--- Backend/starter_project_zsilentcoders/BlogApp3/BlogApp.API/Controllers/BlogsController.cs
+++ Backend/starter_project_zsilentcoders/BlogApp3/BlogApp.API/Controllers/BlogsController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BlogApp.Api.Controllers;
 
-public class BlogsController: BaseController
+public class BlogsController : BaseController
 {
     private IMediator _mediator { get; set; }
 
@@ -15,32 +15,37 @@ public class BlogsController: BaseController
     {
         _mediator = mediator;
     }
-    
+    [HttpGet]
+    public async Task<ActionResult<List<BlogListDto>>> Get()
+    {
+        return HandleResult(await _mediator.Send(new GetBlogListQuery()));
+    }
+
     [HttpGet("{id:int}")]
     public async Task<ActionResult> Get(int id)
     {
         return HandleResult(await _mediator.Send(new GetBlogDetailsQuery { Id = id }));
     }
-    
+
     [HttpPost]
     public async Task<ActionResult> CreateBlog([FromBody] CreateBlogDto createBlogDto)
     {
         return HandleResult(await _mediator.Send(new CreateBlogCommand { CreateBlogDto = createBlogDto }));
     }
-    
-    
+
+
     [HttpDelete("{id:int}")]
     public async Task<ActionResult> DeleteBlog(int id)
     {
         return HandleResult(
             await _mediator.Send(new DeleteBlogCommand { DeleteBlogDto = new DeleteBlogDto { Id = id } }));
     }
-    
+
 
     [HttpPut]
     public async Task<ActionResult> UpdateBlog([FromBody] UpdateBlogDto updateBlogDto)
     {
-        return HandleResult(await _mediator.Send(new UpdateBlogCommand(){UpdateBlogDto = updateBlogDto}));
+        return HandleResult(await _mediator.Send(new UpdateBlogCommand() { UpdateBlogDto = updateBlogDto }));
     }
-    
+
 }
