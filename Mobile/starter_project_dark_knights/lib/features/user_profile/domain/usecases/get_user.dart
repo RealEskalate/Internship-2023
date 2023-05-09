98f21f92ccba37cf9415b8f3b7acfb955e8b0759
hayattofik-01 <hayattofik22@gmail.com>
2023-05-09 14:53:48 +0300

    refactor GetUser usecase

--- Mobile/starter_project_dark_knights/lib/features/user_profile/domain/usecases/get_user.dart
+++ Mobile/starter_project_dark_knights/lib/features/user_profile/domain/usecases/get_user.dart
@@ -1,21 +1,21 @@
 import 'package:dark_knights/core/errors/failures.dart';
+import 'package:dark_knights/core/usecases/usecase.dart';
 import 'package:dark_knights/features/user_profile/domain/entities/user_entity.dart';
 import 'package:dark_knights/features/user_profile/domain/repositories/user_repository.dart';
 import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
 
-class GetUser {
+class GetUser implements UseCase<UserEntity,Params> {
   final UserRepository repo;
   GetUser(this.repo);
   
-Future<Either<Failure,UserEntity>> call (Params params)async {
-
-
-    return await repo.getUser(params.id);
-
+@override
+  Future<Either<Failure, UserEntity>> call(Params params) async {
+   return await repo.getUser(params.id);
   }
-  
 }
+  
+
 class Params extends Equatable {
   final String id;
 
