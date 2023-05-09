49a612839db5fa6541638617fd2971dd8acf68f5
mekdeskebede <kebedemekdes289@gmail.com>
2023-05-09 17:07:09 +0300

    fix single blog page

--- Web/starter_project_iconics/pages/blog/[id].tsx
+++ Web/starter_project_iconics/pages/blog/[id].tsx
@@ -1,29 +1,39 @@
 import Image from 'next/image'
 import { useRouter } from 'next/router'
 import React from 'react'
-import blogs from '../../data/blog/blog-detail'
-import BlogDetail from '../../types/blog/blog-detail'
+import blogs from '../../data/blog/blog-detail.json'
 
-interface Props {
-  id: string
-}
-const SingleBlog: React.FC<Props> = () => {
+const SingleBlog: React.FC = () => {
   const router = useRouter()
   const { id } = router.query
-  const blog: BlogDetail[] = blogs
-  const currBlog = blog.filter((item) => item.blogId === id)
-  const blogDetail: BlogDetail = currBlog[0]
+  const blogList = blogs
+  console.log(id)
+  const currBlog = blogList.blogs.filter((item) => {
+    // TODO 
+    // This Will be fixed on another PR
+    return item.blogId === '1'
+  })
+  const blogDetail = currBlog[0]
+
+  if (blogDetail == null) {
+    return (
+      <div>
+        <h1>Not Found</h1>
+      </div>
+    )
+  }
+
   return (
     <div className="bg-white">
       <h2 className="text-center pt-4 font-imfell font-normal text-3xl text-primary-text">
-        {blogDetail && blogDetail.blogTitle}
+        {blogDetail.blogTitle}
       </h2>
       <p className="text-center uppercase font-montserrat font-thin tracking-widest leading-9 text-xs text-primary-text">
-        {blogDetail && blogDetail.smallBlogTitle}
+        {blogDetail.smallBlogTitle}
       </p>
       <div className="flex h-auto w-5/6 justify-center items-center mx-auto p-8">
         <Image
-          src={`/img/blog/${blogDetail && blogDetail.blogImgUrl}`}
+          src={`/img/blog/${blogDetail.blogImgUrl}`}
           className="mx-auto"
           alt="blog image"
           width={800}
@@ -33,7 +43,7 @@ const SingleBlog: React.FC<Props> = () => {
 
       <div className="mx-auto p-4">
         <Image
-          src={`/img/blog/${blogDetail && blogDetail.author.imageUrl}`}
+          src={`/img/blog/${blogDetail.authors?.imageUrl}`}
           className="mx-auto p-4 rounded-full w-28 h-28"
           alt="author image"
           width={100}
@@ -41,26 +51,24 @@ const SingleBlog: React.FC<Props> = () => {
         />
         <div className="">
           <p className="mx-auto text-center uppercase font-thin text-sm font-montserrat tracking-widest leading-9 text-primary-text">
-            {blogDetail && blogDetail.author.name} |{' '}
-            {blogDetail && blogDetail.author.profession}
+            {blogDetail.authors.name} | {blogDetail.authors.profession}
           </p>
           <a className="text-center " href="#">
             <p className="text-center uppercase font-semibold text-sm text-primary font-montserrat tracking-widest leading-9">
-              {blogDetail && blogDetail.author.socialMediaLink}
+              {blogDetail.authors.socialMediaLink}
             </p>
           </a>
         </div>
       </div>
       <div className="mx-auto pt-8 text-left w-4/6 font-imfell font-medium text-xl text-primary-text leading-10">
-        <b className="bold-text">{blogDetail && blogDetail.heading}</b>
-        {blogDetail &&
-          blogDetail.paragraphs.map((text, index) => (
-            <div className="mt-4" key={index}>
-              <small className="font-light font-montserrat text-base text-secondary-text">
-                {text}
-              </small>
-            </div>
-          ))}
+        <b className="bold-text">{blogDetail.heading}</b>
+        {blogDetail.paragraphs.map((text, index) => (
+          <div className="mt-4" key={index}>
+            <small className="font-light font-montserrat text-base text-secondary-text">
+              {text}
+            </small>
+          </div>
+        ))}
       </div>
     </div>
   )
