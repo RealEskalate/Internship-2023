6dfb771549184e284416b3386c119ef2843a2f35
Edilcc <abenynin2314@gmail.com>
2023-05-09 01:35:08 -0700

    "ApplicationTag2"

--- Backend/starter_project_mavericks/BlogApp/BlogApp.Application/Contracts/Persistence/IUnitOfWork.cs
+++ Backend/starter_project_mavericks/BlogApp/BlogApp.Application/Contracts/Persistence/IUnitOfWork.cs
@@ -12,6 +12,7 @@ namespace BlogApp.Application.Contracts.Persistence
         I_IndexRepository _IndexRepository { get; }
         IBlogRepository BlogRepository { get; }
         IReviewRepository ReviewRepository { get; }
+        I_TagRepository _TagRepository { get; }
 
         Task<int> Save();
     }
