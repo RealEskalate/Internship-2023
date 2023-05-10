40ecf41213e68f285bbcbb1fd048d99d8758522a
abrahamshimekt <abrahamshimektabrahamshimekt4096@gmail.com>
2023-05-10 08:17:47 +0300

    feature(backend-mavericks):add review entity

---
+++ Backend/starter_project_mavericks/BlogApp/BlogApp.Domain/Review.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlogApp.Domain;
+
+public class Review
+{
+    public int Id { get; set; }
+    [Required]
+    public int BlogId { get; set; }
+    [Required]
+    public int ReviewerId { get; set; }
+    [Required]
+    public string Comment { get; set; }
+    [DefaultValue(false)]
+    public bool IsResolved { get; set; }
+}
