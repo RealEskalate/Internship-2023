f44163e10901c1979dc39edddc4163dd51da3bdd
hayattofik-01 <hayattofik22@gmail.com>
2023-05-10 14:45:56 +0300

    refactory usecase tests

--- Mobile/starter_project_dark_knights/test/features/user_profile/domain/usecases/get_user_test.dart
+++ Mobile/starter_project_dark_knights/test/features/user_profile/domain/usecases/get_user_test.dart
@@ -1,30 +1,23 @@
-import 'dart:ffi';
-
-import 'package:dark_knights/core/usecases/usecase.dart';
 import 'package:dark_knights/features/user_profile/domain/entities/user_entity.dart';
-import 'package:dark_knights/features/user_profile/domain/repositories/user_repository.dart';
 import 'package:dark_knights/features/user_profile/domain/usecases/get_user.dart';
 import 'package:dartz/dartz.dart';
-import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'get_followers_test.mocks.dart';
 
 
-@GenerateMocks([UserRepository])
 void main(){
-    
+    late MockUserRepository  mockUserRepository;
+    late GetUser usecase;
     setUp(() {
-final mockUserRepository = MockUserRepository();
-final usecase = GetUser(mockUserRepository);
+ mockUserRepository = MockUserRepository();
+usecase = GetUser(mockUserRepository);
     });
     final user = UserEntity(id: "user_123", username: "@jhon", firstName: "jhon", lastName:"abe", occupation: "designer", selfDescription: "I love design", password: "123", image: "someUrl");
     test(
       'should get user details',
       ()async{
-        final mockUserRepository = MockUserRepository();
-final usecase = GetUser(mockUserRepository);
 when(mockUserRepository.getUser("user_123")).thenAnswer((_) async => Right(user));
 
 final result = await usecase( "user_123");
