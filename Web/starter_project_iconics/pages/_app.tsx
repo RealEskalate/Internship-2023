7e505970003858f3d3477a082923c7391226ffd1
Brook Alemu <brookwworku3@gmail.com>
2023-05-10 05:47:22 -0900

    implement card items

--- Web/starter_project_iconics/pages/_app.tsx
+++ Web/starter_project_iconics/pages/_app.tsx
@@ -6,12 +6,6 @@ import type { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
 
 
-import '@/styles/about/AboutSection.css'
-import '@/styles/about/Card.css'
-import '@/styles/about/ProblemSection.css'
-import '@/styles/about/SessionSection.css'
-import '@/styles/about/SocialProjectsSections.css'
-
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
