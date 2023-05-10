620113e0337057570afd40949f3e93745f7d8846
Gizaw Dagne <gizawaait@gmail.com>
2023-05-10 14:52:19 +0300

    fix(backend-dotnetforce7): fix rateCommand changes

--- Backend/starter_project_dotnetforce7/BlogApp2/BlogApp.Application/Features/Rates/CQRS/Commands/CreateRateCommand.cs
+++ Backend/starter_project_dotnetforce7/BlogApp2/BlogApp.Application/Features/Rates/CQRS/Commands/CreateRateCommand.cs
@@ -1,6 +1,11 @@
 ﻿using BlogApp.Application.Features.Rates.DTOs;
 using BlogApp.Application.Responses;
 using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace BlogApp.Application.Features.Rates.CQRS.Commands
 {
