f867485201841b8e43767b834b75179b3dad2dd5
abrahamshimekt <abrahamshimektabrahamshimekt4096@gmail.com>
2023-05-10 07:59:35 +0300

    feature(backend-mavericks):create repository interface

---
+++ Backend/starter_project_mavericks/BlogApp/BlogApp.Application/Contracts/Persistence/IReviewRepository.cs
@@ -0,0 +1,11 @@
+﻿using BlogApp.Application.Features.Reviews.DTOs;
+using BlogApp.Domain;
+
+namespace BlogApp.Application.Contracts.Persistence;
+
+public interface IReviewRepository : IGenericRepository<Review>
+{
+    Task<Review> GetReviewDetail(int Id);
+    Task<List<Review>> GetReviewsByBlogId(int BlogId);
+    Task<List<Review>> GetReviewsByUserId(int ReviewerId);
+}
