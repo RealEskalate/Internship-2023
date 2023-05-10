81ba010b7781e81ced69cb453afd6c8c7b2f49e4
abrahamshimekt <abrahamshimektabrahamshimekt4096@gmail.com>
2023-05-10 08:06:23 +0300

    Revert "feature(backend-mavericks):add review entity"
    This reverts commit 096bb2ad0b6aa93a947ff494074d1bd56c576c82.

--- Backend/starter_project_mavericks/BlogApp/BlogApp.Application/Features/Reviews/DTOs/UpdateReviewDto.cs
+++
@@ -1,9 +0,0 @@
-﻿using BlogApp.Application.Features.Common;
-
-namespace BlogApp.Application.Features.Reviews.DTOs;
-
-public class UpdateReviewDto : BaseDto
-{
-    public string Comment { get; set; }
-    public bool IsResolved { get; set; }
-}
