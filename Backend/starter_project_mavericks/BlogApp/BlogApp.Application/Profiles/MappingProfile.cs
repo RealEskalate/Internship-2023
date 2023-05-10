a2d12ad429ea1390676b9daf03b4388b72c946f6
Edilcc <abenynin2314@gmail.com>
2023-05-10 00:40:12 -0700

    "TagUnitTest"

--- Backend/starter_project_mavericks/BlogApp/BlogApp.Application/Profiles/MappingProfile.cs
+++ Backend/starter_project_mavericks/BlogApp/BlogApp.Application/Profiles/MappingProfile.cs
@@ -19,8 +19,6 @@ namespace BlogApp.Application.Profiles
         {
             CreateMap<_Index, _IndexDto>().ReverseMap();
             CreateMap<_Index, Create_IndexDto>().ReverseMap();
-            CreateMap<_Tag, _TagDto>().ReverseMap();
-            CreateMap<_Tag, Create_TagDto>().ReverseMap();
             #region _index Mappings
 
             CreateMap<_Index, _IndexDto>().ReverseMap();
