e34b26fd306561aa29c0e8b624de4dd9c34b98a2
abrahamshimekt <abrahamshimektabrahamshimekt4096@gmail.com>
2023-05-11 08:19:50 +0300

    feature(backend-mavericks): change endpoints

--- Backend/starter_project_mavericks/BlogApp/BlogApp.API/Controllers/ReviewsController.cs
+++ Backend/starter_project_mavericks/BlogApp/BlogApp.API/Controllers/ReviewsController.cs
@@ -4,11 +4,12 @@ using BlogApp.Application.Features.Reviews.CQRS.Commands;
 using BlogApp.Application.Features.Reviews.CQRS.Queries;
 using BlogApp.Application.Features.Reviews.DTOs;
 using BlogApp.Application.Responses;
+using BlogApp.Domain;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
-
 namespace BlogApp.Api.Controllers;
-[Route("api/[Controller]")]
+
+[Route("api")]
 [ApiController]
 public class ReviewsController : BaseApiController
 {
@@ -18,7 +19,7 @@ public class ReviewsController : BaseApiController
     
     }
 
-    [HttpGet("blogReviews{blogId}")]
+    [HttpGet("blogs/{blogId}/reviews")]
     public async Task<ActionResult<List<ReviewDto>>> GetBlogReviews(int blogId)
     {
         var query = new GetBlogReviewListQuery{BlogId = blogId};
@@ -28,7 +29,7 @@ public class ReviewsController : BaseApiController
             : getResponse(HttpStatusCode.BadRequest, response);
     }
 
-    [HttpGet("userReviews{reviewerId}")]
+    [HttpGet("users/{reviewerId}/reviews")]
     
     public async Task<ActionResult<List<ReviewDto>>> GetUserReviews(string reviewerId)
     {
@@ -40,17 +41,17 @@ public class ReviewsController : BaseApiController
 
     }
 
-    [HttpGet("{id}")]
-    public async Task<ActionResult> GetReview(int id)
+    [HttpGet("blogs/review/{reviewId}")]
+    public async Task<ActionResult> GetReview(int reviewId)
     {
-        var query = new GetReviewDetailQuery { Id = id };
+        var query = new GetReviewDetailQuery { Id = reviewId };
         var response = await _mediator.Send(query);
         return response.Success
             ? getResponse(HttpStatusCode.OK, response)
             : getResponse(HttpStatusCode.BadRequest, response);
     }
 
-    [HttpPost]
+    [HttpPost("blogs/reviews")]
     public async Task<ActionResult<BaseResponse<int?>>> Post([FromBody] CreateReviewDto createReviewDto)
     {
         var command = new CreateReviewCommand { CreateReviewDto = createReviewDto };
@@ -60,7 +61,7 @@ public class ReviewsController : BaseApiController
             : getResponse(HttpStatusCode.BadRequest, response);
     }
 
-    [HttpPut]
+    [HttpPut("blogs/reviews")]
     public async Task<ActionResult<BaseResponse<Unit?>>> Put([FromBody] UpdateReviewDto updateReviewDto)
     {
         var command = new UpdateReviewCommand { UpdateReviewDto = updateReviewDto };
@@ -70,10 +71,10 @@ public class ReviewsController : BaseApiController
             : getResponse(HttpStatusCode.BadRequest, response);
     }
 
-    [HttpDelete("{id}")]
-    public async Task<ActionResult<BaseResponse<Unit?>>> Delete(int id)
+    [HttpDelete("blogs/reviews/{reviewId}")]
+    public async Task<ActionResult<BaseResponse<Unit?>>> Delete(int reviewId)
     {
-        var command = new DeleteReviewCommand { Id = id };
+        var command = new DeleteReviewCommand { Id = reviewId };
         var response = await _mediator.Send(command);
         return response.Success
             ? getResponse(HttpStatusCode.NoContent, response)
