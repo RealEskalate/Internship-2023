d65b719be311d5a29cb96b05ab2123aff30b4fea
Yared <83911480+Yared-betsega@users.noreply.github.com>
2023-05-11 11:34:20 +0300

    Yared.hayat usecases for user profile (#196)
    * add user model
    
    * add user model
    
    * add user entity
    
    * add repository for the user
    
    * add the repository layer
    
    * add test for user_profile
    
    * add getUser usecase
    
    * add failures for usecases
    
    * (Mobile-Dartknights): add edit user profile use case
    
    * (Mobile-Dartknights): Add edit profile use case test
    
    * add blue card color
    
    * add user entity
    
    * add repository for the user
    
    * add getUser usecase
    
    * fix failures error
    
    * add  test for  getUser usecase
    
    * (Mobile-Dartknights): Add edit profile test features with generated mocks
    
    * (Mobile-Dartknights): Add test for mismatch inputs in edit profile test
    
    * (Mobile-Dartknights): Add get number of followers usecase with tests
    
    * (Mobile-Dartknights): Add delete user usecase with test
    
    * fix(Mobile-Dartknights): remove get user usecases
    
    * (Mobile-Dartknights): Add create user usecase with tests
    
    * (Mobile-Dartknights): Add get followers usecase with tests
    
    * (Mobile-Dartknights): remove models from the data layer
    
    * (Mobile-Dartknights): make variable naming consistent
    
    ---------
    
    Co-authored-by: hayattofik-01 <hayattofik22@gmail.com>

---
+++ Mobile/starter_project_dark_knights/lib/features/user_profile/domain/entities/user_entity.dart
@@ -0,0 +1,35 @@
+import 'package:equatable/equatable.dart';
+
+class UserEntity extends Equatable {
+  final String id;
+  final String username;
+  final String firstName;
+  final String lastName;
+  final String occupation;
+  final String selfDescription;
+  final String password;
+  final String image;
+
+  UserEntity({
+    required this.id,
+    required this.username,
+    required this.firstName,
+    required this.lastName,
+    required this.occupation,
+    required this.selfDescription,
+    required this.password,
+    required this.image,
+  });
+
+  @override
+  List<Object?> get props => [
+        id,
+        username,
+        firstName,
+        lastName,
+        occupation,
+        selfDescription,
+        password,
+        image
+      ];
+}
