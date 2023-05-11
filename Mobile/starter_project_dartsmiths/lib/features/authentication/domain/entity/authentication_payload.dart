18688b88d58be764d8af170b19f64c6770c31974
Beki4382 <91548062+Beki4382@users.noreply.github.com>
2023-05-11 14:23:53 +0300

    feat(mobile-dartsmith): implement login entity_login usecase (#117)
    * create user entity
    
    * update naming
    
    * create login usecase and login repository, test login usecase, add failure.dart and refactore login usecase
    
    * remove unnecessary comments

---
+++ Mobile/starter_project_dartsmiths/lib/features/authentication/domain/entity/authentication_payload.dart
@@ -0,0 +1,10 @@
+import 'package:equatable/equatable.dart';
+
+class UserAuthCredential extends Equatable {
+  final String username;
+  final String password;
+
+  const UserAuthCredential({required this.username, required this.password});
+  @override
+  List<Object> get props => [username, password];
+}
