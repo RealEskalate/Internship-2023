942112b200bbe8d635689a9c95cc3f3121bd26e2
EyasuTesfu <jt272525@gmail.com>
2023-05-11 11:30:42 +0300

    feat(matador.joshua): refactor usecase

--- Mobile/starter_project_matador/lib/features/login/Domain/usecases/login_use_case.dart
+++ Mobile/starter_project_matador/lib/features/login/Domain/usecases/login_use_case.dart
@@ -1,24 +1,37 @@
+import 'package:equatable/equatable.dart';
 import 'package:matador/features/login/Domain/repositories/user_repository.dart';
 
 import '../../../../core/failure.dart';
 import '../entities/user.dart';
 import 'package:dartz/dartz.dart';
+import '../../../../core/usecases/usecases.dart';
 
-class GetUser {
+class GetUser implements UseCase<User, AuthParams> {
   final UserRepository? repository;
 
   GetUser(this.repository);
 
-  Future<Either<Failure, User>> execute(String email, String password) async {
-    if (email.isEmpty) {
+  @override
+  Future<Either<Failure, User>> call(AuthParams params) async {
+    if (params.email.isEmpty) {
       return const Left(InvalidEmailFailure());
     }
 
-    if (password.isEmpty) {
+    if (params.password.isEmpty) {
       return const Left(InvalidPasswordFailure());
     }
 
-    final result = await repository!.authenticate(email, password);
+    final result =
+        await repository!.authenticate(params.email, params.password);
     return result;
   }
+}
+
+class AuthParams extends Equatable {
+  final String email;
+  final String password;
+  const AuthParams({required this.email, required this.password});
+
+  @override
+  List<Object?> get props => ([email, password]);
 }
