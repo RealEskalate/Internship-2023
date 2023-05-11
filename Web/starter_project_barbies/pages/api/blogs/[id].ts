93c6414a926cb93f4b281467a3ef51108d13dbf6
Nazrawi Demeke Workalemahu <nazrawi.demeke@gmail.com>
2023-05-11 14:40:01 +0300

    use api route

--- Web/starter_project_barbies/pages/api/blogs/[id].ts
+++ Web/starter_project_barbies/pages/api/blogs/[id].ts
@@ -7,25 +7,15 @@ interface Blog {
 }
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'GET') {
-    if (req.query.id) {
-      await handleGetBlogByID(req.query.id as string, res);
-    } else {
-      await handleGetBlogs(res);
-    }
+  const {
+    query: { id },
+    method,
+  } = req;
+
+  if (method === 'GET') {
+    await handleGetBlogByID(id as string, res);
   } else {
     res.status(405).json({ message: 'Method Not Allowed' });
-  }
-}
-
-async function handleGetBlogs(res: NextApiResponse) {
-  try {
-    const response = await fetch('http://localhost:5000/blogs');
-    const blogs: Blog[] = await response.json();
-
-    res.status(200).json(blogs);
-  } catch (error) {
-    res.status(500).json({ message: 'Internal Server Error' });
   }
 }
 
