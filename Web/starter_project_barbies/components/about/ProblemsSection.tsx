f8a1a7831356161d493ee3569b06987a4f2119eb
Kaleab Kindu <87898472+kkstarkk@users.noreply.github.com>
2023-05-11 09:24:29 +0300

    Kaleab.about-page (#38)
    * feat(web): implement the hero section
    
    * feat(web): implement the Problem Section components
    
    * feat(web): implement the Social Projects Section component
    
    * feat(web): implement the Session Section component
    
    * feat(web): implement the about page
    
    * feat(web): clean up about page implementation
    
    * fix(web): fix vercel deployment errors
    
    * fix(web): change image folder structure
    
    * fix(web): address pull request review changes
    
    * restore about page folder structure
    
    * fix(web): address the pull request changes requested

---
+++ Web/starter_project_barbies/components/about/ProblemsSection.tsx
@@ -0,0 +1,61 @@
+import problemItemList from '@/data/about/problem-items.json'
+import solutionItemList from '@/data/about/solution-items.json'
+
+import { ProblemItem } from '@/types/about'
+import Image from 'next/image'
+
+interface ItemCardProps {
+  item: ProblemItem
+}
+
+const ItemCard: React.FC<ItemCardProps> = ({ item:{icon, description} }) => {
+  return (
+    <div className="p-3">
+      <Image className='my-5' src={`/img/about/problems/${icon}`} width={50} height={50} alt='' />
+      <div className="max-w-md text-gray-400">{description}</div>
+    </div>
+  )
+}
+
+const ProblemsSection:React.FC = () => {
+  return (
+    <div>
+      <div className="flex gap-24 justify-center items-center mt-16 flex-wrap">
+        <div className="flex-initial order-2 md:order-1">
+          <div className="font-bold text-center md:text-left lg:text-5xl md:text-4xl sm:text-3xl xs:text-2xl max-w-lg">
+            The Problem We <span className="text-primary">Are Solving</span>
+          </div>
+          {problemItemList.map((problemItem, index) => (
+            <ItemCard key={index} item={problemItem} />
+          ))}
+        </div>
+        <Image
+          className="p-10 order-1"
+          src="/img/about/problems/amico-solve.png"
+          alt=""
+          width={450}
+          height={100}
+        />
+      </div>
+      <div className="flex justify-center gap-24 mt-20 flex-wrap">
+        <Image
+          className="flex-initial p-10 order-1"
+          src="/img/about/problems/amico-puzzle.png"
+          alt=""
+          width={500}
+          height={100}
+        />
+        <div className="flex-initial order-2">
+          <div className="font-bold text-center md:text-left lg:text-5xl md:text-4xl sm:text-3xl xs:text-2xl max-w-lg">
+            How We Are <span className="text-primary">Solving it</span>
+          </div>
+          {solutionItemList.map((solutionItem, index) => (
+            <ItemCard key={index} item={solutionItem} />
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProblemsSection
