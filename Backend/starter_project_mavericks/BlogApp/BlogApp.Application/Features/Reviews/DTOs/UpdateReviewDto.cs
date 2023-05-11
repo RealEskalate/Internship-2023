468dca7be62d41020ea62138b96f942c93c0a517
abrahamshimekt <abrahamshimektabrahamshimekt4096@gmail.com>
2023-05-11 10:05:11 +0300

    fix(backend-mavericks): change blog id type

---
+++ Backend/starter_project_mavericks/BlogApp/BlogApp.Application/Features/Reviews/DTOs/UpdateReviewDto.cs
@@ -0,0 +1,9 @@
+﻿using BlogApp.Application.Features.Common;
+
+namespace BlogApp.Application.Features.Reviews.DTOs;
+
+public class UpdateReviewDto : BaseDto
+{
+    public string Comment { get; set; }
+    public bool IsResolved { get; set; }
+}
