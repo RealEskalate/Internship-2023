9daad3473ed943e119e2427c6492ca6d078f25ab
Bruk Tedla <40060643+brukted@users.noreply.github.com>
2023-05-11 14:09:55 +0300

    feat(backend-dotnetforce7): Implement user authentication and autherization  (#150)
    * feat(backend): Create identity model
    
    * fix(backend): fix namespace typo
    
    * feat(backend): add migrations
    
    * feat(admin-frontend): Add service for Ientity and Identity contracts for application
    
    * feat(backend): Add service for Ientity and Identity contracts for application
    
    * feat(admin-frontend): fix dependency issue and clean up
    
    * feat(backend): add user controller
    
    * upgrade projects to .net 7
    
    * fix(backend): update use pk to string
    
    * feat(backend): create identity tests solution
    
    * git(backend): remove git tracked obj directory
    
    * fix(backend): rename identity tests to test
    
    * add user service tests
    
    * test(backend): add auth service tests
    
    * fix(backend-dotnetforce7): converted dotnet version back to 6.0
    
    * fix(backend-dotnetforce7): fix firstName and lastName null issue in authRessponse
    
    * fix(backend-dotnetforce7): deleted migration file
    
    * fix(dotnetforce7): add roles field
    
    * fix(dotnetforce7): address review comments
    
    * fix(dotnetforce7): fix gitignore config
    
    * fix(dotnetforce7): remove .idea folder
    
    ---------
    
    Co-authored-by: Biruk Tassew <biruktassew21@gmail.com>

---
+++ Backend/starter_project_dotnetforce7/BlogApp2/BlogApp.Identity/IdentityServicesRegistration.cs
@@ -0,0 +1,54 @@
+﻿using BlogApp.Application.Contracts.Identity;
+using BlogApp.Application.Models.Identity;
+using BlogApp.Identity.Models;
+using BlogApp.Identity.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace BlogApp.Identity
+{
+    public static class IdentityServicesRegistration
+    {
+        public static IServiceCollection ConfigureIdentityServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<JwtSettings>(configuration.GetSection("JwtSettings"));
+
+            services.AddDbContext<BlogIdentityDbContext>(options => 
+                options.UseNpgsql(configuration.GetConnectionString("BlogAppConnectionString"),
+                b => b.MigrationsAssembly(typeof(BlogIdentityDbContext).Assembly.FullName)));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<BlogIdentityDbContext>().AddDefaultTokenProviders();
+
+            services.AddTransient<IAuthService, AuthService>();
+            services.AddTransient<IUserService, UserService>();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddJwtBearer(o =>
+                {
+                    o.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ClockSkew = TimeSpan.Zero,
+                        ValidIssuer = configuration["JwtSettings:Issuer"],
+                        ValidAudience = configuration["JwtSettings:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JwtSettings:Key"]))
+                    };
+                });
+
+            return services;
+        }
+    }
+}
