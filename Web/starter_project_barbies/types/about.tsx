e22dc844ab4282590ad32fdf31157ae00f1ab48f
Kaleab Kindu <87898472+kkstarkk@users.noreply.github.com>
2023-05-11 09:24:29 +0300

    Kaleab.about-page (#38)
    * feat(web): implement the hero section
    
    * feat(web): implement the Problem Section components
    
    * feat(web): implement the Social Projects Section component
    
    * feat(web): implement the Session Section component
    
    * feat(web): implement the about page
    
    * feat(web): clean up about page implementation
    
    * fix(web): fix vercel deployment errors
    
    * fix(web): change image folder structure
    
    * fix(web): address pull request review changes
    
    * restore about page folder structure
    
    * fix(web): address the pull request changes requested

---
+++ Web/starter_project_barbies/types/about.tsx
@@ -0,0 +1,19 @@
+
+export type Project = {
+  image: string
+  name: string
+  description: string
+}
+
+export type Session = {
+  name: string
+  description: string
+  icon: string
+}
+
+export type ProblemItem = {
+  icon: string
+  description: string
+}
+
+export type SolutionItem = ProblemItem
