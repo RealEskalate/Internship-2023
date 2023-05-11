475d70badc1843e74020ca2db3b8419a3eda3a99
Nazrawi Demeke Workalemahu <nazrawi.demeke@gmail.com>
2023-05-11 16:11:19 +0300

    refactor blogs api route

--- Web/starter_project_barbies/pages/api/blogs/index.ts
+++ Web/starter_project_barbies/pages/api/blogs/index.ts
@@ -3,11 +3,7 @@ import { Blog } from '@/types/blog';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'GET') {
-    if (req.query.id) {
-      await handleGetBlogByID(req.query.id as string, res);
-    } else {
-      await handleGetBlogs(res);
-    }
+    await handleGetBlogs(res);
   } else {
     res.status(405).json({ message: 'Method Not Allowed' });
   }
@@ -19,23 +15,6 @@ async function handleGetBlogs(res: NextApiResponse) {
     const blogs: Blog[] = await response.json();
 
     res.status(200).json(blogs);
-  } catch (error) {
-    res.status(500).json({ message: 'Internal Server Error' });
-  }
-}
-
-async function handleGetBlogByID(id: string, res: NextApiResponse) {
-  try {
-    const response = await fetch(`http://localhost:5000/blogs/${id}`);
-
-    if (response.status === 404) {
-      res.status(404).json({ message: 'Blog not found' });
-      return;
-    }
-
-    const blog: Blog = await response.json();
-
-    res.status(200).json(blog);
   } catch (error) {
     res.status(500).json({ message: 'Internal Server Error' });
   }
