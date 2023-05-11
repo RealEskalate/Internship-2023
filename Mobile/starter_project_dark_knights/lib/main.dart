16dee17f42b950ae8cfa6d032f69e278c89fdce2
FenetShewarega <shewaregafenet@gmail.com>
2023-05-11 14:38:40 +0300

    update main.dart file

--- Mobile/starter_project_dark_knights/lib/main.dart
+++ Mobile/starter_project_dark_knights/lib/main.dart
@@ -1,5 +1,4 @@
-import 'package:dark_knights/features/signup/presentation/screen/signup_page.dart';
-import 'package:dark_knights/features/onboarding/presentation/screen/onboarding_page.dart';
+import 'package:dark_knights/features/splash_screen/presentation/screen/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
