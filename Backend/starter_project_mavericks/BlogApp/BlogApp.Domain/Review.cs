13e913520ffd58cb1d98d15c01c54adc149a8f67
abrahamshimekt <abrahamshimektabrahamshimekt4096@gmail.com>
2023-05-11 10:12:12 +0300

    feature(backend-mavericks): change review field

--- Backend/starter_project_mavericks/BlogApp/BlogApp.Domain/Review.cs
+++ Backend/starter_project_mavericks/BlogApp/BlogApp.Domain/Review.cs
@@ -4,7 +4,6 @@ public class Review : BaseDomainEntity
 {
     
     public string ReviewerId { get; set; }
-
     public string Comment { get; set; }
     public bool IsResolved { get; set; } = false;
      public  int  BlogId { get; set; }
