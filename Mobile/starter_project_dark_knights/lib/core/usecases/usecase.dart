be52c155c7ac2e3fa53efb661ce1ce7c69b3b5e4
FenetShewarega <shewaregafenet@gmail.com>
2023-05-11 10:38:23 +0300

    add font files

--- Mobile/starter_project_dark_knights/lib/core/usecases/usecase.dart
+++ Mobile/starter_project_dark_knights/lib/core/usecases/usecase.dart
@@ -5,8 +5,3 @@ import 'package:equatable/equatable.dart';
 abstract class UseCase<Type, Params> {
   Future<Either<Failure, Type>> call(Params params);
 }
-
-class NoParams extends Equatable {
-  @override
-  List<Object?> get props => throw UnimplementedError();
-}
