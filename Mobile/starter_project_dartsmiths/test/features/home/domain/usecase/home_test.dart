3c40e5a618329797f5d669c379549b9eb2bb88c1
Sefineh Tesfa <sefineh.tesfa@a2sv.org>
2023-05-12 11:04:52 +0300

    [Mobile] home page domain layer

---
+++ Mobile/starter_project_dartsmiths/test/features/home/domain/usecase/home_test.dart
@@ -0,0 +1,46 @@
+import 'package:dartsmiths/features/home/domain/entity/home.dart';
+import 'package:dartsmiths/features/home/domain/repository/home_repository.dart';
+import 'package:dartsmiths/features/home/domain/usecase/home_usecase.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'home_test.mocks.dart';
+
+
+@GenerateMocks([HomeRepository])
+void main() {
+  late MockHomeRepository mockHomeRepository;
+  late Search usecase;
+  
+  setUp(() => {
+        mockHomeRepository = MockHomeRepository(),
+        usecase = Search(homeRepository: mockHomeRepository),
+      });
+      
+  String term = 'Education';
+  String author = 'Jeal';
+  String title = 'Education';
+  String description = "You can reach your goal if you love working";
+  String imageUrl = "https://www.images/fake.jpg";
+  DateTime dateTime = DateTime(2023, 5, 12);
+  String tag = "Techs";
+  final home = Home(
+      author: author,
+      title: title,
+      description: description,
+      imageUrl: imageUrl,
+      dateTime: dateTime,
+      tag: tag);
+  test("Shoud search a data using term and tag", () async {
+    when(mockHomeRepository.search(term, tag))
+        .thenAnswer((_) async => Right(home));
+
+    final result = await usecase.call(Params(tag: tag, term: term));
+    expect(result, Right(home));
+    verify(mockHomeRepository.search(term, tag));
+    verifyNoMoreInteractions(mockHomeRepository);
+
+  });
+}
