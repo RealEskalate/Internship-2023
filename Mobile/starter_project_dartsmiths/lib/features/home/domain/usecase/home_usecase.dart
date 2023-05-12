2554a8b7b61946c51eefe9877bffce3a89e49d75
Sefineh Tesfa <sefineh.tesfa@a2sv.org>
2023-05-12 14:05:29 +0300

    [Mobile] conflict resolved

--- Mobile/starter_project_dartsmiths/lib/features/home/domain/usecase/home_usecase.dart
+++ Mobile/starter_project_dartsmiths/lib/features/home/domain/usecase/home_usecase.dart
@@ -11,7 +11,11 @@ class GetBytag extends UseCase<Home, Params> {
 
   @override
   Future<Either<Failure, Home>> call(Params params) async {
+<<<<<<< HEAD
     return await homeRepository.filterByTag(params.tag!);
+=======
+    return await homeRepository.filterByTag(params.tag);
+>>>>>>> 14264dae ([Mobile] home page domain layer)
   }
 }
 
@@ -21,12 +25,22 @@ class Search extends UseCase<Home, Params> {
   Search({required this.homeRepository});
   @override
   Future<Either<Failure, Home>> call(Params params) async {
+<<<<<<< HEAD
     return await homeRepository.search(params.term!, params.tag!);
+=======
+    return await homeRepository.search(params.term, params.tag);
+>>>>>>> 14264dae ([Mobile] home page domain layer)
   }
 }
 
 class Params {
+<<<<<<< HEAD
   String? term;
   String? tag;
   Params({this.tag, this.term});
+=======
+  String term;
+  String tag;
+  Params({required this.tag, required this.term});
+>>>>>>> 14264dae ([Mobile] home page domain layer)
 }
