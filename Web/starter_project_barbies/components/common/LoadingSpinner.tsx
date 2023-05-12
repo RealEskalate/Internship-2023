86da44d5666608793d78ae318a4fe842ffb0ba37
Nazrawi Demeke Workalemahu <nazrawi.demeke@gmail.com>
2023-05-12 11:18:59 +0300

    minor refactor

--- Web/starter_project_barbies/components/common/LoadingSpinner.tsx
+++ Web/starter_project_barbies/components/common/LoadingSpinner.tsx
@@ -3,8 +3,7 @@ import React from 'react';
 export const LoadingSpinner = () => {
   return (
     <div className="flex justify-center items-center h-screen">
-      <div className="w-16 h-16 border-t-4 border-b-4 border-gray-400 rounded-full animate-spin"></div>
+      <div className="w-16 h-16 border-t-4 border-b-4 border-gray-400 rounded-full animate-spin" />
     </div>
   )
 }
-
