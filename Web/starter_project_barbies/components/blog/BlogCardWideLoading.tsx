6ce9a695e40e829850f3e3ed2017015542d714f4
Nazrawi Demeke Workalemahu <nazrawi.demeke@gmail.com>
2023-05-12 11:05:43 +0300

    create loading shimmer for related blogs

--- Web/starter_project_barbies/components/blog/BlogCardWideLoading.tsx
+++ Web/starter_project_barbies/components/blog/BlogCardWideLoading.tsx
@@ -11,34 +11,34 @@ export const BlogCardWideLoading = () => {
           <div className="flex flex-col">
             {/* Author details */}
             <div className="animate-pulse flex items-center mb-4">
-              <div className="h-10 w-10 bg-gray-200 rounded-full mr-2"></div>
+              <div className="h-10 w-10 bg-gray-200 rounded-full mr-2" />
               <div className="flex flex-col">
-                <div className="h-4 w-20 bg-gray-200 rounded mb-1"></div>
-                <div className="h-3 w-16 bg-gray-200 rounded"></div>
+                <div className="h-4 w-20 bg-gray-200 rounded mb-1" />
+                <div className="h-3 w-16 bg-gray-200 rounded" />
               </div>
             </div>
             {/* Blog title */}
             <div className="animate-pulse mt-4 font-semibold text-xl">
-              <div className="h-7 w-96 bg-gray-200 rounded mb-4"></div>
+              <div className="h-7 w-96 bg-gray-200 rounded mb-4" />
             </div>
             {/* Blog description */}
             <div className="animate-pulse mt-6">
-              <div className="h-4 w-60 bg-gray-200 rounded mb-2"></div>
-              <div className="h-4 w-40 bg-gray-200 rounded"></div>
+              <div className="h-4 w-60 bg-gray-200 rounded mb-2" />
+              <div className="h-4 w-40 bg-gray-200 rounded" />
             </div>
             {/* Blog tags */}
             <div className="animate-pulse mt-6">
               <div className="flex space-x-2">
-                <div className="h-4 w-16 bg-gray-200 rounded"></div>
-                <div className="h-4 w-16 bg-gray-200 rounded"></div>
-                <div className="h-4 w-16 bg-gray-200 rounded"></div>
+                <div className="h-4 w-16 bg-gray-200 rounded" />
+                <div className="h-4 w-16 bg-gray-200 rounded" />
+                <div className="h-4 w-16 bg-gray-200 rounded" />
               </div>
             </div>
           </div>
         </div>
         {/* Blog image */}
         <div className="animate-pulse flex items-center justify-center">
-          <div className="h-36 w-96 bg-gray-200 rounded"></div>
+          <div className="h-36 w-96 bg-gray-200 rounded" />
         </div>
       </div>
     </div>
--- Web/starter_project_barbies/components/blog/RelatedBlogs.tsx
+++ Web/starter_project_barbies/components/blog/RelatedBlogs.tsx
@@ -1,44 +1,42 @@
 import BlogCard from '@/components/blog/BlogCard';
 import { useGetBlogsQuery } from '@/store/blog/blogs-api';
 import { Blog } from '@/types/blog';
+import { BlogCardLoading } from './BlogCardLoading';
 
 interface RelatedBlogsProps {
   blogs: Blog['relatedBlogs']
 }
 
-export const RelatedBlogs: React.FC<RelatedBlogsProps> = ({ blogs }) => {
-
-  const result = useGetBlogsQuery()
-
-  let content
-
-  if (result.isLoading) {
-    content = <div>Loading...</div>
+export const RelatedBlogs: React.FC<RelatedBlogsProps> = ({ blogs }): React.ReactElement | null => {
+  const result = useGetBlogsQuery();
+  
+  if (result.isError) {
+    return null
   }
   
-  else if (result.isError) {
-    content = <div>{result.error.toString()}</div>
-  }
-  
-  else if (result.isSuccess) {
-    content = result.data
-      .filter(blog => blogs.includes(blog.id))
-      .map((blog, index) => <BlogCard blog={blog} key={index} />)
-  }
+  else if (result.isSuccess || result.isLoading) {
+    return (
+      <div className='font-montserrat'>
 
-  return (
-    <div className='font-montserrat'>
+        {/* Title */}
+        <div className='text-lg pt-20 flex items-start font-black'>
+          <h1>Related Blogs</h1>
+        </div>
 
-      {/* Title */}
-      <div className='text-lg pt-20 flex items-start font-black'>
-        <h1>Related Blogs</h1>
+        {/* Related blogs */}
+        <div className='flex items-center gap-8 justify-center mt-6 text-xs'>
+          {/* Render Shimmer component if loading */}
+          { result.isSuccess &&
+              Array.from({ length: 3 }).map((_, index) => ( <BlogCardLoading key={index} /> ))}
+          {/* Render result list if success */}
+          { result.isSuccess && result.data
+              .filter(blog => blogs.includes(blog.id))
+              .map((blog, index) => <BlogCard blog={blog} key={index} />) }
+        </div>
+
       </div>
+    )
+  }
 
-      {/* Related blogs */}
-      <div className='flex items-center gap-8 justify-center mt-6 text-xs'>
-        { content }
-      </div>
-
-    </div>
-  )
+  return null
 }
