591aa488e655001d3c950209ff7af5dd2ccab4d8
Blen <62964622+blenassefa2@users.noreply.github.com>
2023-05-12 10:29:44 +0300

    feat(web-iconics): remove inconsistent naming

--- Web/starter_project_iconics/components/blog/BlogCard.tsx
+++ Web/starter_project_iconics/components/blog/BlogCard.tsx
@@ -21,7 +21,7 @@ const BlogCard: React.FC<BlogCardProps> = ({
   isMyBlogsPage,
 }) => {
   return (
-    <div className="blog w-[400px] font-montserrat flex flex-col border border-gray-300 rounded-lg overflow-hidden shadow-md bg-white h-543 my-8 font-bold">
+    <div className="w-[400px] font-montserrat flex flex-col border border-gray-300 rounded-lg overflow-hidden shadow-md bg-white h-543 my-8 font-bold">
       <img
         className="w-full h-60 object-cover"
         src={imgUrl}
@@ -29,20 +29,20 @@ const BlogCard: React.FC<BlogCardProps> = ({
       />
       <div className="p-4">
         <b>
-          <h2 className="blog_title font-bold text-lg text-gray-700 break-words overflow-hidden line-clamp-3 leading-snug">
+          <h2 className="font-bold text-lg text-gray-700 break-words overflow-hidden line-clamp-3 leading-snug">
             {title}
           </h2>
         </b>
 
-        <div className="blog_writter flex items-center mb-6 mt-4">
+        <div className="flex items-center mb-6 mt-4">
           <img
-            className="blog_writter_image w-10 h-10 object-cover rounded-full mr-2"
+            className="w-10 h-10 object-cover rounded-full mr-2"
             src={author.imageUrl}
             alt={author.name}
           />
 
-          <div className="blog_writter_details flex flex-col items-start">
-            <span className="blog_writter_name m-0 text-gray-600 leading-5">
+          <div className="flex flex-col items-start">
+            <span className="m-0 text-gray-600 leading-5">
               <b className="font-thin">&nbsp;{' by '}</b>&nbsp;
               <b>{author.name}</b>
               <b className="font-thin">
@@ -56,21 +56,21 @@ const BlogCard: React.FC<BlogCardProps> = ({
             </span>
           </div>
         </div>
-        <div className="blog_tags flex flex-row items-center mb-4">
+        <div className="flex flex-row items-center mb-4">
           {tags.map((tag: string, index: number) => (
             <span
               key={index}
-              className="blog_tag mr-2 px-2 py-1 bg-gray-100 text-gray-500 rounded-full text-xs"
+              className="mr-2 px-2 py-1 bg-gray-100 text-gray-500 rounded-full text-xs"
             >
               {tag}
             </span>
           ))}
         </div>
-        <p className="blog_description mt-3 mb-3 text-sm font-thin text-gray-500 break-words overflow-hidden line-clamp-3 leading-snug">
+        <p className="mt-3 mb-3 text-sm font-thin text-gray-500 break-words overflow-hidden line-clamp-3 leading-snug">
           {description}
         </p>
         <br />
-        <hr className="line border-gray-200" />
+        <hr className="border-gray-200" />
         <BlogFooter
           numberOfLikes={likes == undefined ? 0 : likes}
           blogStatus={status == undefined ? 'pending' : status}
