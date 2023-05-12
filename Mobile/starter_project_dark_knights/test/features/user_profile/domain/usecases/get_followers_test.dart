78d0602bd123e89fe9c7a0436fdc08229b21551c
hayattofik-01 <hayattofik22@gmail.com>
2023-05-12 10:36:09 +0300

    add usecase tests

---
+++ Mobile/starter_project_dark_knights/test/features/user_profile/domain/usecases/get_followers_test.dart
@@ -0,0 +1,75 @@
+import 'dart:ffi';
+
+import 'package:dark_knights/features/user_profile/domain/entities/user_entity.dart';
+import 'package:dark_knights/features/user_profile/domain/repositories/user_repository.dart';
+import 'package:dark_knights/features/user_profile/domain/usecases/get_followers.dart';
+import 'package:dartz/dartz.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'create_user_test.mocks.dart';
+
+
+
+
+
+@GenerateMocks([UserRepository])
+void main(){
+     late MockUserRepository mockUserRepository;
+  late GetFollower usecase;
+    setUp(() {
+ mockUserRepository = MockUserRepository();
+ usecase = GetFollower(mockUserRepository);
+    });
+    test(
+      'should get followers',
+      ()async{
+final  List<UserEntity> followers = [
+  UserEntity(
+    id: "1",
+    username: 'johndoe',
+    firstName: 'John',
+    lastName: 'Doe',
+    occupation: 'Software Developer',
+    selfDescription: 'I love coding and building cool stuff!',
+    password: 'password',
+    image: 'https://example.com/johndoe.jpg',
+  ),
+  UserEntity(
+    id: "2",
+    username: 'janedoe',
+    firstName: 'Jane',
+    lastName: 'Doe',
+    occupation: 'UX Designer',
+    selfDescription: 'I create beautiful and intuitive designs.',
+    password: 'password',
+    image: 'https://example.com/janedoe.jpg',
+  ),
+  UserEntity(
+    id: "3",
+    username: 'bobsmith',
+    firstName: 'Bob',
+    lastName: 'Smith',
+    occupation: 'Product Manager',
+    selfDescription: 'I love creating great products that delight customers.',
+    password: 'password',
+    image: 'https://example.com/bobsmith.jpg',
+  ),
+];
+
+when(mockUserRepository.getFollowers("user_123")).thenAnswer((_) async => Right(followers) );
+
+final result = await usecase("user_123");
+
+expect(result,Right(followers));
+verify(mockUserRepository.getFollowers("user_123"));
+verifyNoMoreInteractions(mockUserRepository);
+      }
+    );
+    
+ 
+}
+
+
+
