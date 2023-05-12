8d88dcdf4d8b1e97cf4b2cbcf5901124c97d592f
Nazrawi Demeke Workalemahu <nazrawi.demeke@gmail.com>
2023-05-12 15:01:33 +0300

    add comment on spinner

--- Web/starter_project_barbies/components/common/LoadingSpinner.tsx
+++ Web/starter_project_barbies/components/common/LoadingSpinner.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+// This component is used to render a loading spinner that is centered in its parent component.
 export const LoadingSpinner = () => {
   return (
     <div className="flex justify-center items-center h-screen">
