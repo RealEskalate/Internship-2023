bbbdf5d0cf29fe33c71e6b13353a49bac1854c6f
Zerihun Moges <zerihunmoges78@gmail.com>
2023-05-12 05:36:00 -0700

    destructuer teamMember

--- Web/starter_project_iconics/components/teams/TeamMemberCard.tsx
+++ Web/starter_project_iconics/components/teams/TeamMemberCard.tsx
@@ -19,24 +19,25 @@ const SocialMediaIcon: { [index: string]: any } = {
 }
 
 const TeamMemberCard: React.FC<TeamMemberCardProps> = ({teamMember}) => {
+  const {name, job, avatar, description, socialMedia} = teamMember
   return (
     <div className="flex flex-col self-start bg-white rounded-lg p-6 m-2 items-center justify-center shadow-xl max-w-[500px]">
       <Image
         className="rounded-full w-32 h-32 mt-2 bg-gray-200 object-contain"
-        src={teamMember.avatar}
+        src={avatar}
         alt="avatar"
         width={178}
         height={178}
       ></Image>
 
-      <h1 className="font-bold uppercase text-black text-2xl m-3">{teamMember.name}</h1>
-      <h2 className="uppercase text-black text-xl">{teamMember.job}</h2>
-      <p className="text-center my-4 text-xl text-[#7D7D7D]">{teamMember.description}</p>
-      <hr className="my-6 w-[100%] "></hr>
+      <h1 className="font-bold uppercase text-black text-2xl m-3">{name}</h1>
+      <h2 className="uppercase text-black text-xl">{job}</h2>
+      <p className="text-center my-4 text-xl text-[#7D7D7D]">{description}</p>
+      <hr className="my-6 w-[100%]"></hr>
 
       <div className="flex justify-around w-[100%]">
-        {teamMember.socialMedia &&
-          teamMember.socialMedia.map((social, index) => (
+        {socialMedia &&
+          socialMedia.map((social, index) => (
             <a href={social.link} key={index}>
               {' '}
               {SocialMediaIcon[social.type] || SocialMediaIcon['default']}
