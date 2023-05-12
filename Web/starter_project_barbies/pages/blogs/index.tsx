e9a3a41f23a279473d4dbdcb64defdfca906aa92
Nazrawi Demeke Workalemahu <nazrawi.demeke@gmail.com>
2023-05-12 10:17:35 +0300

    setup loading component for blog card wide

--- Web/starter_project_barbies/pages/blogs/index.tsx
+++ Web/starter_project_barbies/pages/blogs/index.tsx
@@ -1,49 +1,51 @@
-import { BlogsList } from "@/components/blog/BlogsList";
+import { BlogCardWide } from "@/components/blog/BlogCardWide";
+import { BlogCardWideLoading } from "@/components/blog/BlogCardWideLoading";
 import { Pagination } from "@/components/common/Pagination";
 import { SearchForm } from "@/components/common/SearchForm";
 import { useGetBlogsQuery } from "@/store/blog/blogs-api";
 
 const Blogs = () => {
   const result = useGetBlogsQuery()
-
-  let content
-
-  if (result.isLoading) {
-    content = <div>loading...</div>
-  } else if (result.isError) {
-    content = <div>{result.error.toString()}</div>
-  } else if (result.isSuccess) {
-    content = <BlogsList blogs={result.data} />
+  
+  if (result.isError) {
+    return <div>Error: {result.error.toString()}</div>
   }
+  
+  else if (result.isSuccess || result.isLoading) {
+    return (
+      <div className='bg-white text-primary-text font-montserrat'>
 
-  return (
-    <div className='bg-white text-primary-text font-montserrat'>
-
-      {/* Title and search bar */}
-      <div className="flex justify-center pt-20">
-        <div className='grid grid-cols-3 mt-2 w-full'>
-          <div className="flex items-center">
-            <div className='ps-28 text-xl font-semibold'>
-              Blogs
+        {/* Title and search bar */}
+        <div className="flex justify-center pt-20">
+          <div className='grid grid-cols-3 mt-2 w-full'>
+            <div className="flex items-center">
+              <div className='ps-28 text-xl font-semibold'>
+                Blogs
+              </div>
             </div>
+            <div className='flex justify-center'>
+              <SearchForm />
+            </div>
+            <div />
           </div>
-          <div className='flex justify-center'>
-            <SearchForm />
-          </div>
-          <div />
         </div>
+
+        {/* Blog list */}
+        <div className="mt-10 px-56">
+          <div className='bg-white text-primary-text'>
+            {/* Render Shimmer component if loading */}
+            { result.isLoading && Array.from({ length: 3 }).map((_, index) => ( <BlogCardWideLoading key={index} /> )) }
+            {/* Render result list if success */}
+            { result.isSuccess && result.data.map((blog, index) => <BlogCardWide blog={blog} key={index} />) }
+          </div>
+        </div>
+
+        {/* Pagination */}
+        <Pagination numberOfPages={5} />
+
       </div>
-
-      {/* Blog list */}
-      <div className="mt-10 px-56">
-        { content }
-      </div>
-
-      {/* Pagination */}
-      <Pagination numberOfPages={5} />
-
-    </div>
-  )
+    )
+  }
 }
 
 export default Blogs;
