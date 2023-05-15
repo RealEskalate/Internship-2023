d4cd9bcffeb42f6063d4f6a6f767a10328ff16c1
Brook Alemu <brookwworku3@gmail.com>
2023-05-15 01:04:39 -0900

    implenting about_us

--- Web/starter_project_iconics/components/about/SessionSection.tsx
+++ Web/starter_project_iconics/components/about/SessionSection.tsx
@@ -9,7 +9,7 @@ const SessionSection:React.FC  = () => {
     <div className= "col p-2.5 lg">
         <h1 className="font-poppins text-5xl"> <div>How we are <span className = "text-primary">solving it</span></div> </h1>
         <Image  className = " py-2.5" src = "/img/about/session-section/light-bulb-icon-3.png" alt = "info iconicon3" width = {80} height = {80}  />
-        <p className="font-sans text-2xl font-light">Offering students an ecosystem to actualize their ideas means that up-and-coming developers use their skills to benefit Africa, rather than taking their talent elsewhere.</p>
+        <p className="font-poppins text-2xl font-light">Offering students an ecosystem to actualize their ideas means that up-and-coming developers use their skills to benefit Africa, rather than taking their talent elsewhere.</p>
     </div>
 </div>
     )
