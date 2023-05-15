95a3c141b16103b669596729cfd8e1fde14da49f
Brook Alemu <brookwworku3@gmail.com>
2023-05-15 05:23:39 -0900

    implementing about-us-card

--- Web/starter_project_iconics/components/about/A2svSessionCardItem.tsx
+++ Web/starter_project_iconics/components/about/A2svSessionCardItem.tsx
@@ -8,8 +8,7 @@ interface props {
 
 function CardItem({logo, title, description}:props) {
     return (
-    <div className="cards">         
-    <div className="logo w-[350px] font-poppins flex flex-col border border-gray-300 rounded-3xl overflow-hidden shadow-md bg-white h-543 m-2 font-bold">
+    <div className="cards logo w-[350px] font-poppins flex flex-col border border-gray-300 rounded-3xl overflow-hidden shadow-md bg-white h-543 m-2 font-bold">         
     <div className="card_writter flex items-center mb-6 mt-4 p-1">
           <img
             className="card_logo w-10 h-10 object-cover rounded-full mr-2 width{400} height{400}"
@@ -27,8 +26,9 @@ function CardItem({logo, title, description}:props) {
         <p className="card_description mt-3 mb-3 font-poppins font-light p-4">
           {description}
         </p>
-        </div>
-    </div>
+      </div>
+
+    
     )
 }
 
