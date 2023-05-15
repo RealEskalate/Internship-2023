9e08e02fd24d5ad3420e4e2047206cf888375d18
Sefineh Tesfa <sefineh.tesfa@a2sv.org>
2023-05-15 11:03:08 +0300

    [Mobile] home page data layer

---
+++ Mobile/starter_project_dartsmiths/lib/features/home/domain/repository/home_repository.dart
@@ -0,0 +1,8 @@
+import 'package:dartsmiths/core/error/failures.dart';
+import 'package:dartsmiths/features/home/domain/entity/home.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class HomeRepository {
+  Future<Either<Failure, Home>> search(String term, String tag);
+  Future<Either<Failure, Home>> filterByTag(String tag);
+}
