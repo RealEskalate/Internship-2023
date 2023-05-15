6add1b1dd56159456f8cb4a50dcf7dfd44f5ff95
Brook Alemu <brookwworku3@gmail.com>
2023-05-15 04:50:07 -0900

    implemnting about-section

--- Web/starter_project_iconics/components/about/AboutSection.tsx
+++ Web/starter_project_iconics/components/about/AboutSection.tsx
@@ -1,29 +1,47 @@
 import Image from "next/image"
+
+
 const AboutSection:React.FC = () => {
     return (
 
 <div >
 <div className='about-section grid grid-cols-2 lg md:flex-row md:w-full justify-between'>
 
-    <div className='col'>
-        <h1 className="font-extrabold  text-5xl p-12"><div> <span className = "text-primary"> Africa </span> to Silicon Valley</div></h1>
-        <p className="font-sans-serif font-normal text-2xl p-12">A2SV is a social enterprise that enables high-potential university students to create digital solutions to Africa’s most pressing problems.</p>
-        <div className='button pl-12'>Meet our Team</div>
-        <p className="font-sans text-2xl p-12 italic font-light">A2SV is a social enterprise that enables high-potential university students to create digital solutions to Africa’s most pressing problems.</p>
+    <div className='col p-12'>
+        <h1 className="font-extrabold  text-5xl "><div> <span className = "text-primary"> Africa </span> to Silicon Valley</div></h1>
+        <p className="font-sans-serif font-normal text-2xl py-12">A2SV is a social enterprise that enables high-potential university students to create digital solutions to Africa’s most pressing problems.</p>
+        
+        <button className="flex flex-row items-center justify-center font-medium text-2xl rounded bg-primary text-blue-100 hover:bg-blue-600 duration-300 gap-3 p-4">
+             <>Meet our team</>
+        </button>
+
+        <p className="font-sans text-2xl py-12	 italic font-light">A2SV is a social enterprise that enables high-potential university students to create digital solutions to Africa’s most pressing problems.</p>
     </div>
     
-    <div className= "col img-col grid justify-center gap-6 grid-cols-2 py-12 lg ">
-        <div className = "container img1">
+    <div className = "col img-col grid grid-rows-2 justify-center gap-6 lg py-12 px-12 lg">
+    <div className= "col img-col grid justify-center gap-6 grid-cols-2 lg ">
+        <div className = "relative lg">
         <Image src = "/img/about/about-section/rectangle-1.png" alt = "img1" width = {388} height = {276}/>
+        <div className="absolute bottom-2/3 top-1/4 left-1/4 right-2/3 text-white text-center p-2">
+        The education process
+        </div>
         </div>
 
-        <div className='container img2 lg'>
-        <Image src = "/img/about/about-section/rectangle-2.png" alt = "img2" width = {388} height = {276}/>  
+        <div className="relative lg ">
+        <Image src = "/img/about/about-section/rectangle-2.png" alt = "img2" width = {388} height = {276}/>
+        <div className="absolute bottom-2/3 top-1/3 left-1/3 right-2/3 text-white text-center p-2">
+        Development phase
+        </div> 
         </div>
+    </div>
 
-        <div className = "container img3 ">
-    <Image src = "/img/about/about-section/rectangle-3.png" alt = "img3" width={800} height={212}/>      
+        <div className = "relative lg">
+        <Image src = "/img/about/about-section/rectangle-3.png" alt = "img3" width={800} height={212}/>
+        <div className="absolute bottom-1/4 top-1/4 left-1/4 right-1/4 text-white text-center p-2">
+        20% percent growth
+        </div>   
         </div>
+        
     </div>
 
 </div>
@@ -33,3 +51,14 @@ const AboutSection:React.FC = () => {
 }
 
 export default AboutSection
+
+function ImageWithText() {
+  return (
+    <div className="relative">
+      <img src={image} alt="Image description" />
+      <div className="absolute bottom-0 left-0 right-0 bg-black text-white text-center p-2">
+        Your text here
+      </div>
+    </div>
+  );
+}
