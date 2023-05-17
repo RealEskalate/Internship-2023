3692c09dc00a86dd71b7cd0966f9b8a9625a4bad
Natnael16 <natnaeldenbi@gmail.com>
2023-05-17 16:22:52 +0300

    (feat) Add the bloc implementation for login

--- Mobile/starter_project_dartsmiths/lib/features/authentication/presentation/bloc/auth_bloc_bloc.dart
+++ Mobile/starter_project_dartsmiths/lib/features/authentication/presentation/bloc/auth_bloc_bloc.dart
@@ -1,19 +1,32 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 
+import '../../../../core/error/failures.dart';
 import '../../domain/use_cases/login_usecase.dart';
 
 part 'auth_bloc_event.dart';
 part 'auth_bloc_state.dart';
 
 class AuthBlocBloc extends Bloc<AuthBlocEvent, AuthBlocState> {
-  AuthBlocBloc() : super(AuthBlocInitial()) {
+  final LoginUsecase loginUsecase;
+
+  AuthBlocBloc(this.loginUsecase) : super(AuthBlocInitial()) {
     on<LoginEvent>(_onAuthLoginEvent);
   }
 
-  void _onAuthLoginEvent(LoginEvent event, Emitter emit) {
+  void _onAuthLoginEvent(LoginEvent event, Emitter emit) async {
     emit(AuthBlocLoading());
 
-    final reponse = LoginUsecase
+    final failureOrAuthCredentials = await loginUsecase(UserAuthCredentialParams(username: event.username, password: event.password));
+
+    emit(_loginOrFailure(failureOrAuthCredentials));
+
+  }
+  _loginOrFailure(both) {
+    return both.fold(
+      (failure) => ServerFailure(),
+      (authCredential) =>
+          AuthBlocLoginSuccess(username: authCredential.username, password:authCredential.password),
+    );
   }
 }
