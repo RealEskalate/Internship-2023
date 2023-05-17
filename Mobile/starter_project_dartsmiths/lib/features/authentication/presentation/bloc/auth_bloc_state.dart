b57d05394b12dade102d3247af760a3aecc87452
Natnael16 <natnaeldenbi@gmail.com>
2023-05-17 16:22:52 +0300

    (feat) Add the bloc implementation for login

--- Mobile/starter_project_dartsmiths/lib/features/authentication/presentation/bloc/auth_bloc_state.dart
+++ Mobile/starter_project_dartsmiths/lib/features/authentication/presentation/bloc/auth_bloc_state.dart
@@ -2,16 +2,19 @@ part of 'auth_bloc_bloc.dart';
 
 abstract class AuthBlocState extends Equatable {
   const AuthBlocState();
-  
+
   @override
   List<Object> get props => [];
 }
 
 class AuthBlocInitial extends AuthBlocState {}
 
-
 class AuthBlocLoading extends AuthBlocState {}
 
-class AuthBlocLoginSuccess extends AuthBlocState {}
+class AuthBlocLoginSuccess extends AuthBlocState {
+  final String username;
+  final String password;
+  AuthBlocLoginSuccess({required this.username, required this.password});
+}
 
 class AuthBlocLoginFailure extends AuthBlocState {}
