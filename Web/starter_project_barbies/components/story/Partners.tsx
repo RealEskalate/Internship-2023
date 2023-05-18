9c288b958d7ca7d77941b385b3486cda163f4d06
Ekram <ekruiwc2020@gmail.com>
2023-05-18 10:52:44 +0300

    IMplement Partners-section using Redux

--- Web/starter_project_barbies/components/story/Partners.tsx
+++ Web/starter_project_barbies/components/story/Partners.tsx
@@ -1,19 +1,36 @@
+import { useGetCompaniesQuery } from '@/store/story/companies-api'
 import Image from 'next/image'
-import partners from './../../public/data/partners.json'
+import { Company } from './../../types/story'
 
 const Partners = () => {
+  const { data: partners, isError, isLoading } = useGetCompaniesQuery()
 
+  if (isError) {
+    return <div>Error: {isError.toString()}</div>
+  }
+
+  if (partners === undefined || isLoading) {
+    return <div>Loading...</div>
+  }
 
   return (
-    <div className='flex flex-col text-center text-40 lg:mt-20 lg:mx-0 md:mt-40'>
-        <h2 className='font-DM Sans mr-40 lg:mr-20 md:mr-10 text-3xl'>Current Interview Partners</h2>
-        <div className='flex flex-wrap mx-80 lg:mx-60  md:mx-5  justify-evenly gap-5 text-2xl mt-20'>
-            {partners.map((partner , index : number)=>
-            <div key={index} className='w-40 mb-5'>
-                <Image width={150} height={150} src = {partner?.image} alt = "logo" priority/>
-                </div>
-             )}
-        </div>
+    <div className="flex flex-col text-center text-40 lg:mt-20 lg:mx-0 md:mt-40">
+      <h2 className="font-DM Sans mr-40 lg:mr-20 md:mr-10 text-3xl">
+        Current Interview Partners
+      </h2>
+      <div className="flex flex-wrap mx-80 lg:mx-60  md:mx-5  justify-evenly gap-5 text-2xl mt-20">
+        {partners.map((partner: Company, index: number) => (
+          <div key={index} className="w-40 mb-5">
+            <Image
+              width={150}
+              height={150}
+              src={partner?.image}
+              alt="logo"
+              priority
+            />
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
