630352e84657e839120c5ce20099e62868cbd2be
Zerihun Moges <zerihunmoges78@gmail.com>
2023-05-18 01:46:39 -0700

    remove data object

--- Web/starter_project_iconics/pages/api/team-members.ts
+++ Web/starter_project_iconics/pages/api/team-members.ts
@@ -1,12 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import teamMembers from '../../data/teams/team-members.json'
 import { TeamMember } from '@/types/teams'
-type Data = { data: TeamMember[] }
 
 export default function getTeamMembers(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<TeamMember[]>
 ) {
-  res.status(200).json({ data: teamMembers})
+  res.status(200).json(teamMembers)
 }
 
