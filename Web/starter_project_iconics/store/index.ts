ea9fb3cc89b7bebc4e6c9669baf6d3a52c5d49ad
FevenBelay23 <91492132+FevenBelay23@users.noreply.github.com>
2023-05-18 16:06:47 +0300

    feat (web-iconics): add RTK Query to fetch data (#248)
    * create json file
    
    * add RTK query to fetch data
    
    * add RTK Query to fetch data
    
    * add RTK Query to fetch data
    
    * fix PR comment

--- Web/starter_project_iconics/store/index.ts
+++ Web/starter_project_iconics/store/index.ts
@@ -1,14 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { impactStoriesApi } from './features/home/impact-stories/impact-stories-api'
 import impactStoriesSlice from './features/home/impact-stories/impact-stories-slice'
+import { storyApi } from "./features/story/success-stories-api";
 
 export const store = configureStore({
   reducer: {
     impactStories: impactStoriesSlice,
+    [storyApi.reducerPath]: storyApi.reducer,
     [impactStoriesApi.reducerPath]: impactStoriesApi.reducer,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(impactStoriesApi.middleware),
+  middleware: (getDefaultMiddleware) => 
+    getDefaultMiddleware().concat(impactStoriesApi.middleware,storyApi.middleware),
 })
 
 export type RootState = ReturnType<typeof store.getState>
