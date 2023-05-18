a62ce480a1479b4517321143c863460af558b883
Kaleab Kindu <87898472+kkstarkk@users.noreply.github.com>
2023-05-18 21:45:57 +0300

    feat(web): implement state management and integration for about page (#194)
    * feat(web): implement state management and integration for about page
    
    * fix(web): change about page api structure
    
    * fix(web-barbies): migrate to json server for the about page integration
    
    * fix(web-barbies): relocate the about page api slice
    
    * fix(web-barbies): remove api routes of about page
    
    * fix(web-barbies): add base url to environment variable

--- Web/starter_project_barbies/components/about/ProblemsSection.tsx
+++ Web/starter_project_barbies/components/about/ProblemsSection.tsx
@@ -1,5 +1,7 @@
-import problemItemList from '@/data/about/problem-items.json'
-import solutionItemList from '@/data/about/solution-items.json'
+import {
+  useGetProblemItemsQuery,
+  useGetSolutionItemsQuery,
+} from '@/store/about/about-api'
 
 import { ProblemItem } from '@/types/about'
 import Image from 'next/image'
@@ -8,16 +10,25 @@ interface ItemCardProps {
   item: ProblemItem
 }
 
-const ItemCard: React.FC<ItemCardProps> = ({ item:{icon, description} }) => {
+const ItemCard: React.FC<ItemCardProps> = ({ item: { icon, description } }) => {
   return (
     <div className="p-3">
-      <Image className='my-5' src={`/img/about/problems/${icon}`} width={50} height={50} alt='' />
+      <Image
+        className="my-5"
+        src={`/img/about/problems/${icon}`}
+        width={50}
+        height={50}
+        alt=""
+      />
       <div className="max-w-md text-gray-400">{description}</div>
     </div>
   )
 }
 
-const ProblemsSection:React.FC = () => {
+const ProblemsSection: React.FC = () => {
+  const { data: problemItemList } = useGetProblemItemsQuery()
+  const { data: solutionItemList } = useGetSolutionItemsQuery()
+
   return (
     <div>
       <div className="flex gap-24 justify-center items-center mt-16 flex-wrap">
@@ -25,7 +36,7 @@ const ProblemsSection:React.FC = () => {
           <div className="font-bold text-center md:text-left lg:text-5xl md:text-4xl sm:text-3xl xs:text-2xl max-w-lg">
             The Problem We <span className="text-primary">Are Solving</span>
           </div>
-          {problemItemList.map((problemItem, index) => (
+          {problemItemList?.map((problemItem, index) => (
             <ItemCard key={index} item={problemItem} />
           ))}
         </div>
@@ -49,7 +60,7 @@ const ProblemsSection:React.FC = () => {
           <div className="font-bold text-center md:text-left lg:text-5xl md:text-4xl sm:text-3xl xs:text-2xl max-w-lg">
             How We Are <span className="text-primary">Solving it</span>
           </div>
-          {solutionItemList.map((solutionItem, index) => (
+          {solutionItemList?.map((solutionItem, index) => (
             <ItemCard key={index} item={solutionItem} />
           ))}
         </div>
