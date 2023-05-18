c7eea044fd984564c05b2436e8064f6c9cf0bb8e
kkstarkk <kkmichaelstarkk@gmail.com>
2023-05-18 13:36:48 +0300

    fix(web-barbies): add base url to environment variable

--- Web/starter_project_barbies/store/about/about-api.ts
+++ Web/starter_project_barbies/store/about/about-api.ts
@@ -1,9 +1,11 @@
 import { ProblemItem, Project, Session, SolutionItem } from '@/types/about'
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
+
 export const aboutApi = createApi({
   reducerPath: 'about',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
   endpoints: (build) => ({
     getProjects: build.query<Project[], void>({
       query: () => 'projects',
