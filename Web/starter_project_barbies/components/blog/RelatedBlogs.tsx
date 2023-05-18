87a30414768966627f68b046e656f68531a2b916
Nazrawi Demeke <dnazrawi@gmail.com>
2023-05-18 15:21:03 +0300

    feat(web-barbies): implement state management for blogs page (#197)
    * create blogs slice
    
    * setup useSelector for single blog
    
    * setup logic for related blogs
    
    * add key for pagination items
    
    * refactor blog selectors
    
    * setup json-server
    
    * migrate blogsSlice to apiSlice
    
    * use api route
    
    * move blogs api route
    
    * move hooks to dedicated folder
    
    * address comments
    
    * refactor blogs api route
    
    * change folder name in store
    
    * remove db.json
    
    * restore db.json
    
    * setup loading component for blog card wide
    
    * setup loading spinner on single blog page
    
    * create loading shimmer for related blogs
    
    * minor bug fix
    
    * minor refactor
    
    * rename loading to shimmer
    
    * rename to relatedBlogs
    
    * add comment on spinner
    
    * rename hooks file
    
    * minor refactor
    
    * remove pages api routes
    
    * move base url to .env file
    
    * remove unnecessary else clauses
    
    * rename variables
    
    * add comment
    
    * address comments
    
    * show no related blogs message when result is empty

--- Web/starter_project_barbies/components/blog/RelatedBlogs.tsx
+++ Web/starter_project_barbies/components/blog/RelatedBlogs.tsx
@@ -1,27 +1,46 @@
 import BlogCard from '@/components/blog/BlogCard';
-import { Blog } from "@/types/blog";
+import { useGetBlogsQuery } from '@/store/blog/blogs-api';
+import { Blog } from '@/types/blog';
+import { BlogCardShimmer } from './BlogCardShimmer';
 
 interface RelatedBlogsProps {
-  blog: Blog
+  relatedBlogs: Blog['relatedBlogs']
 }
 
-export const RelatedBlogs: React.FC<RelatedBlogsProps> = ({ blog }) => {
-  // Fetch related blogs
-  let relatedBlogs: Blog[] = new Array(3).fill(blog);
+export const RelatedBlogs: React.FC<RelatedBlogsProps> = ({ relatedBlogs }): React.ReactElement | null => {
+  const { data, isSuccess, isLoading, isError, error } = useGetBlogsQuery()
+  
+  // In case of error, the component shouldn't be rendered
+  if (isError) {
+    return <div>{error.toString()}</div>
+  }
+  
+  if (isSuccess || isLoading) {
+    return (
+      <div className='font-montserrat'>
 
-  return (
-    <div className='font-montserrat'>
+        {/* Title */}
+        <div className='text-lg pt-20 flex items-start font-black'>
+          <h1>Related Blogs</h1>
+        </div>
 
-      {/* Title */}
-      <div className='text-lg pt-20 flex items-start font-black'>
-        <h1>Related Blogs</h1>
+        {/* Related blogs */}
+        <div className='flex items-center gap-8 justify-center mt-6 text-xs'>
+          {/* Render Shimmer component if loading */}
+          { isLoading &&
+              Array.from({ length: 3 }).map((_, index) => ( <BlogCardShimmer key={index} /> ))}
+          {/* Render empty list if result is empty */}
+          { (isSuccess && data.length == 0) &&
+              <div>No related blogs</div> }
+          {/* Render result list if success */}
+          { (isSuccess && data.length > 0) && data
+              .filter(blog => relatedBlogs.includes(blog.id))
+              .map((blog, index) => <BlogCard blog={blog} key={index} />) }
+        </div>
+
       </div>
+    )
+  }
 
-      {/* Related blogs */}
-      <div className='flex items-center gap-8 justify-center mt-6 text-xs'>
-        { relatedBlogs.map((blog) => (BlogCard({ blog }))) }
-      </div>
-
-    </div>
-  )
+  return null
 }
--- Web/starter_project_barbies/store/store.ts
+++ Web/starter_project_barbies/store/store.ts
@@ -1,7 +1,12 @@
+import { blogApi } from '@/store/blog/blogs-api'
 import { configureStore } from '@reduxjs/toolkit'
 
 export const store = configureStore({
-  reducer: {}
+  reducer: {
+    [blogApi.reducerPath]: blogApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(blogApi.middleware),
 })
 
 export type RootState = ReturnType<typeof store.getState>
