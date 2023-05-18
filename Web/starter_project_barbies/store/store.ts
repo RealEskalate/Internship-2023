2a78b336d8e4bdcf8e8de942d367da6b58b5c642
Ekram <ekruiwc2020@gmail.com>
2023-05-18 10:57:29 +0300

    Update store

--- Web/starter_project_barbies/store/store.ts
+++ Web/starter_project_barbies/store/store.ts
@@ -1,7 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { companyApi } from './story/companies-api'
+import { storyApi } from './story/story-api'
 
 export const store = configureStore({
-  reducer: {}
+  reducer: {
+    [storyApi.reducerPath]: storyApi.reducer,
+    [companyApi.reducerPath]: companyApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(storyApi.middleware).concat(companyApi.middleware)
 })
 
 export type RootState = ReturnType<typeof store.getState>
