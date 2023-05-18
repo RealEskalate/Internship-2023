e3836443a9474c32c2969907aefed2fc0df885cf
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-05-18 11:09:54 +0300

    Mobile.duressa jemal.user.data layer and bloc (#214)
    * feat(mobile-matador): add user model
    
    * feat(mobile-matador): Add data layer implementation for user feature
    
    * fix(mobile-matador): remove local storage
    
    * feat(mobile-matador): Add Bloc for user feature

--- Mobile/starter_project_matador/lib/features/user/domain/repositories/user_repository.dart
+++ Mobile/starter_project_matador/lib/features/user/domain/repositories/user_repository.dart
@@ -4,5 +4,10 @@ import 'package:matador/core/error/failures.dart';
 import 'package:matador/features/user/domain/entities/user.dart';
 
 abstract class UserRepository {
+
+  Future<Either<Failure, User>> addUser(User user);
   Future<Either<Failure, User>> getUserById(String id);
+  Future<Either<Failure, User>> editUserById(User user);
+  Future<Either<Failure, void>> deleteUserById(String id);
+
 }
