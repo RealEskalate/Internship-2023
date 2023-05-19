bfded48f8ef4f0edf49dd8aa7f8a71ce2bd8461e
Sefineh Tesfa <88246220+sefinehtesfa34@users.noreply.github.com>
2023-05-19 15:44:52 +0300

    feat(mobile.dartsmith): Home page data layer implementation (#266)
    * Home page data layer implementation
    
    * removed unnecessary file
    
    * add throw serverException

---
+++ Mobile/starter_project_dartsmiths/lib/features/feed/home/domain/repository/home_repository.dart
@@ -0,0 +1,7 @@
+import 'package:dartsmiths/core/error/failures.dart';
+import 'package:dartsmiths/features/feed/home/domain/entity/home.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class HomeRepository {
+  Future<Either<Failure, List<Home>>> search(String term, String tag);
+}
