9993d61622b13773b4424971e6cdae6bd468dea7
Rediet-Ferew <91590944+Rediet-Ferew@users.noreply.github.com>
2023-05-19 15:15:17 +0300

    feat(web-vanguard): implement home page components (#152)
    * implement home page components

---
+++ Web/starter_project_vanguard/components/home/HelpUs.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+const HelpUs: React.FC  = () => {
+
+  return (
+    
+    <div className="mb-20">
+      <div className="relative mt-20 bg-fixed h-72 aspect-auto">
+        <Image
+          src="/img/home/africa.png"
+          alt="African image for background"
+          className="w-full bg-opacity-100 aspect-auto"
+          fill
+        />
+        <div className="relative z-10 flex flex-col items-center justify-center h-full mt-28">
+          <h1 className="text-2xl font-bold text-center text-gray-200">
+            Help us sustain our mission!
+          </h1>
+          <div className="px-8 py-3 mt-6 ml-4 font-bold text-primary bg-white rounded whitespace-nowrap">
+            <Link href="/">Support Us</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default HelpUs
---
+++ Web/starter_project_vanguard/components/home/SuccessRate.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import YearlySuccess from './YearlySuccess'
+import successRates from '../../data/home/success-rates.json'
+import activities from '../../data/home/activities.json'
+import Activities from './Activities'
+
+const SuccessRate: React.FC = () => {
+  return (
+    <div>
+      <div className="flex flex-col items-center justify-center mb-20 mt-30">
+        <h1 className="text-3xl font-semibold text-center mx-7">
+          Google SWE Interviews Acceptance
+          <span className="sm:block ml-15"> Rate Comparison </span>
+        </h1>
+      </div>
+
+      <div className="py-7 flex flex-col items-center mb-20 sm:justify-center bg-zinc-200 mx-7 rounded-xl">
+        <div className="self-center w-2/4 py-3 mx-4 text-center aspect-auto">
+          <p className="text-lg text-primary-text">
+            A2SV students are <span className="font-semibold">35</span> times
+            more likely to pass
+            <span className="font-semibold"> Google SWE interviews </span>
+            than average candiadates.
+          </p>
+        </div>
+
+        <div className="flex flex-col items-center w-full grow sm:flex-row sm:justify-center">
+          {successRates.map((info) => (
+            <YearlySuccess
+              id={info.id}
+              year={info.year}
+              success={info.success}
+              average={info.average}
+              key={info.id}
+            />
+          ))}
+        </div>
+      </div>
+
+      {activities.map((activity) => (
+        <Activities
+          id={activity.id}
+          url={activity.url}
+          title={activity.title}
+          description={activity.description}
+          altText={activity.altText}
+          key={activity.id}
+        />
+      ))}
+    </div>
+  )
+}
+
+export default SuccessRate
