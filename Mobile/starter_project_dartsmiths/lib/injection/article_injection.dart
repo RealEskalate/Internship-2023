923c6a78b431681e6ec666414194b6542e41b278
Hiwot Derese <Hiwotderese16@gmail.com>
2023-05-19 14:32:31 +0300

    add article injection

--- Mobile/starter_project_dartsmiths/lib/injection/article_injection.dart
+++ Mobile/starter_project_dartsmiths/lib/injection/article_injection.dart
@@ -1,8 +1,7 @@
 import 'package:dartsmiths/features/article/presentation/bloc/article_bloc.dart';
-import 'package:data_connection_checker/data_connection_checker.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:get_it/get_it.dart';
 import 'package:http/http.dart' as http;
-import 'package:shared_preferences/shared_preferences.dart';
 
 import '../core/network/intwork_info.dart';
 import '../features/article/data/datasources/article_remote_data_source.dart';
@@ -11,16 +10,10 @@ import '../features/article/domain/repositories/article_repository.dart';
 import '../features/article/domain/usecases/get_article.dart';
 import '../features/article/domain/usecases/post_article.dart';
 import '../features/article/domain/usecases/update_article.dart';
-// import '../features';
 
 final sl = GetIt.instance;
 
 Future<void> init() async {
-
-  // sl.registerFactory(
-  //   () => ArticleBloc(postArticle: sl(), updateArticle:sl(), getArticle:sl())
-  // );
-
   sl.registerFactory(() => ArticleBloc(postArticle: sl(), updateArticle: sl(), getArticle: sl()));
 
   // Use cases
@@ -41,8 +34,6 @@ Future<void> init() async {
   sl.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(sl()));
 
   // External
-  final sharedPreferences = await SharedPreferences.getInstance();
-  sl.registerLazySingleton(() => sharedPreferences);
   sl.registerLazySingleton(() => http.Client());
-  sl.registerLazySingleton(() => DataConnectionChecker());
+  sl.registerLazySingleton(() => InternetConnectionChecker());
 }
