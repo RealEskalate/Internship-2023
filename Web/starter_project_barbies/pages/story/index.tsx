a6cd95a8bee675ddb45125ccf61ef5fd3ee5fa0b
Ekram <ekruiwc2020@gmail.com>
2023-05-19 15:12:53 +0300

    Clear files

--- Web/starter_project_barbies/pages/story/index.tsx
+++ Web/starter_project_barbies/pages/story/index.tsx
@@ -2,8 +2,6 @@ import Title from '../../components/story/Title'
 import  Profile  from '../../components/story/SuccessStory'
 import Partners from '../../components/story/Partners'
 
-
-
 export default function Home() {
   return (
     <div>
