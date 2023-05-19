f106da9535fd9f9742f5a9508ef345e644fe1ff7
Natnael Tadele <91603089+Natnael16@users.noreply.github.com>
2023-05-19 14:47:20 +0300

    (feat/dartsmiths/AuthenticaionDataLayer) Add the data layer for the authentication part (#249)
    * (feat) Added model, repository implementation and remote datasource for authentication feature along with some fixes on core folder
    
    * (feat) data layer for authentication with tests and some minor modifications
    
    * (feat) Add the bloc implementation for login
    
    * (feat) Add dependency injection for authenticaiton
    
    * (feat) get_it package added
    
    * (fix) bloc file naming fix
    
    ---------
    
    Co-authored-by: Natnael16 <natnaeldenbi@gmail.com>

--- Mobile/starter_project_dartsmiths/lib/features/authentication/domain/repository/login_repository.dart
+++ Mobile/starter_project_dartsmiths/lib/features/authentication/domain/repository/login_repository.dart
@@ -1,6 +1,7 @@
-import 'package:dartsmiths/core/errors/failures.dart';
 import 'package:dartsmiths/features/authentication/domain/entity/authentication_payload.dart';
 import 'package:dartz/dartz.dart';
+
+import '../../../../core/error/failures.dart';
 
 abstract class AuthenticationRepository {
   Future<Either<Failure, UserAuthCredential>> login(
--- Mobile/starter_project_dartsmiths/lib/main.dart
+++ Mobile/starter_project_dartsmiths/lib/main.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 
 import 'features/feed/presentation/screen/home_page.dart';
+import 'injection/injection.dart' as dependency_injection;
 
 void main() {
+  dependency_injection.init();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
