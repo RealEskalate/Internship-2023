2c03334e380c37a580606a05c92ce564f1478c24
Sefineh Tesfa <sefineh.tesfa@a2sv.org>
2023-05-20 12:07:07 +0300

    Home bloc consumer

--- Mobile/starter_project_dartsmiths/lib/features/feed/home/data/datasource/home_remote.dart
+++ Mobile/starter_project_dartsmiths/lib/features/feed/home/data/datasource/home_remote.dart
@@ -8,45 +8,20 @@ import '../model/home.dart';
 abstract class HomeRemoteDataSource {
   Future<List<Home>> search(String term, String tag);
 }
-
 class HomeRemoteDataSourceImpl extends HomeRemoteDataSource {
   final http.Client client;
   HomeRemoteDataSourceImpl({required this.client});
 
   @override
   Future<List<Home>> search(String term, String tag) async {
-    // https://www.themoviedb.org/movie/now-playing?api_key=80f12d8d0d2c3bf2fdc8027e32702796
-    // https://www.themoviedb.org/search?query=software
-    // https://mocki.io/v1/5b69d35d-eacc-490f-8d59-f96f7fcb4628
-    final List<dynamic> jsonData = [
-      {
-        "author": "Sefineh",
-        "title": "Education",
-        "description": "Software engineer",
-        "tag": "Development",
-        "imageUrl": "https://fake.png",
-        "dateTime": "2023:00:00"
-      },
-      {
-        "author": "Sefineh",
-        "title": "Education",
-        "description": "Software engineer",
-        "tag": "Development",
-        "imageUrl": "https://fake.png",
-        "dateTime": "2023:00:00"
-      },
-    ];
-    const unencodedPath = "/v1/5b69d35d-eacc-490f-8d59-f96f7fcb4628",
+    const unencodedPath = "/v1/59a357bb-37c3-469e-8de6-63130a8ae6af",
         authority = 'www.mocki.io';
-    final uri = _getUri(authority, unencodedPath);
+    final queryParameters = {"query": term, "tag": tag};
+    final uri = _getUri(authority, unencodedPath, queryParameters);
     final response = await _filterFromUrl(uri);
     if (response.statusCode == 200) {
-      // final dynamic jsonData = json.decode(response.body);
-      // jsonData.map((value) => print(value));
+      List<dynamic> jsonData = json.decode(response.body)['data'];
       return jsonData.map<Home>((data) => HomeModel.fromJson(data)).toList();
-      // return List<Home>.from(json.decode(response.body).cast<Map<String, dynamic>>())
-      //     .map((home) => HomeModel.fromJson(home as Map<String, dynamic>))
-      //     .toList();
     }
     throw ServerException();
   }
@@ -55,7 +30,7 @@ class HomeRemoteDataSourceImpl extends HomeRemoteDataSource {
     return client.get(uri, headers: {});
   }
 
-  Uri _getUri(String authority, String unencodedPath) {
-    return Uri.https(authority, unencodedPath);
+  Uri _getUri(String authority, String unencodedPath, Object?queryParameters) {
+    return Uri.https(authority, unencodedPath, queryParameters as Map<String, dynamic>?);
   }
 }
