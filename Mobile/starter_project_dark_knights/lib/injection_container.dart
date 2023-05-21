d32d5fb47d992b7289e31a3fcb030d74639c204c
Yared Tsegaye <yaredtsegaye120@gmail.com>
2023-05-21 20:02:07 +0300

    fix(Mobile-Dartknights): fix comments

--- Mobile/starter_project_dark_knights/lib/injection_container.dart
+++ Mobile/starter_project_dark_knights/lib/injection_container.dart
@@ -1,4 +1,4 @@
-import 'package:dark_knights/article_injection.dart';
+import 'package:dark_knights/article_injection_container.dart';
 import 'package:get_it/get_it.dart';
 
 import 'user_injection_container.dart';
@@ -6,8 +6,10 @@ import 'user_injection_container.dart';
 final sl = GetIt.instance;
 
 Future<void> init() async{
-  //! Features - Number Trivia
+  //! Features - User
   await userInjectionInit();
+
+  //! Features - Article
   await articleInjectionInit();
 
   //! Core
