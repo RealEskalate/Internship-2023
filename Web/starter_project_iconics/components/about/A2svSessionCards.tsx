e4226e76f5ea7f3fc106054e455c7125471fa7ed
Brook Alemu <brookwworku3@gmail.com>
2023-05-22 00:24:36 -0900

    about_us_page

--- Web/starter_project_iconics/components/about/A2svSessionCards.tsx
+++ Web/starter_project_iconics/components/about/A2svSessionCards.tsx
@@ -1,14 +1,14 @@
-import CardItem from "@/components/about/A2svSessionCardItem"
+import CardItem from "@/components/about/A2svSessionCardItem";
 import cardsData from "../../data/about/about-card.json";
 
-
 const Cards = () => {
-
-return <section className="grid grid-cols-3 grid-rows-2 px-12 ">
-    {cardsData.map(({logo, title, description}) => {
-        return <CardItem title={title} logo = {logo} description = {description} />
-    })}
-    </section> 
-}
+  return (
+    <section className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 pl-12">
+      {cardsData.map(({ logo, title, description }) => {
+        return <CardItem title={title} logo={logo} description={description} />;
+      })}
+    </section>
+  );
+};
 
 export default Cards;
