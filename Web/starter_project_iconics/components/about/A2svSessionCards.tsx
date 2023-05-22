19b516efac6843b3231fa258e84f6971d41647b5
Brook Alemu <brookwworku3@gmail.com>
2023-05-22 05:43:02 -0900

    about-us

--- Web/starter_project_iconics/components/about/A2svSessionCards.tsx
+++ Web/starter_project_iconics/components/about/A2svSessionCards.tsx
@@ -1,7 +1,7 @@
 import CardItem from "@/components/about/A2svSessionCardItem";
 import cardsData from "../../data/about/about-card.json";
 
-const Cards = () => {
+const Cards:React.FC = () => {
   return (
     <section className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 pl-12">
       {cardsData.map(({ logo, title, description }) => {
