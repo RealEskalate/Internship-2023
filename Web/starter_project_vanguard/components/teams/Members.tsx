101e6a08069a78f4e1dc3aac6aef1e28c8dd62d6
Rediet-Ferew <redietasnakech@gmail.com>
2023-05-22 15:08:00 +0300

    remove unwanted imports

--- Web/starter_project_vanguard/components/teams/Members.tsx
+++ Web/starter_project_vanguard/components/teams/Members.tsx
@@ -1,6 +1,5 @@
 import React, {useEffect, useState} from 'react'
 import MemberCard from './MemberCard'
-// import teamMembers from '../../data/team-members.json'
 import { useGetTeamMembersQuery } from '@/store/features/teams/teams-api'
 import Pagination from '../common/pagination'
 
