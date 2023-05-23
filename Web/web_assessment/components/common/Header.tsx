f3bb722ab07d1be1bfe5a69b3d0c46d6b774d6e4
Nazrawi Demeke Workalemahu <nazrawi.demeke@gmail.com>
2023-05-23 16:02:11 +0300

    add image

--- Web/web_assessment/components/common/Header.tsx
+++ Web/web_assessment/components/common/Header.tsx
@@ -1,8 +1,17 @@
 import React from "react";
+import Image from 'next/image'
 
 const Header: React.FC = () => {
   return (
-    <header className="h-24" />
+    <header className="flex justify-between">
+      <Image
+        src="/img/logo.png"
+        alt="logo"
+        width={160}
+        height={8}
+        className="mt-8 ml-36"
+      />
+    </header>
   );
 };
 
