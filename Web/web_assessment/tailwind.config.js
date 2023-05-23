817846aa70336da3fa210945e4a89f9e7cf9db4e
Nazrawi Demeke Workalemahu <nazrawi.demeke@gmail.com>
2023-05-23 15:49:22 +0300

    setup font

--- Web/web_assessment/tailwind.config.js
+++ Web/web_assessment/tailwind.config.js
@@ -16,6 +16,9 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily: {
+        'poppins': '"Poppins"',
+      }
     },
   },
   plugins: [],
