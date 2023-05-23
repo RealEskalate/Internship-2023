0b87efe00146b7c236c5d1ae1850b119364674da
aklile-yilma <aklileyilma@gmail.com>
2023-05-23 16:04:35 +0300

    add padding for footer

--- Web/web_assessment/components/layout/Header.tsx
+++ Web/web_assessment/components/layout/Header.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const Header = () => {
   return (
-    <div className="bg-white flex justify-between">
+    <div className="bg-white flex justify-between px-20">
       {/* logo */}
       <section className="flex gap-2">
         <Image
