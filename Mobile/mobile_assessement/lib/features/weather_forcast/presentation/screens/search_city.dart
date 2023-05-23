7e8a6f24ad1de4acd23b7f8b540fd91adf5d13fe
Simon Mekonnen <simonmekonnen80@gmail.com>
2023-05-23 03:52:16 -0800

    bloc consuming

--- Mobile/mobile_assessement/lib/features/weather_forcast/presentation/screens/search_city.dart
+++ Mobile/mobile_assessement/lib/features/weather_forcast/presentation/screens/search_city.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mobile_assessement/features/weather_forcast/presentation/bloc/weather_event.dart';
 import 'package:mobile_assessement/features/weather_forcast/presentation/screens/weather.dart';
+
+import '../bloc/weather_bloc.dart';
 
 class SearchCity extends StatelessWidget {
   const SearchCity({super.key});
+  
 
   @override
   Widget build(BuildContext context) {
+    final TextEditingController controller = TextEditingController();
     return Scaffold(
       backgroundColor:
           const Color(0xFFF2F2F2), // Sets the background color to #F2F2F2
@@ -28,6 +33,7 @@ class SearchCity extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.all(16.0),
             child: TextField(
+              controller: controller,
               decoration: InputDecoration(
                 border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(10.0)),
@@ -36,7 +42,7 @@ class SearchCity extends StatelessWidget {
                 hintText: "Search",
                 suffixIcon: IconButton(
                   onPressed: () {
-                    
+                    context.read<WeatherBloc>().add(GetWeatherForecastEvent(controller.text));
                     Navigator.push(
                         context,
                         MaterialPageRoute(
--- Mobile/mobile_assessement/lib/features/weather_forcast/presentation/screens/weather.dart
+++ Mobile/mobile_assessement/lib/features/weather_forcast/presentation/screens/weather.dart
@@ -1,117 +1,137 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mobile_assessement/features/weather_forcast/presentation/bloc/weather_state.dart';
+
+import '../bloc/weather_bloc.dart';
 
 class WeatherScreen extends StatelessWidget {
   const WeatherScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                IconButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    icon: Icon(Icons.arrow_back)),
-                Column(
-                  children: [
-                    Text(
-                      "Today's Date",
-                      style: TextStyle(
-                        color: Color(0xFF211772),
-                        fontSize: 16.0,
-                      ),
-                    ),
-                    Text("sun,12,2023")
-                  ],
-                ),
-                IconButton(
-                  icon: Icon(Icons.favorite_border),
-                  onPressed: () {},
-                ),
-              ],
-            ),
-          ),
-          Expanded(
-            child: Container(
-              padding: EdgeInsets.all(16.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
+    return SafeArea(
+      child: Scaffold(
+        body: BlocBuilder<WeatherBloc, WeatherState>(
+          builder: (context, state) {
+            if (state is WeatherLoading) {
+              return Center(child: CircularProgressIndicator());
+            } else if (state is WeatherLoaded) {
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-                  Image.asset(
-                    "images/rainy_lightning_windy_sunny.png",
-                    height: 128.0,
-                  ),
-                  SizedBox(height: 16.0),
-                  Text(
-                    "Mostly Sunny",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 24.0,
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        IconButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            icon: Icon(Icons.arrow_back)),
+                        Column(
+                          children: [
+                            Text(
+                              "Today's Date",
+                              style: TextStyle(
+                                color: Color(0xFF211772),
+                                fontSize: 16.0,
+                              ),
+                            ),
+                            Text("sun,12,2023")
+                          ],
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.favorite_border),
+                          onPressed: () {},
+                        ),
+                      ],
                     ),
                   ),
-                  SizedBox(height: 16.0),
-                  Text(
-                    "76°F",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 48.0,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Container(
-            height: 240.0,
-            color: Color(0xFF211772),
-            child: ListView.builder(
-              itemCount: 6,
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 8.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        "Date",
-                        style: TextStyle(fontSize: 16.0, color: Colors.white),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  Expanded(
+                    child: Container(
+                      padding: EdgeInsets.all(16.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
                         children: [
-                          SizedBox(width: 8.0),
+                          Image.asset(
+                            "images/rainy_lightning_windy_sunny.png",
+                            height: 128.0,
+                          ),
+                          SizedBox(height: 16.0),
                           Text(
-                            "High",
+                            "Mostly Sunny",
                             style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 16.0,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 24.0,
                             ),
                           ),
-                          SizedBox(width: 8.0),
+                          SizedBox(height: 16.0),
                           Text(
-                            "Low",
+                            "76°F",
                             style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 16.0,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 48.0,
                             ),
                           ),
                         ],
                       ),
-                    ],
+                    ),
                   ),
-                );
-              },
-            ),
-          ),
-        ],
+                  Container(
+                    height: 240.0,
+                    color: Color(0xFF211772),
+                    child: ListView.builder(
+                      itemCount: 6,
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding:
+                              const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 8.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                "Date",
+                                style: TextStyle(
+                                    fontSize: 16.0, color: Colors.white),
+                              ),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  SizedBox(width: 8.0),
+                                  Text(
+                                    "High",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 16.0,
+                                    ),
+                                  ),
+                                  SizedBox(width: 8.0),
+                                  Text(
+                                    "Low",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 16.0,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              );
+            } else if (state is WeatherError) {
+              return Text(state.failure.toString());
+            }
+            return Text("test");
+          },
+        ),
       ),
     );
   }
