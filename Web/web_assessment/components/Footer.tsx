f46296bdb9fda808933cabe0c1e8ef2cfaebf57b
zerabruckmuluneh <zerabruck@a2sv.org>
2023-05-23 15:58:54 +0300

    add footer

--- Web/web_assessment/components/Footer.tsx
+++ Web/web_assessment/components/Footer.tsx
@@ -1,30 +1,52 @@
 import React from 'react'
+import SiFacebook from "react-icons/si"
+import {RiFacebookCircleFill} from "react-icons/ri"
+import {FaLinkedinIn} from "react-icons/fa"
+import {BsTwitter} from "react-icons/bs"
+import {GrInstagram} from "react-icons/gr"
 
 function Footer() {
   return (
-    <div>
+    <div className = "bg-primary text-white pr-24 ">
+        <div className = " flex justify-between p-10">
         <div>
-            <p>HakimHub</p>
+            <p className = "text-3xl font-bold">HakimHub</p>
         </div>
-        <div>
+        <div className = "flex gap-8">
             <div>
-                <h1>Get Connected</h1>
-                <p>For Physicians</p>
-                <p>For Hospitals</p>
+                <h1 className = "font-bold">Get Connected</h1>
+                <p className = "py-2"> > For Physicians</p>
+                <p> > For Hospitals</p>
             </div>
             <div>
-                <h1>Actions</h1>
-                <p>Find a doctor</p>
-                <p>Find a hospital</p>
+                <h1 className = "font-bold">Actions</h1>
+                <p  className = "py-2"> > Find a doctor</p>
+                <p> > Find a hospital</p>
             </div>
             <div>
-                <h1>Company</h1>
-                <p>About us</p>
-                <p>carrer</p>
-                <p>join our team</p>
+                <h1  className = "font-bold">Company</h1>
+                <p className = "py-2"> > About us</p>
+                <p className = "pb-2"> > carrer</p>
+                <p> > join our team</p>
             </div>
 
         </div>
+        
+    </div>
+    <div className = "border-t-2 flex justify-between p-4">
+        <div className = " flex gap-10" >
+            <p>Privacy Policy</p>
+            <p>Term of use</p>
+        </div>
+        <div className = "flex justify-evenly gap-9">
+            {/* <SiFacebook></SiFacebook> */}
+            <RiFacebookCircleFill></RiFacebookCircleFill>
+            <FaLinkedinIn></FaLinkedinIn>
+            <GrInstagram></GrInstagram>
+            <BsTwitter></BsTwitter>
+
+        </div>
+    </div>
     </div>
   )
 }
--- Web/web_assessment/pages/_app.tsx
+++ Web/web_assessment/pages/_app.tsx
@@ -2,10 +2,12 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
 import { store } from '@/store/store'
+import Footer from '@/components/Footer'
 
 export default function App({ Component, pageProps }: AppProps) {
   return<Provider store={store}>
 
   <Component {...pageProps} />
+  <Footer/>
   </Provider>
 }
