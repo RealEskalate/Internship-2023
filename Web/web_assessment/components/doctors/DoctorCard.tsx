4c348f9025307cd94c2025bf787e183925e52062
Zerihun Moges <zerihunmoges78@gmail.com>
2023-05-23 05:20:52 -0700

    fix issue

--- Web/web_assessment/components/doctors/DoctorCard.tsx
+++ Web/web_assessment/components/doctors/DoctorCard.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import doctor from './doctor.json'
 import SpecialityTag from './SpecialityTag'
 import Image from 'next/image'
 interface DoctorCardProps{
@@ -16,7 +15,7 @@ const DoctorCard: React.FC<DoctorCardProps> = ({_id, fullname, photo, speciality
     <div className='w-80 min-w-80 items-center p-2 pt-12'>
 
         <div className='bg-bgsecondary flex flex-col justify-center rounded-3xl items-center gap-1'>
-            <Image unoptimized className='rounded-full border-2 border-primary -mt-[45px]' src={photo} alt={doctor.fullName} width={90} height={90}></Image>
+            <Image unoptimized className='rounded-full border-2 border-primary -mt-[45px]' src={photo} alt={fullname} width={90} height={90}></Image>
             <p className='font-bold'> {fullname}</p>
         
             <SpecialityTag title={speciality}></SpecialityTag>
