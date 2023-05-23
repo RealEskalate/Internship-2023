635a5a677b5d4c5e6262155797638e0216267979
Yared Tsegaye <yaredtsegaye120@gmail.com>
2023-05-23 08:26:55 +0300

    (Mobile): add home page

--- Mobile/mobile_assessement/lib/core/utils/colors.dart
+++ Mobile/mobile_assessement/lib/core/utils/colors.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/material.dart';
 
 const primaryColor = Color.fromRGBO(255, 186, 37, 1);
+const primaryTextColor = Color.fromRGBO(33, 23, 114, 1);
--- Mobile/mobile_assessement/lib/features/weatherify/presentation/screen/home_page.dart
+++ Mobile/mobile_assessement/lib/features/weatherify/presentation/screen/home_page.dart
@@ -1,5 +1,9 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:mobile_assessement/features/weatherify/presentation/widgets/action_button.dart';
+
+import '../../../../core/utils/colors.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,6 +15,48 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Choose a city",
+                style: TextStyle(
+                    color: primaryTextColor,
+                    fontSize: MediaQuery.of(context).size.height * 0.03,
+                    fontWeight: FontWeight.bold),
+              ),
+              Row(
+          children: [
+            Expanded(
+              child: SizedBox(
+                height: MediaQuery.of(context).size.height * 0.05,
+                child: Center(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Search a new city',
+                      hintStyle: const TextStyle(
+                          fontFamily: "Poppins", fontWeight: FontWeight.w100),
+                      border: InputBorder.none,
+                      contentPadding:
+                          EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width * 0.04),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            ActionButton(
+              height: MediaQuery.of(context).size.height * 0.05,
+              child: Icon(Icons.search),
+            )
+          ],
+        ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
