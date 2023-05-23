3e1be92ce5b83da58f288abb486fbf9b43e1ca0e
Zerihun Moges <zerihunmoges78@gmail.com>
2023-05-23 05:55:21 -0700

    add direct forwarding to /doctors

--- Web/web_assessment/next.config.js
+++ Web/web_assessment/next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/doctors',
+        permanent: true,
+      },
+    ]
+  },
   images: {
     remotePatterns: [
       {
