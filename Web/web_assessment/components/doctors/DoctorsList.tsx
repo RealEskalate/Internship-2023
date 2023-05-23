f8b9bcb0dc746f3cb9f14c095a8d08a201dabc38
FevenBelay23 <fevenbelay942@gmail.com>
2023-05-23 16:18:14 +0300

    implement doctors list and doctor details pages

--- Web/web_assessment/components/doctors/DoctorsList.tsx
+++ Web/web_assessment/components/doctors/DoctorsList.tsx
@@ -3,6 +3,7 @@ import { useGetDoctorsQuery } from "../../store/features/doctors/doctor-api";
 
 const DoctorsList = () => {
   const { data: doctors = [], isLoading, isError } = useGetDoctorsQuery();
+  console.log(doctors);
 
   useEffect(() => {}, []);
 
@@ -13,24 +14,24 @@ const DoctorsList = () => {
   if (isError) {
     return <div>Error while fetching doctors data.</div>;
   }
-
+  const data = doctors.data;
   return (
     <div>
       <h2>Doctor List</h2>
-      {doctors.map((doctor: any) => (
+      {data.map((doctor: any) => (
         <div key={doctor.id} className="border rounded p-4 mb-4">
           <div className="flex items-center mb-2">
             <img
-              src={doctor.photoUrl}
+              src={data.photoUrl}
               alt="Doctor"
               className="w-16 h-16 rounded-full mr-4"
             />
             <div>
-              <h3 className="text-lg font-bold">{doctor.name}</h3>
-              <p className="text-gray-500">{doctor.specialty}</p>
+              <h3 className="text-lg font-bold">{data.name}</h3>
+              <p className="text-gray-500">{data.specialty}</p>
             </div>
           </div>
-          <p className="text-gray-600">Institution: {doctor.institutionName}</p>
+          <p className="text-gray-600">Institution: {data.institutionName}</p>
         </div>
       ))}
     </div>
