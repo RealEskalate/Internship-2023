ad5283ba4d8a335c84ed0bc7499570ba2b2f0527
Ekram <ekruiwc2020@gmail.com>
2023-05-23 15:26:55 +0300

    set up the environment

--- Web/web_assessment/pages/_app.tsx
+++ Web/web_assessment/pages/_app.tsx
@@ -1,6 +1,9 @@
+import { store } from './../store/store'
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { Provider } from 'react-redux'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App() {
+  return <Provider store={store}>
+      
+     </Provider>
 }
