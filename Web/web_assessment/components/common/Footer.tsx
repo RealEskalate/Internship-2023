8ca2bd4314928af0d6ea3a1adf400b8950bbaa47
hello_world <haileformal@gmail.com>
2023-05-23 15:37:11 +0300

    update footer page

--- Web/web_assessment/components/common/Footer.tsx
+++ Web/web_assessment/components/common/Footer.tsx
@@ -24,54 +24,52 @@ const Footer: React.FC = () => {
 
     return (
         <footer className="bg-indigo-800 text-white p-4">
-            <div className="container mx-auto flex justify-between items-center">
+            <div className="container mx-auto grid grid-cols-3 gap-4">
                 <div>
-                    <h3 className="text-2xl font-bold">HakimHub</h3>
+                    <h3 className="text-2xl font-bold mb-4">HakimHub</h3>
+                </div>
+
+                <div>
                     <h3 className="text-xl mb-4">Get Connected</h3>
                     <ul>
-                        <li className="mb-2">
-                            <a href="#" className="hover:text-blue-500">Actions</a>
-                        </li>
-                        <li className="mb-2">
-                            <a href="#" className="hover:text-blue-500">Company</a>
-                        </li>
                         <li className="mb-2">
                             <a href="#" className="hover:text-blue-500">For Physicians</a>
                         </li>
                         <li className="mb-2">
                             <a href="#" className="hover:text-blue-500">For Hospitals</a>
                         </li>
+                    </ul>
+                </div>
+
+                <div>
+                    <h3 className="text-lg font-bold mb-4">Actions</h3>
+                    <ul>
+                        <li className="mb-2">
+                            <a href="#" className="hover:text-blue-500">Find a Doctor</a>
+                        </li>
+                        <li className="mb-2">
+                            <a href="#" className="hover:text-blue-500">Find a Hospital</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div>
+                    <h3 className="text-lg font-bold mb-4">Company</h3>
+                    <ul>
+                        <li className="mb-2">
+                            <a href="#" className="hover:text-blue-500">Join Us</a>
+                        </li>
                         <li className="mb-2">
                             <a href="#" className="hover:text-blue-500">Career</a>
                         </li>
                         <li className="mb-2">
-                            <a href="#" className="hover:text-blue-500">Privacy Policy</a>
-                        </li>
-                        <li className="mb-2">
-                            <a href="#" className="hover:text-blue-500">Terms of Use</a>
-                        </li>
-                    </ul>
-                </div>
-
-                <div>
-                    <h3 className="text-lg font-bold mb-4">Find a doctor</h3>
-                    <ul>
-                        <li className="mb-2">
-                            <a href="#" className="hover:text-blue-500">About Us</a>
-                        </li>
-                    </ul>
-                </div>
-
-                <div>
-                    <h3 className="text-lg font-bold mb-4">Find a hospital</h3>
-                    <ul>
-                        <li className="mb-2">
-                            <a href="#" className="hover:text-blue-500">Join our team</a>
+                            <a href="#" className="hover:text-blue-500">Our Team</a>
                         </li>
                     </ul>
                 </div>
             </div>
         </footer>
+
     );
 };
 
