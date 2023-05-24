687fffe11547aae5e2c2a8995185241c08dede89
zerabruckmuluneh <zerabruck@a2sv.org>
2023-05-24 12:06:18 +0300

    add header

--- Web/web_assessment/components/Header.tsx
+++ Web/web_assessment/components/Header.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Image from 'next/image'
 
 function Header() {
   return (
-    <div>
-        <h1>Hakim <span className = "text-secondary"> Hub</span></h1>
-
+    <div className=' px-20 pt-8 flex justify-between'>
+        <h1 className='font-bold text-2xl'>Hakim<span className = "text-secondary">Hub</span></h1>
+        <p>Jonathan Alemayew</p>
     </div>
   )
 }
--- Web/web_assessment/pages/_app.tsx
+++ Web/web_assessment/pages/_app.tsx
@@ -3,10 +3,12 @@ import type { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
 import { store } from '@/store/store'
 import Footer from '@/components/Footer'
+import Header from '@/components/Header'
 
 export default function App({ Component, pageProps }: AppProps) {
   return<Provider store={store}>
       <div className='min-h-screen'>
+      <Header/>
       <Component {...pageProps} />
       </div>
       <Footer/>
