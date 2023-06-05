2e80cbd338a55368719c0430ddf5898c069205af
Tseganesh Yifru <tseganeshyifruberta>
2023-06-05 16:58:28 +0300

    fix minor issues

--- Web/web_assessment/components/hospital/HospitalDetails.tsx
+++ Web/web_assessment/components/hospital/HospitalDetails.tsx
@@ -3,6 +3,7 @@ import { useRouter } from "next/router";
 import { LoadingPage } from "../common/Loading";
 import { MockDetail } from "./HospitalDetailMock";
 import ErrorPage from "../common/Error";
+import Image from 'next/image';
 
 const HospitalDetails: React.FC = () => {
   const router = useRouter();
@@ -16,59 +17,217 @@ const HospitalDetails: React.FC = () => {
   if (isError) {
     return <ErrorPage />;
   }
-  return (
-    <div>
-      <div>Region</div>
-      <div>NAME</div>
-      <div>tags</div>
-      <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis dolore
-        maiores, natus ducimus, harum tempore quaerat veritatis minima molestiae
-        maxime labore vel quasi. Et fuga perspiciatis veritatis quidem, voluptas
-        accusamus animi ipsa autem! A amet dolore quod vel eos quos, numquam est
-        consequuntur possimus repudiandae dignissimos, nobis culpa similique
-        optio.
-      </div>
-      <div>
-        <div>Services</div>
+  return ( <div className="p-44 pt-0">
+  <div>
+    <Image
+      src="/image/background.png"
+      alt={""}
+      width={1500}
+      height={400}
+      className="rounded-lg"
+    />
+  </div>
+  <div className="font-extrabold text-4xl mb-3 mt-3">
+    Black Lion Hospital
+  </div>
+
+  <div className="flex flex-wrap mb-8">
+    <div className="">
+      <IsOpen isopen={"close"} />{" "}
+    </div>
+    <div className="bg-blue-200 rounded-full pl-4 pr-4 pt-1 pb-1 flex justify-center self-center ml-4">
+      {" "}
+      Available 24 Hours
+    </div>
+  </div>
+  <div className="mb-20">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis dolore
+    maiores, natus ducimus, harum tempore quaerat veritatis minima molestiae
+    maxime labore vel quasi. Et fuga perspiciatis veritatis quidem, voluptas
+    accusamus animi ipsa autem! A amet dolore quod vel eos quos, numquam est
+    consequuntur possimus repudiandae dignissimos, nobis culpa similique
+    optio.
+  </div>
+  <div>
+    <div className="mb-6 font-bold text-xl">Services</div>
+    <div className="flex flex-wrap mb-20">
+      <div className="w-1/3 ">
         <div className="flex flex-wrap">
-          <div>
-            <li>Lorem, ipsum.</li>
-            <li>Lorem, ipsum.</li>
-            <li>Lorem, ipsum.</li>
-            <li>Lorem, ipsum.</li>
+          <div className="flex">
+            <Image
+              src="/image/dot.png"
+              alt={""}
+              width={8}
+              height={8}
+              className="justify-center self-center mr-2"
+            />
           </div>
-          <div>
-            <li>Lorem, ipsum.</li>
-            <li>Lorem, ipsum.</li>
-            <li>Lorem, ipsum.</li>
-            <li>Lorem, ipsum.</li>
+          <div>Lorem, ipsum.</div>
+        </div>
+        <div className="flex flex-wrap">
+          <div className="flex">
+            <Image
+              src="/image/dot.png"
+              alt={""}
+              width={8}
+              height={8}
+              className="justify-center self-center mr-2"
+            />
+          </div>
+          <div>Lorem, ipsum.</div>
+        </div>
+        <div className="flex flex-wrap">
+          <div className="flex">
+            <Image
+              src="/image/dot.png"
+              alt={""}
+              width={8}
+              height={8}
+              className="justify-center self-center mr-2"
+            />
+          </div>
+          <div>Lorem, ipsum.</div>
+        </div>
+        <div className="flex flex-wrap">
+          <div className="flex">
+            <Image
+              src="/image/dot.png"
+              alt={""}
+              width={8}
+              height={8}
+              className="justify-center self-center mr-2"
+            />
+          </div>
+          <div>Lorem, ipsum.</div>
+        </div>
+      </div>
+      <div className="w-1/3 justify-center">
+        <div className="flex flex-wrap">
+          <div className="flex">
+            <Image
+              src="/image/dot.png"
+              alt={""}
+              width={8}
+              height={8}
+              className="justify-center self-center mr-2"
+            />
+          </div>
+          <div>Lorem, ipsum.</div>
+        </div>
+        <div className="flex flex-wrap">
+          <div className="flex">
+            <Image
+              src="/image/dot.png"
+              alt={""}
+              width={8}
+              height={8}
+              className="justify-center self-center mr-2"
+            />
+          </div>
+          <div>Lorem, ipsum.</div>
+        </div>
+        <div className="flex flex-wrap">
+          <div className="flex">
+            <Image
+              src="/image/dot.png"
+              alt={""}
+              width={8}
+              height={8}
+              className="justify-center self-center mr-2"
+            />
+          </div>
+          <div>Lorem, ipsum.</div>
+        </div>
+        <div className="flex flex-wrap">
+          <div className="flex">
+            <Image
+              src="/image/dot.png"
+              alt={""}
+              width={8}
+              height={8}
+              className="justify-center self-center mr-2"
+            />
+          </div>
+          <div>Lorem, ipsum.</div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div className="border-2 border-gray-400 p-10 pt-1 pl-1 rounded-lg">
+    <div className="font-bold text-xl mb-4">Contact Details</div>
+    <div className="flex flex-wrap">
+      <div className="w-1/2">
+        <div>
+          <div className="text-md font-bold text-primary flex">
+            <div className="flex">
+              <Image
+                src="/image/phone.png"
+                alt={""}
+                width={15}
+                height={15}
+                className="self-center mr-2"
+              />
+            </div>
+            <div>Main Office :</div>
+          </div>
+          <div className="ml-4">
+            <div>phone 1</div>
+            <div>phone 2</div>
+          </div>
+        </div>
+        <div>
+          <div className="text-md font-bold text-primary flex">
+            <div className="flex">
+              <Image
+                src="/image/phone.png"
+                alt={""}
+                width={15}
+                height={15}
+                className="self-center mr-2"
+              />
+            </div>
+            <div>Laboratory Office :</div>
+          </div>
+          <div className="ml-4">
+            <div>phone 1</div>
+            <div>phone 2</div>
           </div>
         </div>
       </div>
-      <div className="border-2 border-black">
-        <div>Contact Details</div>
-        <div className="flex flex-wrap">
+      <div className="flex flex-wrap justify-center w-1/2">
+        <div>
           <div>
-            <div>
-              <div>Main Office</div>
-              <div>phone 1</div>
-              <div>phone 2</div>
+            <div className="text-md font-bold text-primary flex">
+              <div className="flex">
+                <Image
+                  src="/image/email.png"
+                  alt={""}
+                  width={15}
+                  height={15}
+                  className="self-center mr-2"
+                />
+              </div>
+              <div>Email :</div>
             </div>
-            <div>
-              <div>Main Office</div>
+            <div className="ml-4">
               <div>phone 1</div>
               <div>phone 2</div>
             </div>
           </div>
           <div>
-            <div>
-              <div>Main Office</div>
-              <div>phone 1</div>
-              <div>phone 2</div>
+            <div className="text-md font-bold text-primary flex">
+              <div className="flex">
+                <Image
+                  src={hospital.photo}
+                  alt={""}
+                  width={15}
+                  height={15}
+                  className="self-center mr-2"
+                />
+              </div>
+              <div>Location :</div>
             </div>
-            <div>
-              <div>Main Office</div>
+            <div className="ml-4">
               <div>phone 1</div>
               <div>phone 2</div>
             </div>
@@ -76,7 +235,8 @@ const HospitalDetails: React.FC = () => {
         </div>
       </div>
     </div>
-  );
+  </div>
+</div>);
 };
 
 export default HospitalDetails;
