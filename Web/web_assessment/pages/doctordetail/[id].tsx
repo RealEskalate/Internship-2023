9eb5c44a1e45c687f5d8a8a364e854ac8790f387
zerabruckmuluneh <zerabruck@a2sv.org>
2023-06-21 20:04:35 +0300

    update state managment of doctor detail

--- Web/web_assessment/pages/doctordetail/[id].tsx
+++ Web/web_assessment/pages/doctordetail/[id].tsx
@@ -14,7 +14,14 @@ const DoctorCard: React.FC = () => {
   }
 
   if (error) {
-    return <div>Error: {error.message}</div>;
+    return <div>
+        <div className="flex items-center justify-center h-16">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="red" className="w-6 h-6">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+        </svg>
+        <span className="ml-2 text-red-500 font-medium">Error occurred. Please try again.</span>
+        </div>  
+      </div>;
   }
 
   return (
